ac3c02ea7e1fdd5f3ecd77d09e93bf1ace8ce9ba
Jason Dent <jason@streetsidesoftware.nl>
2017-02-01 13:35:33 +0100

    Add some more tests

--- src/application.test.ts
+++ src/application.test.ts
@@ -16,4 +16,32 @@ describe('Validate the Application', () => {
                 expect(result.files).equals(1);
             });
     });
+
+    it('Tests running the application verbose', () => {
+        const files = ['samples/text.txt'];
+        const options = { verbose: true };
+        const logs: string[] = [];
+        const logger = (message?: any) => {
+            logs.push(message + '');
+        };
+        const app = new App.CSpellApplication(files, options, logger);
+        return app.run()
+            .then(result => {
+                expect(result.files).equals(1);
+            });
+    });
+
+    it('Tests running the application words only', () => {
+        const files = ['samples/text.txt'];
+        const options = { wordsOnly: true, unique: true };
+        const logs: string[] = [];
+        const logger = (message?: any) => {
+            logs.push(message + '');
+        };
+        const app = new App.CSpellApplication(files, options, logger);
+        return app.run()
+            .then(result => {
+                expect(result.files).equals(1);
+            });
+    });
 });
