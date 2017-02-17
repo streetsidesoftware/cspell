2c1f9948fe4c5d59e632752be63cbb1c344fc020
Jason Dent <jason@streetsidesoftware.nl>
2017-02-17 13:28:56 +0100

    Remove dependency on cspell-tools

--- packages/hunspell-reader/src/fileReader.ts
+++ packages/hunspell-reader/src/fileReader.ts
@@ -1,4 +1,4 @@
-export {lineReaderRx as lineReader} from 'cspell-tools';
-export {textFileStreamRx as textFileStream} from 'cspell-tools';
-export {stringsToLinesRx as stringsToLines} from 'cspell-tools';
+export {lineReaderRx as lineReader} from 'cspell-lib';
+export {textFileStreamRx as textFileStream} from 'cspell-lib';
+export {stringsToLinesRx as stringsToLines} from 'cspell-lib';
 
--- packages/hunspell-reader/src/get-uri.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "get-uri" {
-    import * as fs from 'fs';
-
-    function getUri(uri: string, callback?: (err: any, fs: fs.ReadStream) => any): void;
-    function getUri(uri: string, options?: { cache?: fs.ReadStream }, callback?: (err: any, fs: fs.ReadStream) => any): void;
-    export = getUri;
-}
