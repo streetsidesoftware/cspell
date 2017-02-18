72fc42cf4db6da03a0a3667dc5a7ccc71a8fdecf
Jason Dent <jason@streetsidesoftware.nl>
2017-02-18 23:08:04 +0100

    Increase coverage.

--- packages/cspell-trie/src/lib/trie.test.ts
+++ packages/cspell-trie/src/lib/trie.test.ts
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import {Trie} from './trie';
+import {isWordTerminationNode} from './util';
 
 describe('Validate Trie Class', () => {
     it('Tests creating a Trie', () => {
@@ -21,8 +22,17 @@ describe('Validate Trie Class', () => {
     it('Tests complete', () => {
         const trie = Trie.create(sampleWords);
         expect([...trie.complete('lift')]).to.be.deep.equal(sampleWords.filter(w => w.slice(0, 4) === 'lift').sort());
+        expect([...trie.complete('life')]).to.be.deep.equal([]);
+        expect([...trie.complete('lifting')]).to.be.deep.equal(['lifting']);
     });
 
+    it('Tests iterate', () => {
+        const trie = Trie.create(sampleWords);
+        const words = [...trie.iterate()]
+            .filter(r => isWordTerminationNode(r.node))
+            .map(r => r.text);
+        expect(words).to.be.deep.equal(sampleWords.sort());
+    });
 });
 
 const sampleWords = [
