f60b23976239ea00a0a759c7b75ee050469c1908
Jason Dent <jason@streetsidesoftware.nl>
2017-06-10 16:48:43 +0200

    Update packages

--- src/Settings/LanguageSettings.test.ts
+++ src/Settings/LanguageSettings.test.ts
@@ -27,7 +27,7 @@ describe('Validate LanguageSettings', () => {
         expect(sPhp.allowCompoundWords).to.be.undefined;
         expect(sPhp.dictionaries).to.not.be.empty;
         expect((sPhp.dictionaries!).sort())
-            .to.be.deep.equal(['wordsEnGb', 'filetypes', , 'companies', 'softwareTerms', 'php', 'html', 'npm', 'fonts', 'css', 'typescript', 'misc'].sort());
+            .to.be.deep.equal(['wordsEnGb', 'filetypes', 'companies', 'softwareTerms', 'php', 'html', 'npm', 'fonts', 'css', 'typescript', 'misc'].sort());
     });
 
     it('tests that settings at language level are merged', () => {
