b1bfd28d1a84c922590943a6ad5cdd0eab29a0f0
Jason Dent <jason@streetsidesoftware.nl>
2017-07-23 18:02:03 +0200

    Adjust the order so the language setting is imported before the dictionaries are filtered out.

--- src/Settings/TextDocumentSettings.ts
+++ src/Settings/TextDocumentSettings.ts
@@ -4,8 +4,11 @@ import * as CSpellSettings from './CSpellSettingsServer';
 import { getInDocumentSettings } from './InDocSettings';
 
 export function combineTextAndLanguageSettings(settings: CSpellUserSettings, text: string, languageId: string | string[]): CSpellUserSettings {
-    const langSettings = calcSettingsForLanguageId(settings, languageId);
-    return CSpellSettings.mergeSettings(langSettings, extractSettingsFromText(text));
+    const docSettings = extractSettingsFromText(text);
+    const settingsForText = CSpellSettings.mergeSettings(settings, docSettings);
+    const langSettings = calcSettingsForLanguageId(settingsForText, languageId);
+    // Merge again, to force In-Doc settings.
+    return CSpellSettings.mergeSettings(langSettings, docSettings);
 }
 
 export function extractSettingsFromText(text: string) {
