58d16bafc4e0660809653f3f8a52e0478906bf79
Jason Dent <jason@streetsidesoftware.nl>
2018-04-21 14:16:10 +0200

    Improve regex performance.

---
+++ src/Settings/RegExpPatterns.spec.ts
@@ -0,0 +1,6 @@
+// import { expect } from 'chai';
+// import { regExCStyleComments } from './RegExpPatterns';
+
+// describe('test performance of ', () => {
+
+// });
--- src/Settings/RegExpPatterns.ts
+++ src/Settings/RegExpPatterns.ts
@@ -5,18 +5,19 @@ export const regExHRef = /\bhref\s*=\s*".*?"/gi;
 export const regExHexDigits = /^x?[0-1a-f]+$/i;
 export const regExMatchCommonHexFormats = /(?:#[0-9a-f]{3,8})|(?:0x[0-9a-f]+)|(?:\\u[0-9a-f]{4})|(?:\\x\{[0-9a-f]{4}\})/gi;
 // tslint:disable-next-line
-export const regExSpellingGuard = /(?:(?:spell-?checker|cSpell)::?\s*disable(?!-line|-next)\b(?:.|\s)*?(?:(?:spell-?checker|cSpell)::?\s*enable\b|$))|(?:.*(?:spell-?checker|cSpell)::?\s*disable-line\b.*)|(?:(?:spell-?checker|cSpell)::?\s*disable-next\b.*\n.*\n)/gi;
+export const regExSpellingGuard = /(?:(?:spell-?checker|cSpell)::?\s*disable(?!-line|-next)\b[\s\S]*?(?:(?:spell-?checker|cSpell)::?\s*enable\b|$))|(?:.*(?:spell-?checker|cSpell)::?\s*disable-line\b.*)|(?:(?:spell-?checker|cSpell)::?\s*disable-next\b.*\n.*\n)/gi;
 export const regExPublicKey = /BEGIN\s+PUBLIC\s+KEY(?:.|\s)+?END\s+PUBLIC\s+KEY/gi;
 export const regExCert = /BEGIN\s+CERTIFICATE(?:.|\s)+?END\s+CERTIFICATE/gi;
 export const regExEscapeCharacters = /\\(?:[anrvtbf]|[xu][a-f0-9]+)/gi;
 export const regExBase64 = /(?:[a-z0-9\/+]{40,}\s*)+(?:[a-z0-9\/+]+=*)?/gi;
 
 // Include Expressions
-export const regExPhpHereDoc = /<<<['"]?(\w+)['"]?(?:.|\s)+?^\1;/gim;
-export const regExString = /(?:(['"])(?:\\\\|(?:\\\1)|[^\1\n])+\1)|(?:([`])(?:\\\\|(?:\\\2)|[^\2])+?\2)/gi;
+export const regExPhpHereDoc = /<<<['"]?(\w+)['"]?[\s\S]+?^\1;/gm;
+export const regExString = /(?:(['"])(?:\\\\|(?:\\\1)|[^\1\n])+\1)|(?:([`])(?:\\\\|(?:\\\2)|[^\2])+?\2)/g;
 
 // Note: the C Style Comments incorrectly considers '/*' and '//' inside of strings as comments.
-export const regExCStyleComments = /(?:\/\/.*$)|(?:\/\*(?:.|\s)+?\*\/)/gim;
+export const regExCStyleComments = /(?:\/\/.*)|(?:\/\*[\s\S]+?\*\/)/g;
+export const rexExPythonStyleComments = /#.*|(?:('''|""")[^\1]+?\1)/gm;
 
 export const regExEmail = /<?[\w.\-+]+@\w+(\.\w+)+>?/gi;
 
