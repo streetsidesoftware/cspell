cf9041b71da3be2b5abb0779e9a8a58146c3e55e
Jason Dent <jason@streetsidesoftware.nl>
2018-04-28 19:43:36 +0200

    Remove dependency on rxjs-compat

--- packages/cspell-lib/src/file/fileReader.test.ts
+++ packages/cspell-lib/src/file/fileReader.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as fReader from './fileReader';
-import * as Rx from 'rxjs/Rx';
+import { of } from 'rxjs';
+import { toArray } from 'rxjs/operators';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 
@@ -14,23 +15,24 @@ describe('Validate the fileReader', () => {
     ].map(f => path.join(samplePath, f));
 
     it('tests stringsToLines', () => {
-        const strings = Rx.Observable.of('a1\n2\n3\n4', '5\n6');
-        return fReader.stringsToLinesRx(strings).toArray().toPromise().then((a) => {
+        const strings = of('a1\n2\n3\n4', '5\n6');
+        return fReader.stringsToLinesRx(strings).pipe(toArray())
+        .toPromise().then((a) => {
             expect(a).to.be.deep.equal(['a1', '2', '3', '45', '6']);
         });
     });
 
     it('tests stringsToLines trailing new line', () => {
-        const strings = Rx.Observable.of('a1\n2\n3\n4', '5\n6\n');
-        return fReader.stringsToLinesRx(strings).toArray().toPromise().then((a) => {
+        const strings = of('a1\n2\n3\n4', '5\n6\n');
+        return fReader.stringsToLinesRx(strings).pipe(toArray()).toPromise().then((a) => {
             expect(a).to.be.deep.equal(['a1', '2', '3', '45', '6', '']);
         });
     });
 
     it('test the file reader', () => {
         return fReader.stringsToLinesRx(fReader.textFileStreamRx(__filename))
-            .toArray()
-            .toPromise()
+        .pipe(toArray())
+        .toPromise()
             .then(lines => {
                 const actual = lines.join('\n');
                 const expected = fs.readFileSync(__filename, 'UTF-8');
@@ -40,8 +42,8 @@ describe('Validate the fileReader', () => {
 
     it('test the lineReaderRx', () => {
         return fReader.lineReaderRx(__filename)
-            .toArray()
-            .toPromise()
+        .pipe(toArray())
+        .toPromise()
             .then(lines => {
                 const expected = fs.readFileSync(__filename, 'UTF-8').split('\n');
                 expect(lines).to.deep.equal(expected);
@@ -50,8 +52,8 @@ describe('Validate the fileReader', () => {
 
     it('tests reading the cities sample', async () => {
         const lines = await fReader.lineReaderRx(fileCities)
-            .toArray()
-            .toPromise();
+        .pipe(toArray())
+        .toPromise();
         const file = await fs.readFile(fileCities, 'utf8');
         expect(lines).to.be.deep.equal(file.split('\n'));
     });
@@ -60,8 +62,8 @@ describe('Validate the fileReader', () => {
         await Promise.all(sampleFiles
             .map(async filename => {
                 const lines = await fReader.streamFileLineByLineRx(filename)
-                    .toArray()
-                    .toPromise();
+                .pipe(toArray())
+                .toPromise();
                 const file = await fs.readFile(filename, 'utf8');
                 expect(lines, `compare to file: ${filename}`).to.be.deep.equal(file.split(/\r?\n/));
             }));
@@ -69,16 +71,16 @@ describe('Validate the fileReader', () => {
 
     it('tests streamFileLineByLineRx 2', async () => {
         const lines = await fReader.streamFileLineByLineRx(__filename)
-            .toArray()
-            .toPromise();
+        .pipe(toArray())
+        .toPromise();
         const file = await fs.readFile(__filename, 'utf8');
         expect(lines).to.be.deep.equal(file.split('\n'));
     });
 
     it('test missing file', () => {
         return fReader.lineReaderRx(__filename + 'not.found')
-            .toArray()
-            .toPromise()
+        .pipe(toArray())
+        .toPromise()
             .then(
                 () => {
                     expect('not to be here').to.be.true;
--- packages/cspell-lib/src/file/fileWriter.test.ts
+++ packages/cspell-lib/src/file/fileWriter.test.ts
@@ -1,42 +1,43 @@
 import { expect } from 'chai';
 import * as fileWriter from './fileWriter';
 import * as fileReader from './fileReader';
-import * as Rx from 'rxjs/Rx';
+import { fromEvent, from } from 'rxjs';
+import { flatMap, concatMap, reduce, take, map } from 'rxjs/operators';
 import * as loremIpsum from 'lorem-ipsum';
 import * as path from 'path';
 import { mkdirp } from 'fs-extra';
 import * as fse from 'fs-extra';
 
 describe('Validate the writer', () => {
-    it('tests writing an Rx.Observable and reading it back.', () => {
+    it('tests writing an Observable and reading it back.', () => {
         const text = loremIpsum({ count: 1000, format: 'plain', units: 'words'}) + ' éåáí';
         const data = text.split(/\b/);
         const filename = path.join(__dirname, '..', '..', 'temp', 'tests-writing-an-observable.txt');
 
-        return Rx.Observable.from(mkdirp(path.dirname(filename)))
-            .flatMap(() => {
-                return fileWriter.writeToFileRxP(filename, Rx.Observable.from(data));
-            })
-            .concatMap(() => fileReader.textFileStreamRx(filename))
-            .reduce((a, b) => a + b)
-            .toPromise()
+        return from(mkdirp(path.dirname(filename))).pipe(
+            flatMap(() => {
+                return fileWriter.writeToFileRxP(filename, from(data));
+            }),
+            concatMap(() => fileReader.textFileStreamRx(filename)),
+            reduce((a, b) => a + b),
+        ).toPromise()
             .then(result => {
                 expect(result).to.equal(text);
             });
     });
 
-    it('tests writing an Rx.Observable and reading it back. gz', () => {
+    it('tests writing an Observable and reading it back. gz', () => {
         const text = loremIpsum({ count: 1000, format: 'plain', units: 'words'}) + ' éåáí';
         const data = text.split(/\b/);
         const filename = path.join(__dirname, '..', '..', 'temp', 'tests-writing-an-observable.txt.gz');
 
-        return Rx.Observable.from(mkdirp(path.dirname(filename)))
-            .flatMap(() => {
-                return fileWriter.writeToFileRxP(filename, Rx.Observable.from(data));
-            })
-            .concatMap(() => fileReader.textFileStreamRx(filename))
-            .reduce((a, b) => a + b)
-            .toPromise()
+        return from(mkdirp(path.dirname(filename))).pipe(
+            flatMap(() => {
+                return fileWriter.writeToFileRxP(filename, from(data));
+            }),
+            concatMap(() => fileReader.textFileStreamRx(filename)),
+            reduce((a, b) => a + b),
+        ).toPromise()
             .then(result => {
                 expect(result).to.equal(text);
             });
@@ -46,16 +47,16 @@ describe('Validate the writer', () => {
         const text = loremIpsum({ count: 1000, format: 'plain', units: 'words'}) + ' éåáí';
         const filename = path.join(__dirname, '..', '..', 'temp', 'tests-writing.txt');
 
-        return Rx.Observable.from(mkdirp(path.dirname(filename)))
-            .flatMap(() => {
+        return from(mkdirp(path.dirname(filename))).pipe(
+            flatMap(() => {
                 const wStream = fileWriter.writeToFile(filename, text);
-                return Rx.Observable.fromEvent(wStream, 'close');
-            })
-            .take(1)
-            .concatMap(() => fse.readFile(filename))
-            .map(buffer => buffer.toString('utf8'))
-            .take(1)
-            .toPromise()
+                return fromEvent(wStream, 'close');
+            }),
+            take(1),
+            concatMap(() => fse.readFile(filename)),
+            map(buffer => buffer.toString('utf8')),
+            take(1),
+        ).toPromise()
             .then(result => {
                 expect(result).to.equal(text);
             });
@@ -65,15 +66,15 @@ describe('Validate the writer', () => {
         const text = loremIpsum({ count: 1000, format: 'plain', units: 'words'}) + ' éåáí';
         const filename = path.join(__dirname, '..', '..', 'temp', 'tests-writing.txt.gz');
 
-        return Rx.Observable.from(mkdirp(path.dirname(filename)))
-            .flatMap(() => {
+        return from(mkdirp(path.dirname(filename))).pipe(
+            flatMap(() => {
                 const wStream = fileWriter.writeToFile(filename, text);
-                return Rx.Observable.fromEvent(wStream, 'close');
-            })
-            .take(1)
-            .concatMap(() => fileReader.textFileStreamRx(filename))
-            .reduce((a, b) => a + b)
-            .toPromise()
+                return fromEvent(wStream, 'close');
+            }),
+            take(1),
+            concatMap(() => fileReader.textFileStreamRx(filename)),
+            reduce((a, b) => a + b),
+        ).toPromise()
             .then(result => {
                 expect(result).to.equal(text);
             });
