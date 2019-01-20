3d7a7b6dfca6423b9087f136f6667cb7c27912a7
Jason Dent <jason@streetsidesoftware.nl>
2019-01-20 22:32:52 +0100

    Make loading test dictionaries easier.

--- packages/cspell-trie/src/lib/suggest-en.test.ts
+++ packages/cspell-trie/src/lib/suggest-en.test.ts
@@ -1,133 +1,121 @@
 import {expect} from 'chai';
-import {Trie} from './trie';
 import {
     genCompoundableSuggestions,
     CompoundWordsMethod,
     suggestionCollector,
 } from './suggest';
-import {readTrieFile} from './reader.test';
-import * as cspellDict from 'cspell-dict-en_us';
+import { readTrie } from './dictionaries.test';
 
-
-let trie: Promise<Trie>;
 
 function getTrie() {
-    if (!trie) {
-        trie = readTrieFile(cspellDict.getConfigLocation());
-    }
-    return trie;
+    return readTrie('cspell-dict-en_us');
 }
+
 
 const timeout = 10000;
 
 describe('Validate English Suggestions', function() {
 
-    it('Tests suggestions', function() {
+    it('Tests suggestions', async function() {
         this.timeout(timeout);
-        return getTrie().then(trie => {
-            const collector = suggestionCollector('joyful', 8, undefined, 1);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.NONE
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('joyful');
-            expect(suggestions[0]).to.be.equal('joyful');
-        });
+        const trie = await getTrie();
+        const collector = suggestionCollector('joyful', 8, undefined, 1);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.NONE
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('joyful');
+        expect(suggestions[0]).to.be.equal('joyful');
     });
 
-    it('Tests suggestions', function() {
+    it('Tests suggestions', async function() {
         this.timeout(timeout);
-        return getTrie().then(trie => {
-            // cspell:ignore joyfull
-            const collector = suggestionCollector('joyfull', 8);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.SEPARATE_WORDS
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('joyful');
-            expect(suggestions[0]).to.be.equal('joyfully');
-            expect(suggestions[1]).to.be.equal('joyful');
-            expect(suggestions).to.be.length(collector.maxNumSuggestions);
-        });
+        const trie = await getTrie();
+        // cspell:ignore joyfull
+        const collector = suggestionCollector('joyfull', 8);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.SEPARATE_WORDS
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('joyful');
+        expect(suggestions[0]).to.be.equal('joyfully');
+        expect(suggestions[1]).to.be.equal('joyful');
+        expect(suggestions).to.be.length(collector.maxNumSuggestions);
     });
 
-    it('Tests compound SEPARATE_WORDS suggestions', function() {
+    it('Tests compound SEPARATE_WORDS suggestions', async function() {
         this.timeout(timeout);
-        return getTrie().then(trie => {
-            // cspell:ignore onetwothreefour
-            const collector = suggestionCollector('onetwothreefour', 8, undefined, 3.3);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.SEPARATE_WORDS
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('one two three four');
-            expect(suggestions[0]).to.be.equal('one two three four');
-            expect(suggestions).to.be.length(collector.maxNumSuggestions);
-        });
+        const trie = await getTrie();
+        // cspell:ignore onetwothreefour
+        const collector = suggestionCollector('onetwothreefour', 8, undefined, 3.3);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.SEPARATE_WORDS
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('one two three four');
+        expect(suggestions[0]).to.be.equal('one two three four');
+        expect(suggestions).to.be.length(collector.maxNumSuggestions);
     });
 
-    it('Tests compound JOIN_WORDS suggestions', function() {
+    it('Tests compound JOIN_WORDS suggestions', async function() {
         this.timeout(timeout);
-        return getTrie().then(trie => {
-            // cspell:ignore onetwothrefour
-            const collector = suggestionCollector('onetwothreefour', 8, undefined, 3);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.JOIN_WORDS
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('one+two+three+four');
-            expect(suggestions).to.be.length(collector.maxNumSuggestions);
-        });
+        const trie = await getTrie();
+        // cspell:ignore onetwothrefour
+        const collector = suggestionCollector('onetwothreefour', 8, undefined, 3);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.JOIN_WORDS
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('one+two+three+four');
+        expect(suggestions).to.be.length(collector.maxNumSuggestions);
     });
 
-    it('Tests compound suggestions', function() {
+    it('Tests compound suggestions', async function() {
         this.timeout(timeout);
-        return getTrie().then(trie => {
-            // cspell:ignore onetwothrefour
-            const collector = suggestionCollector('onetwothreefour', 8, undefined, 3);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.JOIN_WORDS
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('one+two+three+four');
-            expect(suggestions).to.be.length(collector.maxNumSuggestions);
-        });
+        const trie = await getTrie();
+        // cspell:ignore onetwothrefour
+        const collector = suggestionCollector('onetwothreefour', 8, undefined, 3);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.JOIN_WORDS
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('one+two+three+four');
+        expect(suggestions).to.be.length(collector.maxNumSuggestions);
     });
 
     // Takes a long time.
-    it('Tests long compound suggestions', function() {
+    it('Tests long compound suggestions', async function() {
         this.timeout(timeout * 2);
-        return getTrie().then(trie => {
-            // cspell:ignore testscompundsuggestions
-            const collector = suggestionCollector('testscompundsuggestions', 1, undefined, 3);
-            collector.collect(genCompoundableSuggestions(
-                trie.root,
-                collector.word,
-                CompoundWordsMethod.SEPARATE_WORDS
-            ));
-            const results = collector.suggestions;
-            const suggestions = results.map(s => s.word);
-            // console.log('Results:');
-            // console.log(results.map((r, i) => `${i} ${r.cost} ${r.word}`).join('\n'));
-            expect(suggestions).to.be.length(collector.maxNumSuggestions);
-            expect(suggestions).to.contain('tests compound suggestions');
-            expect(suggestions[0]).to.be.equal('tests compound suggestions');
-        });
+        const trie = await getTrie();
+        // cspell:ignore testscompundsuggestions
+        const collector = suggestionCollector('testscompundsuggestions', 1, undefined, 3);
+        collector.collect(genCompoundableSuggestions(
+            trie.root,
+            collector.word,
+            CompoundWordsMethod.SEPARATE_WORDS
+        ));
+        const results = collector.suggestions;
+        const suggestions = results.map(s => s.word);
+        // console.log('Results:');
+        // console.log(results.map((r, i) => `${i} ${r.cost} ${r.word}`).join('\n'));
+        expect(suggestions).to.be.length(collector.maxNumSuggestions);
+        expect(suggestions).to.contain('tests compound suggestions');
+        expect(suggestions[0]).to.be.equal('tests compound suggestions');
     });
 });
 
--- packages/cspell-trie/src/lib/suggest-es.test.ts
+++ packages/cspell-trie/src/lib/suggest-es.test.ts
@@ -1,29 +1,20 @@
 import {expect} from 'chai';
-import {Trie} from './trie';
-import {readTrieFile} from './reader.test';
-import * as cspellDict from 'cspell-dict-es-es';
-
-
-let trie: Promise<Trie>;
+import { readTrie } from './dictionaries.test';
 
 function getTrie() {
-    if (!trie) {
-        trie = readTrieFile(cspellDict.getConfigLocation());
-    }
-    return trie;
+    return readTrie('cspell-dict-es-es');
 }
 
 describe('Validate Spanish Suggestions', () => {
-    it('Tests suggestions', function() {
+    it('Tests suggestions', async function() {
         this.timeout(5000);
-        return getTrie().then(trie => {
-            // cspell:ignore Carmjen
-            const results = trie.suggestWithCost('carmjen', 10);
-            // console.log(JSON.stringify(results));
-            const suggestions = results.map(s => s.word);
-            expect(suggestions).to.contain('carmen');
-            expect(suggestions[0]).to.be.equal('carmen');
-        });
+        const trie = await getTrie();
+        // cspell:ignore Carmjen
+        const results = trie.suggestWithCost('carmjen', 10);
+        // console.log(JSON.stringify(results));
+        const suggestions = results.map(s => s.word);
+        expect(suggestions).to.contain('carmen');
+        expect(suggestions[0]).to.be.equal('carmen');
     });
 });
 
