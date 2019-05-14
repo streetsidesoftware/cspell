b8884196484cb021c1c254862aebaae008f38e30
Jason Dent <jason@streetsidesoftware.nl>
2019-05-14 07:31:54 +0200

    Remove compiled files

--- packages/cspell-glob/lib/GlobMatcher.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class GlobMatcher {
-    readonly patterns: string[];
-    readonly root?: string | undefined;
-    readonly matcher: (filename: string) => boolean;
-    constructor(patterns: string[], root?: string | undefined);
-    match(filename: string): boolean;
-}
