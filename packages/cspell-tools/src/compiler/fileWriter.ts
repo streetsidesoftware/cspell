53928fadde347f70436d62ec055cb3993ff252a5
Jason Dent <jason@streetsidesoftware.nl>
2019-05-23 14:57:55 +0200

    Improve code coverage
    - Add hunspell test
    - Remove dead code.

--- packages/cspell-tools/src/compiler/fileWriter.ts
+++ packages/cspell-tools/src/compiler/fileWriter.ts
@@ -1,50 +1,7 @@
-import { Sequence, genSequence } from 'gensequence';
-import { iterableToStream } from 'hunspell-reader/dist/iterableToStream';
-import { batch } from 'hunspell-reader/dist/util';
+import { Sequence } from 'gensequence';
 import { writeToFileIterableP } from 'cspell-io';
 export { writeToFile, writeToFileIterableP, writeToFileIterable } from 'cspell-io';
 
-export function writeSeqToFile(seq: Sequence<string>, outFile: string | undefined): Promise<void> {
-    if (outFile) {
-        return writeToFileIterableP(outFile, seq);
-    }
-
-    return new Promise((resolve, reject)  => {
-        let resolved = false;
-        const out = process.stdout;
-        const bufferedSeq = genSequence(batch(seq, 500)).map(batch => batch.join(''));
-        const dataStream = iterableToStream(bufferedSeq);
-        const fileStream = dataStream.pipe(out);
-        const endEvents = ['finish', 'close', 'end'];
-
-        function resolvePromise() {
-            if (!resolved) {
-                resolved = true;
-                resolve();
-            }
-        }
-        const endHandler = () => {
-            cleanupStreams();
-            setTimeout(resolvePromise, 10);
-        };
-        const errorHandler = (e: Error) => {
-            cleanupStreams();
-            reject(e);
-        };
-
-        listenToStreams();
-
-        function listenToStreams() {
-            endEvents.forEach(event => fileStream.addListener(event, endHandler));
-            fileStream.addListener('error', errorHandler);
-            dataStream.addListener('end', endHandler);
-        }
-
-        function cleanupStreams() {
-            endEvents.forEach(event => fileStream.removeListener(event, endHandler));
-            fileStream.removeListener('error', errorHandler);
-            dataStream.removeListener('end', endHandler);
-        }
-    });
+export function writeSeqToFile(seq: Sequence<string>, outFile: string): Promise<void> {
+    return writeToFileIterableP(outFile, seq);
 }
-
