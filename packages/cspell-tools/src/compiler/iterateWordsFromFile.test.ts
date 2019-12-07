bb63957718b775889a9ecd5bb9707d07b0473fe4
Jason Dent <jason@streetsidesoftware.nl>
2019-12-07 09:53:55 +0100

    Add ability to read trie files as source.

--- packages/cspell-tools/src/compiler/iterateWordsFromFile.test.ts
+++ packages/cspell-tools/src/compiler/iterateWordsFromFile.test.ts
@@ -1,12 +1,20 @@
-import { expect } from 'chai';
 import { streamWordsFromFile } from './iterateWordsFromFile';
 import * as path from 'path';
 
+const samples = path.join(__dirname, '..', '..', 'Samples');
+
 describe('Validate the iterateWordsFromFile', () => {
     test('streamWordsFromFile: hunspell', async () => {
-        const reader = await streamWordsFromFile(path.join(__dirname, '..', '..', 'Samples', 'hunspell', 'example.aff'), {});
+        const reader = await streamWordsFromFile(path.join(samples, 'hunspell', 'example.aff'), {});
         const results = [...reader];
         // this might break if the processing order of hunspell changes.
-        expect(results.join(' ')).to.equal('hello tried try rework reworked work worked');
+        expect(results.join(' ')).toBe('hello tried try rework reworked work worked');
+    });
+
+    test('stream words from trie', async () => {
+        const reader = await streamWordsFromFile(path.join(samples, 'cities.trie.gz'), {});
+        const results = [...reader];
+        expect(results.join('|')).toBe('amsterdam|angeles|city|delhi|francisco|london|los|los angeles'
+            + '|mexico|mexico city|new|new amsterdam|new delhi|new york|paris|san|san francisco|york');
     });
 });
