a7c139ffd79fe0413d16f434b85d8de1ba9a3a24
Jason Dent <jason@streetsidesoftware.nl>
2019-12-11 23:46:37 +0100

    Remove id from Trie2

--- packages/cspell-trie2-lib/src/lib/trie2Helper.ts
+++ packages/cspell-trie2-lib/src/lib/trie2Helper.ts
@@ -1,12 +1,12 @@
 import { Trie2, isTrieNode2Branch } from './trie2';
-import { TrieNode2 } from './TrieNode2';
+import { TrieNode2, END_OF_WORD } from './TrieNode2';
 
 export function displayTrie2(trie: Trie2, sort = false, dot = '.') {
     function *walk(nodes: TrieNode2[], prefix: string): Generator<string> {
         const i = sort ? [...nodes].sort((a, b) => a.s.localeCompare(b.s)) : nodes;
         const dots = dot.repeat(prefix.length);
         for (const n of i) {
-            if (!n.s) {
+            if (n.s === END_OF_WORD) {
                 yield prefix;
             }
             if (isTrieNode2Branch(n)) {
