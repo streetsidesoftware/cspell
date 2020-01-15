442a9272393a90b7c342ad4b7aad4ff4e09b927c
Jason Dent <jason@streetsidesoftware.nl>
2020-01-15 17:06:16 +0100

    Improve hinting

--- packages/cspell-trie-lib/src/lib/walker.ts
+++ packages/cspell-trie-lib/src/lib/walker.ts
@@ -40,7 +40,6 @@ export interface WalkerIterator extends Generator<YieldResult, any, boolean | un
 export function* walker(
     root: TrieNode,
     compoundingMethod: CompoundWordsMethod = CompoundWordsMethod.NONE,
-
 ): WalkerIterator {
 
     const roots: { [index: number]: ChildMap | [string, TrieNode][] } = {
@@ -48,7 +47,6 @@ export function* walker(
         [CompoundWordsMethod.JOIN_WORDS]: [[JOIN_SEPARATOR, root]],
         [CompoundWordsMethod.SEPARATE_WORDS]: [[WORD_SEPARATOR, root]],
     };
-
 
     function* children(n: TrieNode): IterableIterator<[string, TrieNode]> {
         if (n.c) {
@@ -113,43 +111,57 @@ export function* hintedWalker(
 
     const compoundCharacter = root.compoundCharacter;
 
-    function* children(n: TrieNode, depth: number): IterableIterator<[string, TrieNode]> {
+    interface StackItemEntry {
+        letter: string;
+        node: TrieNode;
+        hintOffset: number;
+    }
+
+    type StackItem = IterableIterator<StackItemEntry>;
+    type Stack = StackItem[];
+
+    function* children(n: TrieNode, hintOffset: number): StackItem {
         if (n.c) {
-            const h = hint.slice(depth, depth + 3) + hint.slice(Math.max(0, depth - 2), depth);
+            const h = hint.slice(hintOffset, hintOffset + 3) + hint.slice(Math.max(0, hintOffset - 2), hintOffset);
             const hints = new Set<string>(h);
 
             // First yield the hints
-            yield* [...hints].filter(a => n.c!.has(a)).map(a => [a, n.c!.get(a)!] as [string, TrieNode]);
+            yield* [...hints]
+                .filter(a => n.c!.has(a))
+                .map(letter => ({ letter, node: n.c!.get(letter)!, hintOffset: hintOffset + 1}));
             // We don't want to suggest the compound character.
             hints.add(compoundCharacter);
             // Then yield everything else.
-            yield* [...n.c].filter(a => !hints.has(a[0]));
+            yield* [...n.c]
+                .filter(a => !hints.has(a[0]))
+                .map(([letter, node]) => ({ letter, node, hintOffset: hintOffset + 1 }));
             if (n.c.has(compoundCharacter)) {
                 const compoundRoot = root.c.get(compoundCharacter);
                 if (compoundRoot) {
-                    yield *children(compoundRoot, depth);
+                    yield *children(compoundRoot, hintOffset);
                 }
             }
         }
         if (n.f) {
-            yield* roots[compoundingMethod];
+            yield* [...roots[compoundingMethod]]
+                .map(([letter, node]) => ({ letter, node, hintOffset }));
         }
     }
 
     let depth = 0;
-    const stack: Iterator<[string, TrieNode]>[] = [];
+    const stack: Stack = [];
     let baseText = '';
     stack[depth] = children(baseRoot, depth);
-    let ir: IteratorResult<[string, TrieNode]>;
+    let ir: IteratorResult<StackItemEntry, StackItemEntry>;
     while (depth >= 0) {
         while (!(ir = stack[depth].next()).done) {
-            const [char, node] = ir.value;
+            const { letter: char, node, hintOffset } = ir.value;
             const text = baseText + char;
             const hinting = (yield { text, node, depth }) as Hinting;
             if (hinting && hinting.goDeeper) {
                 depth++;
                 baseText = text;
-                stack[depth] = children(node, depth);
+                stack[depth] = children(node, hintOffset);
             }
         }
         depth -= 1;
