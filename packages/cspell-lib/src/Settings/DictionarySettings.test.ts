cd6a142ca2005f89336e15a07079a85df242b360
Jason Dent <Jason3S@users.noreply.github.com>
2020-02-23 12:22:13 +0100

    fix: Fix incompatible schema definition (#195)

--- packages/cspell-lib/src/Settings/DictionarySettings.test.ts
+++ packages/cspell-lib/src/Settings/DictionarySettings.test.ts
@@ -4,7 +4,6 @@ import * as fsp from 'fs-extra';
 import * as path from 'path';
 import * as os from 'os';
 import { getDefaultSettings } from './DefaultSettings';
-import { DictionaryDefinitionLegacy } from './CSpellSettingsDef';
 
 const defaultSettings = getDefaultSettings();
 
@@ -55,7 +54,7 @@ describe('Validate DictionarySettings', () => {
         expect(defs.length).to.be.equal(dictionaryDefinitions!.length);
 
         const basePath = path.join('some', 'dir', 'words.txt');
-        const legacyDictionaryDefinitions = (dictionaryDefinitions || []).map(a => ({...a}) as DictionaryDefinitionLegacy);
+        const legacyDictionaryDefinitions = (dictionaryDefinitions || []).map(a => ({...a}));
         legacyDictionaryDefinitions[0].path = path.join('~', basePath);
         legacyDictionaryDefinitions[0].file = '';
         const tildeDefs = DictSettings.normalizePathForDictDefs(legacyDictionaryDefinitions!, '.');
