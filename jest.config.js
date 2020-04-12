829c9131820b54da923d7b90af9630d352237f29
Jason Dent <Jason3S@users.noreply.github.com>
2020-04-12 13:36:01 +0200

    ci: update package-lock.json and coverage (#214)
    * re-install lerna@3.20.2
    * npm audit fix
    * Update codecov and coveralls
    * fix code coverage reporting

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "browser": false,
+    "roots": [
+        "./src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec|perf))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "coverageReporters": ["html", "json", ["lcov", {"projectRoot": "../../"}], "text"],
+    "collectCoverageFrom": [
+        "src/**/*.ts",
+        "!**/*.test.helper.ts",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ],
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
+        "\\.(css|less)$": "<rootDir>/src/__mocks__/styleMock.js"
+    }
+}
