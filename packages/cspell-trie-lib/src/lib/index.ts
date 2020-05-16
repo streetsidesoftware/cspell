a62e5cb334da51cc3c574e887b08dca4c390aa1e
Jason Dent <jason@streetsidesoftware.nl>
2020-05-16 16:28:45 +0200

    fix: node 14 and update dependencies

--- packages/cspell-trie-lib/src/lib/index.ts
+++ packages/cspell-trie-lib/src/lib/index.ts
@@ -5,4 +5,4 @@ export * from './walker';
 export * from './importExport';
 export * from './TrieBuilder';
 export * from './consolidate';
-export {SuggestionResult, MaxCost, suggestionCollector, SuggestionCollector, CompoundWordsMethod} from './suggest';
+export {SuggestionResult, MaxCost, suggestionCollector, SuggestionCollector} from './suggest';
