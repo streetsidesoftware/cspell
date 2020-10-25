70213a06363f3d8839a393d1c32170da0cf2ab5a
Jason Dent <Jason3S@users.noreply.github.com>
2020-10-25 11:34:02 +0100

    ci: Integration Test Framework (#431)
    * ci: integration tests
    
    * Add some commands
    
    * Add more tests
    
    * Fix adding repos.
    
    * preserve args if added again.
    
    * add typescript and jest
    
    * Update .gitmodules
    
    * Add back licia
    
    * Add more integrations
    
    * Add more integrations
    
    * More integrations
    
    * Add snapshot support
    
    * Fix cspell.json linter error.
    
    * Update settings.json

---
+++ integration-tests/src/config.ts
@@ -0,0 +1,37 @@
+import { Config, Repository } from './configDef';
+import * as fs from 'fs';
+import * as Path from 'path';
+
+const configFile = Path.join(__dirname, '..', 'config', 'config.json');
+
+const defaultConfig: Config = {
+    repositories: [],
+}
+
+export function readConfig(): Config {
+    try {
+        const file = fs.readFileSync(configFile, 'utf-8');
+        return JSON.parse(file);
+    } catch (e) {
+        return JSON.parse(JSON.stringify(defaultConfig));
+    }
+}
+
+export function writeConfig(config: Config) {
+    fs.writeFileSync(configFile, JSON.stringify(config, undefined, 2))
+}
+
+export function addRepository(path: string, url: string) {
+    const config = readConfig();
+    const entries = new Map<string, Repository>(config.repositories.map(r => [r.path, r]));
+    const args = entries.get(path)?.args || ['**/*.*', '*.*'];
+    const entry: Repository = {
+        path,
+        url,
+        args
+    }
+
+    entries.set(path, entry);
+    config.repositories = [...entries.values()];
+    writeConfig(config);
+}
