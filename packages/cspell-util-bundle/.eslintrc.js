e25f3f369c8a6233900375a805d27e278c3ba532
Nick Schonning <nschonni@gmail.com>
2020-10-31 13:51:12 -0400

    chore: Convert TSLint to ESLint

---
+++ packages/cspell-util-bundle/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
