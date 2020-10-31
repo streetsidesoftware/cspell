5fbe0d7e4b218fc592ba07e60b9bb8c6e91bad2f
Nick Schonning <nschonni@gmail.com>
2020-10-31 14:10:56 -0400

    chore: Expand out ESLint glob
    - Auto-fix JS files
    - Ignore the dist folder's generated JS files

--- packages/cspell-util-bundle/.eslintrc.js
+++ packages/cspell-util-bundle/.eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-    "env": {
-        "es2020": true,
-        "node": true
+    env: {
+        es2020: true,
+        node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    plugins: ['@typescript-eslint'],
+    rules: {},
 };
