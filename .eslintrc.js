9f3893d0f49176ba9168c165bfa69db35dc72209
Jason Dent <jason@streetsidesoftware.nl>
2020-11-07 19:48:28 +0100

    ci: run eslint from project root

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,27 @@ const config = {
         es2020: true,
         node: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    extends: [ 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended' ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 11,
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint'],
+    plugins: [ '@typescript-eslint' ],
     rules: {
-        'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'no-unused-vars': [ 'error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' } ],
+        '@typescript-eslint/no-unused-vars': [ 'warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' } ],
     },
+    ignorePatterns: [
+        '.eslintrc.js',
+        'packages/*/dist',
+        'packages/*/temp',
+        'packages/*/Temp',
+        '**/samples',
+        '**/Samples',
+        'integration-tests/repositories',
+        'integration-tests/dist',
+    ],
     overrides: [
         {
             files: '**/*.test.{ts,js}',
