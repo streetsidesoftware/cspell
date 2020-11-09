470970faef0a9147e17cbb1e00caa8a8bcad62c7
Jason Dent <jason@streetsidesoftware.nl>
2020-11-09 14:25:08 +0100

    ci: add linter files

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/**
+ * @type { import("eslint").Linter.Config }
+ */
+const config = {
+    env: {
+        es2020: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    overrides: [
+        {
+            files: '**/*.ts',
+            extends: 'plugin:@typescript-eslint/recommended',
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint'],
+            rules: {
+                'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+                '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            },
+        },
+        {
+            files: '**/*.test.{ts,js}',
+            extends: 'plugin:jest/recommended',
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
+
+module.exports = config;
