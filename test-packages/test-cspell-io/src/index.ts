27e9348dedfb67827223bef8176536a55eb9d191
Jason Dent <jason@streetsidesoftware.nl>
2020-11-12 12:30:01 +0100

    Clean up tests

--- test-packages/test-cspell-io/src/index.ts
+++ test-packages/test-cspell-io/src/index.ts
@@ -1,8 +1,9 @@
 import { assert } from 'console';
-import * as lib from 'cspell-lib';
+import * as io from 'cspell-io';
 
 /**
  * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
  */
-const functions = [lib.asyncIterableToArray, lib.calcOverrideSettings];
+const functions = [io.asyncIterableToArray, io.lineReaderAsync, io.readFile];
+
 functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
