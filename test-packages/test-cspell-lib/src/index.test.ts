2c8d66bff90e1c2ef235b2ba8f9df7c9c073775b
Jason Dent <jason@streetsidesoftware.nl>
2020-11-12 09:53:46 +0100

    Move test packages under test-packages

---
+++ test-packages/test-cspell-lib/src/index.test.ts
@@ -0,0 +1,10 @@
+import * as lib from 'cspell-lib';
+
+/**
+ * The main goal here is to make sure it compiles. The unit tests are just place holders.
+ */
+describe('Verify we can import cspell-io', () => {
+    test('it exports functions', () => {
+        expect(typeof lib.checkText).toBe('function');
+    });
+});
