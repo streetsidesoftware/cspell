f457bebf78dbcef82c3130b544f070f9ff616225
Jason Dent <jason@streetsidesoftware.nl>
2020-11-12 15:29:05 +0100

    Update tests in test-packages

--- test-packages/test-cspell-lib/src/index.ts
+++ test-packages/test-cspell-lib/src/index.ts
@@ -1,8 +1,12 @@
 import { assert } from 'console';
 import * as lib from 'cspell-lib';
 
+console.log('start');
+
 /**
  * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
  */
 const functions = [lib.asyncIterableToArray, lib.calcOverrideSettings];
 functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
+
+console.log('done');
