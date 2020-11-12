c37dfd2bbdf24191ef33ee2359c2f21b4fbc65e8
Jason Dent <jason@streetsidesoftware.nl>
2020-11-12 13:34:11 +0100

    Fix test-cspell-glob

---
+++ test-packages/test-cspell-glob/src/index.ts
@@ -0,0 +1,9 @@
+import { assert } from 'console';
+import * as glob from 'cspell-glob';
+
+/**
+ * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
+ */
+const functions = [glob.GlobMatcher];
+
+functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
