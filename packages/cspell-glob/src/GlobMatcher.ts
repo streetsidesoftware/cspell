1b3f932d964b3a1182587a708454eda37bd5087f
Jason Dent <Jason3S@users.noreply.github.com>
2020-11-12 08:03:14 +0100

    dev: remove dependency upon 'path' (#554)

--- packages/cspell-glob/src/GlobMatcher.ts
+++ packages/cspell-glob/src/GlobMatcher.ts
@@ -3,7 +3,35 @@ import * as path from 'path';
 
 // cspell:ignore fname
 
-export type PathInterface = typeof path.posix;
+export interface ParsedPath {
+    /**
+     * The root of the path such as '/' or 'c:\'
+     */
+    root: string;
+    /**
+     * The full directory path such as '/home/user/dir' or 'c:\path\dir'
+     */
+    dir: string;
+    /**
+     * The file name including extension (if any) such as 'index.html'
+     */
+    base: string;
+    /**
+     * The file extension (if any) such as '.html'
+     */
+    ext: string;
+    /**
+     * The file name without extension (if any) such as 'index'
+     */
+    name: string;
+}
+
+export interface PathInterface {
+    normalize(p: string): string;
+    join(...paths: string[]): string;
+    parse(p: string): ParsedPath;
+    sep: string;
+}
 
 export type GlobMatch = GlobMatchRule | GlobMatchNoRule;
 
