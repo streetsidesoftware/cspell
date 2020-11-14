0e5dd97b114b6e86c7225c5aa60c120591cf89eb
Nick Schonning <nschonni@gmail.com>
2020-11-14 18:44:08 -0500

    fix: Add back explicit cspell/application import

--- test-packages/test-cspell/src/index.ts
+++ test-packages/test-cspell/src/index.ts
@@ -1,13 +1,13 @@
 import { assert } from 'console';
-import * as cspell from 'cspell';
-import * as cli from 'cspell/dist/app';
+import { checkText, lint, trace } from 'cspell/dist/application';
+import { run } from 'cspell/dist/app';
 
 console.log('start');
 
 /**
  * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
  */
-const functions = [cspell.checkText, cspell.lint, cspell.trace, cli.run];
+const functions = [checkText, lint, trace, run];
 
 functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
 
