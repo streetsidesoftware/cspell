db34adfad6a5d65316faa8c347792c94889cfbaf
Jason Dent <jason@streetsidesoftware.nl>
2020-11-14 09:31:11 +0100

    ci: increase code coverage of cspell-trie

--- packages/cspell-trie/src/app.ts
+++ packages/cspell-trie/src/app.ts
@@ -14,67 +14,66 @@ const UTF8: BufferEncoding = 'utf8';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageInfo = require('../package.json');
 const version = packageInfo['version'];
-commander.version(version);
+export function run(program: commander.Command, argv: string[]): Promise<commander.Command> {
+    program.version(version);
 
-commander
-    .command('create <file.txt>')
-    .option('-o, --output <file>', 'output file - defaults to stdout')
-    .option('-l, --lower_case', 'output in lower case')
-    .option(
-        '-b, --base <number>',
-        'Use base n for reference ids.  Defaults to 32. Common values are 10, 16, 32. Max of 36'
-    )
-    .description('Generate a file for use with cspell')
-    .action(async (filename, options) => {
-        const { output: outputFile, lower_case: lowerCase = false, base = 32 } = options;
-        notify('Create Trie', !!outputFile);
-        const pOutputStream = createWriteStream(outputFile);
-        notify(`Generating...`, !!outputFile);
-        const lines = await fileToLines(filename);
-        const toLower = lowerCase ? (a: string) => a.toLowerCase() : (a: string) => a;
+    program
+        .command('create <file.txt>')
+        .option('-o, --output <file>', 'output file - defaults to stdout')
+        .option('-l, --lower_case', 'output in lower case')
+        .option(
+            '-b, --base <number>',
+            'Use base n for reference ids.  Defaults to 32. Common values are 10, 16, 32. Max of 36'
+        )
+        .description('Generate a file for use with cspell')
+        .action(async (filename, options) => {
+            const { output: outputFile, lower_case: lowerCase = false, base = 32 } = options;
+            notify('Create Trie', !!outputFile);
+            const pOutputStream = createWriteStream(outputFile);
+            notify(`Generating...`, !!outputFile);
+            const lines = await fileToLines(filename);
+            const toLower = lowerCase ? (a: string) => a.toLowerCase() : (a: string) => a;
 
-        const wordsRx = lines
-            .map(toLower)
-            .map((a) => a.trim())
-            .filter((a) => !!a);
+            const wordsRx = lines
+                .map(toLower)
+                .map((a) => a.trim())
+                .filter((a) => !!a);
 
-        notify('Processing Trie');
-        const root = wordsRx.reduce(
-            (node: Trie.TrieNode, word: string) => Trie.insert(word, node),
-            {} as Trie.TrieNode
-        );
+            notify('Processing Trie');
+            const trie = Trie.buildTrie(wordsRx);
 
-        notify('Export Trie');
-        const serialStream = iterableToStream(Trie.serializeTrie(root, base - 0 || 32));
-        const outputStream = await pOutputStream;
-        return new Promise((resolve) => {
-            serialStream.pipe(outputStream).on('finish', () => resolve());
+            notify('Export Trie');
+            const serialStream = iterableToStream(Trie.serializeTrie(trie.root, base - 0 || 32));
+            const outputStream = await pOutputStream;
+            return new Promise((resolve) => {
+                serialStream.pipe(outputStream).on('finish', () => resolve());
+            });
         });
-    });
 
-commander
-    .command('reader <file.trie>')
-    .option('-o, --output <file>', 'output file - defaults to stdout')
-    .description('Read a cspell trie file and output the list of words.')
-    .action(async (filename, options) => {
-        const { output: outputFile } = options;
-        notify('Reading Trie', !!outputFile);
-        const pOutputStream = createWriteStream(outputFile);
-        const lines = await fileToLines(filename);
-        const root = Trie.importTrie(lines);
-        const words = Trie.iteratorTrieWords(root);
-        const outputStream = await pOutputStream;
-        return new Promise((resolve) => {
-            iterableToStream(words.map((a) => a + '\n'))
-                .pipe(outputStream)
-                .on('finish', () => resolve());
+    program
+        .command('reader <file.trie>')
+        .option('-o, --output <file>', 'output file - defaults to stdout')
+        .description('Read a cspell trie file and output the list of words.')
+        .action(async (filename, options) => {
+            const { output: outputFile } = options;
+            notify('Reading Trie', !!outputFile);
+            const pOutputStream = createWriteStream(outputFile);
+            const lines = await fileToLines(filename);
+            const root = Trie.importTrie(lines);
+            const words: Sequence<string> = Trie.iteratorTrieWords(root);
+            const outputStream = await pOutputStream;
+            return new Promise((resolve) => {
+                iterableToStream(words.map((a) => a + '\n'))
+                    .pipe(outputStream)
+                    .on('finish', () => resolve());
+            });
         });
-    });
 
-commander.parse(process.argv);
-
-if (!commander.args.length) {
-    commander.help();
+    try {
+        return program.parseAsync(argv);
+    } catch (e) {
+        return Promise.reject(e);
+    }
 }
 
 async function fileToLines(filename: string): Promise<Sequence<string>> {
@@ -89,6 +88,7 @@ function createWriteStream(filename?: string): Promise<NodeJS.WritableStream> {
         : mkdirp(path.dirname(filename)).then(() => fs.createWriteStream(filename));
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function notify(message: any, useStdOut = true) {
     if (useStdOut) {
         console.log(message);
