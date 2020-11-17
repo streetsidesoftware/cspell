b0de78e0fd1f4531f2f1a874fb55f424cfacc857
Jason Dent <Jason3S@users.noreply.github.com>
2020-11-17 17:54:03 +0100

    fix: improve file resolution (#601)

---
+++ packages/cspell-lib/src/util/resolveFile.test.ts
@@ -0,0 +1,55 @@
+import { resolveFile } from './resolveFile';
+import * as path from 'path';
+import { parse } from 'comment-json';
+import * as fs from 'fs';
+
+interface Config {
+    import: string[];
+}
+
+const config: Config = parse(
+    fs.readFileSync(path.join(__dirname, '..', '..', 'config', 'cspell-default.json'), 'utf-8')
+);
+
+const ext = path.extname(__filename);
+
+describe('Validate resolveFile', () => {
+    interface ResolveFileTest {
+        filename: string;
+        relativeTo: string;
+        expected: string;
+        found: boolean;
+    }
+    test.each`
+        filename                                | relativeTo   | expected                                              | found
+        ${__filename}                           | ${__dirname} | ${__filename}                                         | ${true}
+        ${path.relative(__dirname, __filename)} | ${__dirname} | ${__filename}                                         | ${true}
+        ${'cspell-dict-cpp/cspell-ext.json'}    | ${__dirname} | ${require.resolve('cspell-dict-cpp/cspell-ext.json')} | ${true}
+        ${'cspell-ext.json'}                    | ${__dirname} | ${'cspell-ext.json'}                                  | ${false}
+        ${`./resolveFile${ext}`}                | ${__dirname} | ${require.resolve('./resolveFile')}                   | ${true}
+        ${`resolveFile${ext}`}                  | ${__dirname} | ${require.resolve('./resolveFile')}                   | ${true}
+        ${'lerna'}                              | ${__dirname} | ${require.resolve('lerna')}                           | ${true}
+    `('resolveFile "$filename" rel "$relativeTo"', validateResolveFile);
+
+    test.each(
+        config.import
+            .concat(config.import.map((f) => f.replace('node_modules/', '')))
+            .map((f) => ({
+                filename: f,
+                relativeTo: __dirname,
+                expected: require.resolve(f.replace('node_modules/', '')),
+                found: true,
+            }))
+            .map(({ filename, relativeTo, expected, found }) => [filename, relativeTo, expected, found])
+    )('resolveFile "%s" rel "%s"', validateResolveArgs);
+
+    function validateResolveFile({ filename, relativeTo, expected, found }: ResolveFileTest) {
+        validateResolveArgs(filename, relativeTo, expected, found);
+    }
+
+    function validateResolveArgs(filename: string, relativeTo: string, expected: string, found: boolean) {
+        const r = resolveFile(filename, relativeTo);
+        expect(r.filename).toBe(expected);
+        expect(r.found).toBe(found);
+    }
+});
