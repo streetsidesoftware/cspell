f5e9e8260203fb7106ccb9d1e427c59610f1dbd3
Jason Dent <jason@streetsidesoftware.nl>
2021-01-23 12:12:35 +0100

    fix: #880

--- packages/cspell/src/app.ts
+++ packages/cspell/src/app.ts
@@ -426,7 +426,7 @@ function template(s: string): TemplateStringsArray {
 }
 
 export class CheckFailed extends Error {
-    constructor(message: string, readonly errorCode: number) {
+    constructor(message: string, readonly exitCode: number = 1) {
         super(message);
     }
 }
