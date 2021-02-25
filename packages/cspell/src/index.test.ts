c625479be185f287e297a1dcddbcfa2aa24b0d0d
Jason Dent <Jason3S@users.noreply.github.com>
2021-02-25 15:48:47 +0100

    fix: make sure to export all needed cspell types. (#1006)
    * ci: spell check `.github` as well.
    
    * Update cspellrc.json
    
    * Update cspell.yml
    
    * Update cspell-dict.txt
    
    * Update test.yml
    
    * Update cspell.json
    
    * fix: make sure the types are exported.
    
    * dev: add some tests
    
    * testing: update cspell integration test

--- packages/cspell/src/index.test.ts
+++ packages/cspell/src/index.test.ts
@@ -1,7 +1,62 @@
 import * as index from './index';
 
+// Make sure the types are exported.
+import { CSpellApplicationOptions, RunResult, Emitters, Issue, ProgressFileComplete } from './index';
+
 describe('Validate index.ts', () => {
     test('index', () => {
         expect(index).toBeDefined();
     });
+
+    test('quick run', async () => {
+        const appOptions: CSpellApplicationOptions = {};
+        const logger = new Logger();
+
+        const result: RunResult = await index.lint(['*.md'], appOptions, logger);
+
+        expect(result).toEqual(
+            expect.objectContaining({
+                errors: 0,
+                issues: 0,
+            })
+        );
+    });
 });
+
+class Logger implements Emitters {
+    log: string[] = [];
+    issueCount = 0;
+    errorCount = 0;
+    debugCount = 0;
+    infoCount = 0;
+    progressCount = 0;
+    issues: Issue[] = [];
+
+    issue = (issue: Issue) => {
+        this.issues.push(issue);
+        this.issueCount += 1;
+        const { uri, row, col, text } = issue;
+        this.log.push(`Issue: ${uri}[${row}, ${col}]: Unknown word: ${text}`);
+    };
+
+    error = (message: string, error: Error) => {
+        this.errorCount += 1;
+        this.log.push(`Error: ${message} ${error.toString()}`);
+        return Promise.resolve();
+    };
+
+    info = (message: string) => {
+        this.infoCount += 1;
+        this.log.push(`Info: ${message}`);
+    };
+
+    debug = (message: string) => {
+        this.debugCount += 1;
+        this.log.push(`Debug: ${message}`);
+    };
+
+    progress = (p: ProgressFileComplete) => {
+        this.progressCount += 1;
+        this.log.push(`Progress: ${p.type} ${p.fileNum} ${p.fileCount} ${p.filename}`);
+    };
+}
--- packages/cspell/src/index.ts
+++ packages/cspell/src/index.ts
@@ -1,2 +1,4 @@
 export * from './application';
 export * from './emitters';
+export { BaseOptions, CSpellApplicationOptions, TraceOptions } from './options';
+export { RunResult } from './lint';
