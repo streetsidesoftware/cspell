fae0a8e50a4f6f94227205a6aaa51c09a21bad14
Jason Dent <jason@streetsidesoftware.nl>
2021-02-25 15:40:25 +0100

    testing: update cspell integration test

--- test-packages/test-cspell/src/index.ts
+++ test-packages/test-cspell/src/index.ts
@@ -1,14 +1,75 @@
 import { assert } from 'console';
-import { checkText, lint, trace } from 'cspell/dist/application';
+import {
+    checkText,
+    lint,
+    trace,
+    Emitters,
+    Issue,
+    ProgressFileComplete,
+    CSpellApplicationOptions,
+    RunResult,
+} from 'cspell';
 import { run } from 'cspell/dist/app';
 
-console.log('start');
+async function test() {
+    console.log('start');
 
-/**
- * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
- */
-const functions = [checkText, lint, trace, run];
+    /**
+     * The main goal here is to make sure it compiles. The unit tests are validation that it compiled as expected.
+     */
+    const functions = [checkText, lint, trace, run];
 
-functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
+    functions.forEach((fn) => assert(typeof fn === 'function', "typeof %o === 'function'", fn));
 
-console.log('done');
+    const logger = new ConsoleLogger();
+
+    const options: CSpellApplicationOptions = {};
+
+    const result: RunResult = await lint(['*.md'], options, logger);
+    assert(result.errors === 0);
+    assert(result.issues === 0);
+    assert(result.files === 2);
+
+    console.log(JSON.stringify(result));
+
+    console.log('done');
+}
+
+class ConsoleLogger implements Emitters {
+    log: string[] = [];
+    issueCount = 0;
+    errorCount = 0;
+    debugCount = 0;
+    infoCount = 0;
+    progressCount = 0;
+    issues: Issue[] = [];
+
+    issue = (issue: Issue) => {
+        this.issueCount += 1;
+        const { uri, row, col, text } = issue;
+        console.log(`Issue: ${uri}[${row}, ${col}]: Unknown word: ${text}`);
+    };
+
+    error = (message: string, error: Error) => {
+        this.errorCount += 1;
+        console.error(`Error: ${message} ${error.toString()}`);
+        return Promise.resolve();
+    };
+
+    info = (_message: string) => {
+        this.infoCount += 1;
+        // console.info(`Info: ${message}`);
+    };
+
+    debug = (_message: string) => {
+        this.debugCount += 1;
+        // console.debug(`Debug: ${message}`);
+    };
+
+    progress = (p: ProgressFileComplete) => {
+        this.progressCount += 1;
+        console.error(`Progress: ${p.type} ${p.fileNum} ${p.fileCount} ${p.filename}`);
+    };
+}
+
+test();
