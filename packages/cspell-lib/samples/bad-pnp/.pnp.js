01ae176ce4a23bd3714cfd6c95f0157eb0d48fe7
Jason Dent <jason@streetsidesoftware.nl>
2021-06-02 21:15:03 +0200

    feat: support `.pnp.js` when loading configurations.

---
+++ packages/cspell-lib/samples/bad-pnp/.pnp.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const pnp = {};
+
+module.exports = pnp;
---
+++ packages/cspell-lib/src/Settings/pnpLoader.ts
@@ -0,0 +1,119 @@
+/**
+ * Handles loading of `.pnp.js` and `.pnp.js` files.
+ */
+import { URI } from 'vscode-uri';
+import importFresh from 'import-fresh';
+import findUp from 'find-up';
+import clearModule from 'clear-module';
+
+const pnpFiles = ['.pnp.cjs', '.pnp.js'];
+
+const supportedSchemas = new Set(['file']);
+
+type LoaderResult = URI | undefined;
+
+const cachedPnpImports = new Map<string, Promise<LoaderResult>>();
+const cachedRequests = new Map<string, Promise<LoaderResult>>();
+let lock: Promise<undefined> | undefined = undefined;
+
+export class PnpLoader {
+    /**
+     * Request that the nearest .pnp file gets loaded
+     * @param uriDirectory starting directory
+     * @returns promise - rejects on error - success if loaded or not found.
+     */
+    public async load(uriDirectory: URI): Promise<LoaderResult> {
+        await lock;
+        const cacheKey = uriDirectory.toString();
+        const cached = cachedRequests.get(cacheKey);
+        if (cached) return cached;
+
+        const r = findPnpAndLoad(uriDirectory);
+        cachedRequests.set(cacheKey, r);
+        return r;
+    }
+
+    public async peek(uriDirectory: URI): Promise<LoaderResult> {
+        await lock;
+        return cachedRequests.get(uriDirectory.toString()) ?? Promise.resolve(undefined);
+    }
+
+    /**
+     * Clears the cached so .pnp files will get reloaded on request.
+     */
+    public clearCache(): void {
+        cleanCache();
+    }
+}
+
+export function pnpLoader(): PnpLoader {
+    return new PnpLoader();
+}
+
+export class UnsupportedSchema extends Error {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
+
+export class UnsupportedPnpFile extends Error {
+    constructor(msg: string) {
+        super(msg);
+    }
+}
+
+/**
+ * @param uriDirectory - directory to start at.
+ */
+async function findPnpAndLoad(uriDirectory: URI): Promise<LoaderResult> {
+    validateSchema(uriDirectory);
+    const found = await findUp(pnpFiles, { cwd: uriDirectory.fsPath });
+    if (!found) return;
+    const c = cachedPnpImports.get(found);
+    if (c) return c;
+
+    const r = loadPnp(found);
+    cachedPnpImports.set(found, r);
+    return r;
+}
+
+interface Pnp {
+    setup?: () => void;
+}
+
+async function loadPnp(pnpFile: string): Promise<LoaderResult> {
+    const pnp = importFresh<Pnp>(pnpFile);
+    if (pnp.setup) {
+        pnp.setup();
+        return URI.file(pnpFile);
+    }
+    throw new UnsupportedPnpFile(`Unsupported pnp file: "${pnpFile}"`);
+}
+
+function validateSchema(uri: URI): true | never {
+    if (!supportedSchemas.has(uri.scheme)) {
+        throw new UnsupportedSchema(`Unsupported schema for PNP: "${uri.scheme}"`);
+    }
+    return true;
+}
+
+function cleanCache(): Promise<undefined> {
+    if (lock) return lock;
+    lock = _cleanCache().finally(() => {
+        lock = undefined;
+    });
+    return lock;
+}
+
+async function _cleanCache(): Promise<undefined> {
+    await Promise.all([...cachedRequests.values()].map(rejectToUndefined));
+    const modules = await Promise.all([...cachedPnpImports.values()].map(rejectToUndefined));
+    modules.forEach((r) => r && clearModule.single(r.fsPath));
+    cachedRequests.clear();
+    cachedPnpImports.clear();
+    return undefined;
+}
+
+function rejectToUndefined<T>(p: Promise<T>): Promise<T | undefined> {
+    return p.catch(() => undefined);
+}
