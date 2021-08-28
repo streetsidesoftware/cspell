7d0e123311361f82734412dd4c05ae6aafe4486c
Jason Dent <jason@streetsidesoftware.nl>
2021-08-28 13:33:50 +0200

    test: Update yarn in test packages.

--- test-packages/yarn2/test-yarn2-med/cspell.config.js
+++ test-packages/yarn2/test-yarn2-med/cspell.config.js
@@ -1,6 +1,4 @@
 'use strict';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('./.pnp.js').setup();
 
 /** @type { import("@cspell/cspell-types").CSpellUserSettings } */
 const cspell = {
