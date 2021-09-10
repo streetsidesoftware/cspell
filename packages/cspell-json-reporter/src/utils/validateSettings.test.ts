d4c215827d0db9d034393f8f885f37ff46e3d501
mad-gooze <alex-gusev@yandex-team.ru>
2021-09-10 22:58:45 +0300

    simplify tests

--- packages/cspell-json-reporter/src/utils/validateSettings.test.ts
+++ packages/cspell-json-reporter/src/utils/validateSettings.test.ts
@@ -1,29 +1,24 @@
-import { CSpellJSONReporterSettings } from "../CSpellJSONReporterSettings";
 import { validateSettings } from "./validateSettings";
 
 describe('validateSettings', () => {
-    it('passes valid settings', () => {
-        const valid: CSpellJSONReporterSettings[] = [
-            { outFile: 'foobar' },
-            { outFile: 'foobar', verbose: true },
-            { outFile: 'foobar', debug: false },
-            { outFile: 'foobar', progress: true },
-            { outFile: 'foobar', verbose: undefined },
-        ];
-        valid.forEach((settings) => {
-            expect(validateSettings(settings)).toEqual(undefined);
-        });
+    test.each`
+        settings
+        ${{ outFile: 'foobar' }}
+        ${{ outFile: 'foobar', verbose: true }}
+        ${{ outFile: 'foobar', debug: false }}
+        ${{ outFile: 'foobar', progress: true }}
+        ${{ outFile: 'foobar', verbose: undefined }}
+    `('passes valid settings', ({ settings }) => {
+        expect(validateSettings(settings)).not.toThrow();
     });
 
-    it('throws for invalid settings', () => {
-        const invalid = [
-            { },
-            { outFile: 1 },
-            { outFile: 'foobar', verbose: 123 },
-            { outFile: 'foobar', debug: [] },
-        ];
-        invalid.forEach((settings) => {
-            expect(() => validateSettings(settings)).toThrowErrorMatchingSnapshot();
-        });
+    test.each`
+        settings
+        ${{ }}
+        ${{ outFile: 1 }}
+        ${{ outFile: 'foobar', verbose: 123 }}
+        ${{ outFile: 'foobar', debug: [] }}
+    `('throws for invalid settings', ({ settings }) => {
+        expect(() => validateSettings(settings)).toThrowErrorMatchingSnapshot();
     });
 });
