2a1e8f07b536cd7bad05b00a4acd376c240ce990
Jason Dent <jason@streetsidesoftware.nl>
2021-09-11 17:31:34 +0200

    feat: drop support for `lineReaderAsync` and related functions

---
+++ packages/cspell-io/src/async/asyncIterable.test.ts
@@ -0,0 +1,14 @@
+import { toArray } from './asyncIterable';
+
+describe('asyncIterable', () => {
+    test('toArray', async () => {
+        const r = toArray(emit([1, 2, 3, Promise.resolve(4), 5]));
+        await expect(r).resolves.toEqual([1, 2, 3, 4, 5]);
+    });
+});
+
+async function* emit<T>(values: Iterable<T | Promise<T>>): AsyncIterableIterator<T> {
+    for await (const v of values) {
+        yield v;
+    }
+}
