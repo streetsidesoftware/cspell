ed21038c6707bba339657a7271c6ba6c1b0c8e02
Jason Dent <jason@streetsidesoftware.nl>
2021-09-17 17:39:19 +0200

    Update timer.test.ts

--- packages/cspell-trie-lib/src/lib/timer.test.ts
+++ packages/cspell-trie-lib/src/lib/timer.test.ts
@@ -6,14 +6,14 @@ const delay = promisify(setTimeout);
 describe('timer', () => {
     test('createTimer', async () => {
         const t = createTimer();
-        await delay(11);
+        await delay(12);
         expect(t.elapsed()).toBeGreaterThan(10);
     });
 
     test('polyHrTime', async () => {
         const a = createTimer();
         const b = createTimer(polyHrTime);
-        await delay(11);
+        await delay(12);
         const a1 = a.elapsed();
         const b1 = b.elapsed();
         expect(a1).toBeGreaterThanOrEqual(10);
