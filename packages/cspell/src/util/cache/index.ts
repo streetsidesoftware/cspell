410b86d87ee7e4b868da8dfa1c7759b9fb4f74d5
Alexey D. Gusev <alex-gusev@yandex-team.ru>
2021-09-25 00:55:09 +0300

    add jsdoc

--- packages/cspell/src/util/cache/index.ts
+++ packages/cspell/src/util/cache/index.ts
@@ -8,6 +8,9 @@ import { FileCache } from './FileCache';
 // cspell:word cspellcache
 export const DEFAULT_CACHE_LOCATION = '.cspellcache';
 
+/**
+ * Creates CSpellLintResultCache (file cache if caching is enabled in config or dummy otherwise)
+ */
 export function createCache(cfg: CSpellApplicationConfiguration, configInfo: ConfigInfo): CSpellLintResultCache {
     const { cache, cacheLocation = DEFAULT_CACHE_LOCATION } = cfg.options;
     return cache ? new FileCache(path.resolve(cfg.root, cacheLocation), configInfo) : new DummyCache();
