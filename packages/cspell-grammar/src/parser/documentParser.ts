474ecb2cfa39de9bc666660c10f67cbbe3c8a916
Jason Dent <jason@streetsidesoftware.nl>
2021-10-13 21:51:50 +0200

    Update documentParser.ts

--- packages/cspell-grammar/src/parser/documentParser.ts
+++ packages/cspell-grammar/src/parser/documentParser.ts
@@ -1,5 +1,7 @@
 // import { Grammar, Pattern, PatternMatch } from './grammarDefinition';
-// import { DocumentParser } from './parser';
+// import { Rule, NGrammar, NPattern, MatchResult, MatchingRule } from './grammarNormalized';
+// import { normalizeGrammar } from './grammarNormalizer';
+// import { DocumentParser, ParsedText, ParseLineResult } from './parser';
 
 // interface LineOffset {
 //     text: string;
@@ -7,14 +9,57 @@
 // }
 
 // class DocumentParserFromGrammar implements DocumentParser {
-//     constructor(readonly grammar: Grammar) {}
+//     constructor(readonly grammar: NGrammar) {}
 //     parse(line: string): ParseLineResult {}
 // }
 
 // export function createDocumentParser(grammar: Grammar): DocumentParser {
-//     return new DocumentParserFromGrammar(grammar);
+//     return new DocumentParserFromGrammar(normalizeGrammar(grammar));
 // }
 
-// function matchPattern(pattern: Pattern, line: LineOffset): RegExpExecArray | undefined {}
+// interface Stack {
+//     scope: string | undefined;
+//     parent: Stack | undefined;
+// }
 
-// function matchPatternMatch(p: PatternMatch, line: LineOffset): RegExpExecArray | undefined {}
+// export function parseLine(line: string, rule: Rule): ParseLineResult {
+//     const ln = { line, offset: 0 };
+
+//     let find: Rule | undefined = rule.find(ln, undefined);
+//     const root: Stack = {
+//         scope: grammar.scopeName,
+//         parent: undefined,
+//     };
+//     let stack = root;
+
+//     while (ln.offset < line.length && find) {
+//         pushText(find.match.index);
+//         find = pattern.find();
+//     }
+
+//     pushText(line.length);
+
+//     function pushText(index: number) {
+//         if (ln.offset < index) {
+//             parsedText.push({
+//                 scope: stackToScope(),
+//                 text: ln.line.slice(ln.offset, index),
+//                 offset: ln.offset,
+//             });
+//             ln.offset = index;
+//         }
+//     }
+
+//     function stackToScope(): string[] {
+//         const scope: string[] = [];
+//         for (let s: Stack | undefined = stack; s; s = s.parent) {
+//             s.scope && scope.push(s.scope);
+//         }
+//         return scope;
+//     }
+
+//     return {
+//         parsedText,
+//         parse: () => {},
+//     };
+// }
