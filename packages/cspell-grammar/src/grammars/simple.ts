95c232e45a4e879bb92377841c4beb9c2dcb18f6
Jason Dent <jason@streetsidesoftware.nl>
2021-10-16 21:16:31 +0200

    fix: Make sure pattern names match grammar.

--- packages/cspell-grammar/src/grammars/simple.ts
+++ packages/cspell-grammar/src/grammars/simple.ts
@@ -24,15 +24,62 @@ const repository: Repository = {
         patterns: ['$self'],
     },
     comment: {
-        patterns: [{ include: '#comment_line' }, { include: '#comment_block' }],
+        patterns: ['#comment_line', '#comment_doc_block', '#comment_block'],
     },
     comment_line: {
+        patterns: ['#comment_line_double_slash', '#comment_line_hash'],
+    },
+    comment_line_hash: {
         name: 'comment.line.s',
-        match: /#.*/,
+        match: /#.*\s?/,
+    },
+    comment_line_double_slash: {
+        name: 'comment.line.s',
+        match: /\/\/.*\s?/,
+    },
+    comment_doc_block: {
+        name: 'comment.block.s',
+        begin: '/**',
+        end: '*/',
     },
     comment_block: {
         name: 'comment.block.s',
-        disabled: true,
+        begin: '/*',
+        end: '*/',
+    },
+    string: {
+        patterns: [{ include: '#string_q_single' }, { include: '#string_q_double' }, { include: '#string_template' }],
+    },
+    string_q_single: {
+        name: 'string.quoted.single.s',
+        begin: "'",
+        end: /'|((?:[^\\\n])$)/,
+        patterns: [{ include: '#string_character_escape' }],
+    },
+    string_q_double: {
+        name: 'string.quoted.double.s',
+        begin: '"',
+        end: /"|((?:[^\\\n])$)/,
+        patterns: [{ include: '#string_character_escape' }],
+    },
+    string_template: {
+        name: 'string.template.s',
+        begin: '`',
+        end: '`',
+        patterns: [{ include: '#string_character_escape' }, '#template_embedded'],
+    },
+    string_wrap: {
+        match: /(?:[^\\\n])$/,
+    },
+    string_character_escape: {
+        name: 'constant.character.escape.s',
+        match: /\\(x[0-9A-Fa-f]{2}|[0-3][0-7]{0,2}|[4-7][0-7]?|.|$)/,
+    },
+    template_embedded: {
+        name: 'embedded.template.s',
+        begin: '${',
+        end: '}',
+        patterns: ['#statement'],
     },
 };
 
