836dde90f33681db20dadcd8f0de0dfd9426f128
Jason Dent <jason@streetsidesoftware.nl>
2021-12-28 18:17:41 +0100

    Update lint.test.ts

--- packages/cspell/src/lint/lint.test.ts
+++ packages/cspell/src/lint/lint.test.ts
@@ -24,23 +24,23 @@ describe('Linter Validation Tests', () => {
 
     // cspell:ignore Tufte
     test.each`
-        files               | options                                                                       | expectedRunResult              | expectedReport
-        ${[]}               | ${{ root: latexSamples }}                                                     | ${oc({ errors: 0, files: 4 })} | ${oc({ errorCount: 0, issues: [oc({ text: 'Tufte' })] })}
-        ${['**/ebook.tex']} | ${{ root: latexSamples }}                                                     | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/ebook.tex']} | ${{ root: latexSamples, gitignore: true }}                                    | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/hidden.md']} | ${{ root: hiddenSamples }}                                                    | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/hidden.md']} | ${{ root: hiddenSamples, dot: true }}                                         | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/*.md']}      | ${{ root: hiddenSamples, dot: false }}                                        | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/*.md']}      | ${{ root: hiddenSamples }}                                                    | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/*.md']}      | ${{ root: hiddenSamples, dot: true }}                                         | ${oc({ errors: 0, files: 2 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**']}           | ${{ root: samples, config: j(samples, 'cspell-not-found.json') }}             | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
-        ${['**']}           | ${{ root: samples, config: j(samples, 'linked/cspell-import-missing.json') }} | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
-        ${['**/ebook.tex']} | ${{ root: samples, config: j(samples, 'cspell-missing-dict.json') }}          | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, errors: [], issues: [] })}
-        ${['**/ebook.tex']} | ${{ root: samples, config: j(samples, 'linked/cspell-import.json') }}         | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${[]}               | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck] }}        | ${oc({ errors: 0, files: 2 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/*.md']}      | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck] }}        | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${['**/*.ts']}      | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck] }}        | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
-        ${[]}               | ${{ root, config: j(root, 'cspell.json'), fileLists: ['missing-file.txt'] }}  | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
+        files               | options                                                                           | expectedRunResult              | expectedReport
+        ${[]}               | ${{ root: latexSamples }}                                                         | ${oc({ errors: 0, files: 4 })} | ${oc({ errorCount: 0, issues: [oc({ text: 'Tufte' })] })}
+        ${['**/ebook.tex']} | ${{ root: latexSamples }}                                                         | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/ebook.tex']} | ${{ root: latexSamples, gitignore: true }}                                        | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/hidden.md']} | ${{ root: hiddenSamples }}                                                        | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/hidden.md']} | ${{ root: hiddenSamples, dot: true }}                                             | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/*.md']}      | ${{ root: hiddenSamples, dot: false }}                                            | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/*.md']}      | ${{ root: hiddenSamples }}                                                        | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/*.md']}      | ${{ root: hiddenSamples, dot: true }}                                             | ${oc({ errors: 0, files: 2 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**']}           | ${{ root: samples, config: j(samples, 'cspell-not-found.json') }}                 | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
+        ${['**']}           | ${{ root: samples, config: j(samples, 'linked/cspell-import-missing.json') }}     | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
+        ${['**/ebook.tex']} | ${{ root: samples, config: j(samples, 'cspell-missing-dict.json') }}              | ${oc({ errors: 0, files: 0 })} | ${oc({ errorCount: 0, errors: [], issues: [] })}
+        ${['**/ebook.tex']} | ${{ root: samples, config: j(samples, 'linked/cspell-import.json') }}             | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${[]}               | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck], dot: true }} | ${oc({ errors: 0, files: 2 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/*.md']}      | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck] }}            | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${['**/*.ts']}      | ${{ root, config: j(root, 'cspell.json'), fileLists: [filesToCheck] }}            | ${oc({ errors: 0, files: 1 })} | ${oc({ errorCount: 0, issues: [] })}
+        ${[]}               | ${{ root, config: j(root, 'cspell.json'), fileLists: ['missing-file.txt'] }}      | ${oc({ errors: 1, files: 0 })} | ${oc({ errorCount: 1, errors: [expect.any(Error)], issues: [] })}
     `('runLint $files $options', async ({ files, options, expectedRunResult, expectedReport }) => {
         const reporter = new InMemoryReporter();
         const runResult = await runLint(new LintRequest(files, options, reporter));
