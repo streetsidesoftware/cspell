c445e4ffccbcedb807bf8df82c36e46dac3499d9
Jason Dent <jason@streetsidesoftware.nl>
2022-01-07 15:39:13 +0100

    fix: Fix type for some older TypeScript parsers.

--- packages/cspell-types/src/index.ts
+++ packages/cspell-types/src/index.ts
@@ -1,4 +1,6 @@
-export { ConfigFields, type CSpellUserSettingsFields } from './configFields';
+// Using old-style of `type` exports because the new style breaks some integrations.
+export { ConfigFields } from './configFields';
+export type { CSpellUserSettingsFields } from './configFields';
 export * from './CSpellReporter';
 export * from './CSpellSettingsDef';
 export type { TextDocumentOffset, TextOffset } from './TextOffset';
