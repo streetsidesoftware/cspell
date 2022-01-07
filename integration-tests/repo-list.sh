7aa9c6e1f777791f7a4cb4f5e2e5b3dddc6ec889
Jason Dent <jason@streetsidesoftware.nl>
2022-01-07 13:33:09 +0100

    fix spelling issue

--- integration-tests/repo-list.sh
+++ integration-tests/repo-list.sh
@@ -1,2 +1,4 @@
 # output the list of repositories sorted
 cat config/config.json | jq -r ".repositories | sort_by(.path | ascii_downcase) | .[].path  | select(startswith(\"streetside\") | not) | \"          - \" + ."
+
+# cspell:ignore downcase startswith streetside
