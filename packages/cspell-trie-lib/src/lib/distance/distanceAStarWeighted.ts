3fd60c52828b9bc1334e118a18d8f211adc71e26
Jason Dent <jason@streetsidesoftware.nl>
2022-01-17 18:04:46 +0100

    refactor: Weighted distance calculations.

--- packages/cspell-trie-lib/src/lib/distance/distanceAStarWeighted.ts
+++ packages/cspell-trie-lib/src/lib/distance/distanceAStarWeighted.ts
@@ -1,12 +1,12 @@
 import { PairingHeap } from '../utils/PairingHeap';
-import { WeightedMapTrie, WeightedRepMapTrie } from './weightedMaps';
+import { WeightMap } from './weightedMaps';
 
 /**
  * Calculate the edit distance between two words using an A* algorithm.
  *
  * Using basic weights, this algorithm has the same results as the Damerau-Levenshtein algorithm.
  */
-export function distanceAStarWeighted(a: string, b: string, map: WeightedMapTrie, cost = 100): number {
+export function distanceAStarWeighted(a: string, b: string, map: WeightMap, cost = 100): number {
     const aN = a.length;
     const bN = b.length;
 
@@ -45,92 +45,13 @@ export function distanceAStarWeighted(a: string, b: string, map: WeightedMapTrie
 
     function opMap(n: Node) {
         const { ai, bi, c } = n;
-
-        function ins(ai: number, bi: number, m: WeightedMapTrie | undefined) {
-            if (bi >= bN || !m) return;
-            const n = m[b[bi]];
-            if (!n) return;
-            const cost = n.insDel;
-            ++bi;
-            if (cost !== undefined) {
-                candidates.add({ ai, bi, c: c + cost });
+        const pos = { a, b, ai, bi, c };
+        const costCalculations = [map.calcInsDelCosts(pos), map.calcSwapCosts(pos), map.calcReplaceCosts(pos)];
+        costCalculations.forEach((iter) => {
+            for (const nn of iter) {
+                candidates.add(nn);
             }
-            ins(ai, bi, n.t);
-        }
-
-        function del(ai: number, bi: number, m: WeightedMapTrie | undefined) {
-            if (ai >= aN || !m) return;
-            const n = m[a[ai]];
-            if (!n) return;
-            ++ai;
-            const cost = n.insDel;
-            if (cost !== undefined) {
-                candidates.add({ ai, bi, c: c + cost });
-            }
-            del(ai, bi, n.t);
-        }
-
-        function repApply(ai: number, bi: number, m: WeightedRepMapTrie | undefined) {
-            if (!m || bi >= bN) return;
-            const char = b[bi];
-            const n = m[char];
-            if (!n) return;
-            ++bi;
-            const cost = n.rep;
-            if (cost !== undefined) {
-                candidates.add({ ai, bi, c: c + cost });
-            }
-            repApply(ai, bi, n.r);
-        }
-
-        function rep(ai: number, bi: number, m: WeightedMapTrie | undefined) {
-            if (!m || ai >= aN || bi >= bN) return;
-            const n = m[a[ai]];
-            if (!n) return;
-            ++ai;
-            repApply(ai, bi, n.r);
-            rep(ai, bi, n.t);
-        }
-
-        function swap(ai: number, bi: number, m: WeightedMapTrie | undefined) {
-            if (!m || ai >= aN || bi >= bN) return;
-
-            function apply(mid: number, right: number, cost: number | undefined) {
-                if (cost === undefined) return;
-                const swap = a.slice(mid, right) + a.slice(ai, mid);
-                const len = swap.length;
-
-                const subB = b.slice(bi, bi + len);
-                if (swap === subB) {
-                    candidates.add({ ai: ai + len, bi: bi + len, c: cost });
-                }
-            }
-
-            function right(aim: number, ail: number, m: WeightedRepMapTrie | undefined) {
-                if (!m || ail >= aN) return;
-                const n = m[a[ail]];
-                if (!n) return;
-                ++ail;
-                apply(aim, ail, n.swap);
-                right(aim, ail, n.r);
-            }
-
-            function left(aim: number, m: WeightedMapTrie | undefined) {
-                if (!m || aim >= aN) return;
-                const n = m[a[aim]];
-                if (!n) return;
-                ++aim;
-                right(aim, aim, n.r);
-                left(aim, n.t);
-            }
-
-            left(ai, m);
-        }
-
-        ins(ai, bi, map);
-        del(ai, bi, map);
-        rep(ai, bi, map);
-        swap(ai, bi, map);
+        });
     }
 
     let best: Node | undefined;
--- packages/cspell-trie-lib/src/lib/distance/weightedMaps.test.ts
+++ packages/cspell-trie-lib/src/lib/distance/weightedMaps.test.ts
@@ -1,7 +1,8 @@
 import type { SuggestionCostMapDef } from './suggestionCostsDef';
-import { addWeightedDefMapToTrie, __testing__ } from './weightedMaps';
+import type { CostPosition } from './weightedMaps';
+import { createWeightMap, __testing__ } from './weightedMaps';
 
-const { splitMapSubstrings, splitMap } = __testing__;
+const { splitMapSubstrings, splitMap, findTrieCostPrefixes } = __testing__;
 
 // const u = undefined;
 
@@ -17,51 +18,183 @@ describe('Validate weightedMaps', () => {
 
     test.each`
         map                | expected
-        ${''}              | ${[[]]}
-        ${'||'}            | ${[[], [], []]}
+        ${''}              | ${[]}
+        ${'||'}            | ${[]}
         ${'abc'}           | ${['abc'.split('')]}
+        ${'abc|'}          | ${['abc'.split('')]}
         ${'f(ph)(gh)v|eé'} | ${[['f', 'ph', 'gh', 'v'], ['e', 'é']]}
     `('splitMap "$map"', ({ map, expected }) => {
         expect(splitMap({ map })).toEqual(expected);
     });
 
+    const iBeforeE = {
+        n: {
+            e: {
+                n: {
+                    i: {
+                        t: {
+                            n: {
+                                i: {
+                                    n: {
+                                        e: { c: 3 },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            i: {
+                n: {
+                    e: {
+                        t: {
+                            n: {
+                                e: {
+                                    n: {
+                                        i: { c: 3 },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    };
+
     test.each`
-        map      | insDel       | replace      | swap         | expected
-        ${''}    | ${undefined} | ${undefined} | ${undefined} | ${{}}
-        ${''}    | ${1}         | ${1}         | ${1}         | ${{}}
-        ${'a'}   | ${1}         | ${undefined} | ${undefined} | ${{ a: { insDel: 1 } }}
-        ${'ab'}  | ${1}         | ${undefined} | ${undefined} | ${{ a: { insDel: 1 }, b: { insDel: 1 } }}
-        ${'a'}   | ${1}         | ${2}         | ${3}         | ${{ a: { insDel: 1, r: { a: { rep: 2, swap: 3 } } } }}
-        ${'a'}   | ${0}         | ${0}         | ${0}         | ${{ a: { insDel: 0, r: { a: { rep: 0, swap: 0 } } } }}
-        ${'ab'}  | ${undefined} | ${2}         | ${undefined} | ${{ a: { r: { a: { rep: 2 }, b: { rep: 2 } } }, b: { r: { a: { rep: 2 }, b: { rep: 2 } } } }}
-        ${'a|b'} | ${undefined} | ${2}         | ${undefined} | ${{ a: { r: { a: { rep: 2 } } }, b: { r: { b: { rep: 2 } } } }}
-    `('splitMap "$map"', ({ map, insDel, replace, swap, expected }) => {
-        const def: SuggestionCostMapDef = {
-            map,
-            insDel,
-            replace,
-            swap,
-        };
-        expect(addWeightedDefMapToTrie(def)).toEqual(expected);
+        defs                                  | expected
+        ${[]}                                 | ${{ insDel: {}, replace: {}, swap: {} }}
+        ${[defIns('ab', 3)]}                  | ${{ insDel: { n: { a: { c: 3 }, b: { c: 3 } } }, replace: {}, swap: {} }}
+        ${[defIns('ab', 3), defIns('bc', 2)]} | ${{ insDel: { n: { a: { c: 3 }, b: { c: 2 }, c: { c: 2 } } }, replace: {}, swap: {} }}
+        ${[defRep('ab', 3)]}                  | ${{ insDel: {}, replace: { n: { a: { t: { n: { b: { c: 3 } } } }, b: { t: { n: { a: { c: 3 } } } } } }, swap: {} }}
+        ${[defSwap('ab', 3)]}                 | ${{ insDel: {}, replace: {}, swap: { n: { a: { t: { n: { b: { c: 3 } } } }, b: { t: { n: { a: { c: 3 } } } } } } }}
+        ${[defRep('(ei)(ie)', 3)]}            | ${{ insDel: {}, replace: iBeforeE, swap: {} }}
+    `('buildWeightMap $defs', ({ defs, expected }) => {
+        expect(createWeightMap(...defs)).toEqual(expected);
     });
 
     test.each`
-        defA                          | defB                                     | expected
-        ${{ map: '' }}                | ${{ map: '' }}                           | ${{}}
-        ${{ map: '' }}                | ${{ map: 'b' }}                          | ${{ b: {} }}
-        ${{ map: 'a' }}               | ${{ map: 'b' }}                          | ${{ a: {}, b: {} }}
-        ${{ map: '(ab)' }}            | ${{ map: 'b' }}                          | ${{ a: { t: { b: {} } }, b: {} }}
-        ${{ map: 'a', insDel: 5 }}    | ${{ map: 'a', insDel: 10 }}              | ${{ a: { insDel: 5 } }}
-        ${{ map: 'a', insDel: 5 }}    | ${{ map: 'ab', insDel: 10 }}             | ${{ a: { insDel: 5 }, b: { insDel: 10 } }}
-        ${{ map: 'a', replace: 5 }}   | ${{ map: 'ab', insDel: 10, replace: 3 }} | ${{ a: { insDel: 10, r: { a: { rep: 3 }, b: { rep: 3 } } }, b: { insDel: 10, r: { a: { rep: 3 }, b: { rep: 3 } } } }}
-        ${{ map: '(ab)', insDel: 3 }} | ${{ map: 'b' }}                          | ${{ a: { t: { b: { insDel: 3 } } }, b: {} }}
-    `('splitMap $defA $defB', ({ defA, defB, expected }) => {
-        const tAB = addWeightedDefMapToTrie(defA);
-        addWeightedDefMapToTrie(defB, tAB);
-        expect(tAB).toEqual(expected);
-
-        const tBA = addWeightedDefMapToTrie(defA);
-        addWeightedDefMapToTrie(defB, tBA);
-        expect(tBA).toEqual(expected);
+        defs                                                        | word      | offset | expected
+        ${[]}                                                       | ${''}     | ${1}   | ${[]}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'read'} | ${1}   | ${[{ i: 2, c: 7 }, { i: 3, c: 11 }] /* cspell:disable-line */}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'read'} | ${2}   | ${[{ i: 3, c: 7 }] /* cspell:disable-line */}
+    `('findTrieCostPrefixes with insDel $defs.0 $defs.1 $word $offset', ({ defs, word, offset, expected }) => {
+        const map = createWeightMap(...defs);
+        expect([...findTrieCostPrefixes(map.insDel, word, offset)]).toEqual(expected);
     });
+
+    // cspell:ignore aeiou
+    test.each`
+        defs                                                        | wordA     | ai   | wordB     | bi   | expected
+        ${[]}                                                       | ${''}     | ${1} | ${''}     | ${0} | ${[]}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'read'} | ${1} | ${''}     | ${0} | ${[{ ai: 2, bi: 0, c: 1007 }, { ai: 3, bi: 0, c: 1011 }]}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'read'} | ${1} | ${'ride'} | ${1} | ${[{ ai: 2, bi: 1, c: 1007 /* del e */ }, { ai: 3, bi: 1, c: 1011 /* del ea */ }, { ai: 1, bi: 2, c: 1007 /* ins i */ }]}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'red'}  | ${1} | ${'read'} | ${1} | ${[{ ai: 2, bi: 1, c: 1007 /* del e */ }, { ai: 1, bi: 2, c: 1007 /* ins */ }, { ai: 1, bi: 3, c: 1011 /* ins ea */ }]}
+        ${[defIns('aeiou', 7), defIns('(ae)(ea)(ou)(ei)(ie)', 11)]} | ${'red'}  | ${2} | ${'read'} | ${2} | ${[{ ai: 2, bi: 3, c: 1007 /* ins a */ }]}
+    `(
+        'calcInsDelCosts with $defs.0 $defs.1 "$wordA"@$ai, "$wordB"@$bi',
+        ({
+            defs,
+            wordA,
+            ai,
+            wordB,
+            bi,
+            expected,
+        }: {
+            defs: SuggestionCostMapDef[];
+            wordA: string;
+            wordB: string;
+            ai: number;
+            bi: number;
+            expected: CostPosition[];
+        }) => {
+            const map = createWeightMap(...defs);
+            const results = [...map.calcInsDelCosts({ a: wordA, b: wordB, ai, bi, c: 1000 })];
+            expected.forEach((p) => {
+                (p.a = p.a ?? wordA), (p.b = p.b ?? wordB);
+            });
+            expect(results).toEqual(expect.arrayContaining(expected));
+            expect(results).toHaveLength(expected.length);
+        }
+    );
+
+    test.each`
+        defs                                                | wordA        | ai   | wordB        | bi   | expected
+        ${[]}                                               | ${''}        | ${0} | ${''}        | ${0} | ${[]}
+        ${[defRep('aeiou', 5), defRep('ae(ae)(ea)', 7)]}    | ${'read'}    | ${1} | ${'red'}     | ${1} | ${[{ ai: 3, bi: 2, c: 1007 /* ea -> a */ }]}
+        ${[defRep('aeiou', 5), defRep('ae(ae)(ea)', 7)]}    | ${'read'}    | ${1} | ${'road'}    | ${1} | ${[{ ai: 2, bi: 2, c: 1005 /* e -> o */ }]}
+        ${[defRep('aeiou', 5), defRep('o(oo)|e(ee)', 7)]}   | ${'met'}     | ${1} | ${'meet'}    | ${1} | ${[{ ai: 2, bi: 3, c: 1007 /* e -> ee */ }]}
+        ${[defRep('aeiou', 5), defRep('o(oo)|e(ee)', 7)]}   | ${'meet'}    | ${1} | ${'met'}     | ${1} | ${[{ ai: 3, bi: 2, c: 1007 /* ee -> e */ }]}
+        ${[defRep('aeiou', 5), defRep('(ei)(ie)', 7)]}      | ${'believe'} | ${3} | ${'receive'} | ${3} | ${[{ ai: 4, bi: 4, c: 1005 /* i => e */ }, { ai: 5, bi: 5, c: 1007 /* ie => ei */ }]}
+        ${[defRep('(sk)(sch)', 5), defRep('(sch)(sk)', 7)]} | ${'school'}  | ${0} | ${'skull'}   | ${0} | ${[{ ai: 3, bi: 2, c: 1005 /* sch => sk */ }]}
+    `(
+        'calcReplaceCosts with $defs.0 $defs.1 "$wordA"@$ai, "$wordB"@$bi',
+        ({
+            defs,
+            wordA,
+            ai,
+            wordB,
+            bi,
+            expected,
+        }: {
+            defs: SuggestionCostMapDef[];
+            wordA: string;
+            wordB: string;
+            ai: number;
+            bi: number;
+            expected: CostPosition[];
+        }) => {
+            const map = createWeightMap(...defs);
+            const results = [...map.calcReplaceCosts({ a: wordA, b: wordB, ai, bi, c: 1000 })];
+            expected.forEach((p) => {
+                (p.a = p.a ?? wordA), (p.b = p.b ?? wordB);
+            });
+            expect(results).toEqual(expect.arrayContaining(expected));
+            expect(results).toHaveLength(expected.length);
+        }
+    );
+
+    test.each`
+        defs                                    | wordA        | ai   | wordB        | bi   | expected
+        ${[defSwap('ae', 9), defSwap('ei', 7)]} | ${'believe'} | ${1} | ${'receive'} | ${1} | ${[]}
+        ${[defSwap('ae', 9), defSwap('ei', 7)]} | ${'believe'} | ${3} | ${'receive'} | ${3} | ${[{ ai: 5, bi: 5, c: 1007 /* swap ei -> ie */ }]}
+    `(
+        'calcSwapCosts with $defs.0 $defs.1 "$wordA"@$ai, "$wordB"@$bi',
+        ({
+            defs,
+            wordA,
+            ai,
+            wordB,
+            bi,
+            expected,
+        }: {
+            defs: SuggestionCostMapDef[];
+            wordA: string;
+            wordB: string;
+            ai: number;
+            bi: number;
+            expected: CostPosition[];
+        }) => {
+            const map = createWeightMap(...defs);
+            const results = [...map.calcSwapCosts({ a: wordA, b: wordB, ai, bi, c: 1000 })];
+            expected.forEach((p) => {
+                (p.a = p.a ?? wordA), (p.b = p.b ?? wordB);
+            });
+            expect(results).toEqual(expect.arrayContaining(expected));
+            expect(results).toHaveLength(expected.length);
+        }
+    );
 });
+
+function defIns(map: string, insDel: number): SuggestionCostMapDef {
+    return { map, insDel };
+}
+
+function defRep(map: string, replace: number): SuggestionCostMapDef {
+    return { map, replace };
+}
+
+function defSwap(map: string, swap: number): SuggestionCostMapDef {
+    return { map, swap };
+}
