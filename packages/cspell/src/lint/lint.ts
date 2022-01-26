7b62539ceb241698dff632489abecca9a04df9cb
Jason Dent <jason@streetsidesoftware.nl>
2022-01-26 09:38:26 +0100

    refactor: Refactor the linter

--- packages/cspell/src/lint/lint.ts
+++ packages/cspell/src/lint/lint.ts
@@ -29,7 +29,6 @@ import { LintRequest } from './LintRequest';
 
 export async function runLint(cfg: LintRequest): Promise<RunResult> {
     let { reporter } = cfg;
-    const { fileLists } = cfg;
     cspell.setLogger(getLoggerFromReporter(reporter));
     const configErrors = new Set<string>();
 
@@ -222,19 +221,9 @@ export async function runLint(cfg: LintRequest): Promise<RunResult> {
         reporter = mergeReporters(cfg.reporter, ...loadReporters(configInfo.config));
         cspell.setLogger(getLoggerFromReporter(reporter));
 
-        const useGitignore = cfg.options.gitignore ?? configInfo.config.useGitignore ?? false;
-        const gitignoreRoots = cfg.options.gitignoreRoot ?? configInfo.config.gitignoreRoot;
-        const gitIgnore = useGitignore ? await generateGitIgnore(gitignoreRoots) : undefined;
-
-        const cliGlobs: Glob[] = cfg.fileGlobs;
-        const allGlobs: Glob[] = cliGlobs.length ? cliGlobs : configInfo.config.files || [];
-        const combinedGlobs = normalizeGlobsToRoot(allGlobs, cfg.root, false);
-        const cliExcludeGlobs = extractPatterns(cfg.excludes).map((p) => p.glob);
-        const normalizedExcludes = normalizeGlobsToRoot(cliExcludeGlobs, cfg.root, true);
-        const includeGlobs = combinedGlobs.filter((g) => !g.startsWith('!'));
-        const excludeGlobs = combinedGlobs.filter((g) => g.startsWith('!')).concat(normalizedExcludes);
-        const fileGlobs: string[] = includeGlobs;
-        const hasFileLists = !!fileLists.length;
+        const globInfo = await determineGlobs(configInfo, cfg);
+        const { fileGlobs, excludeGlobs } = globInfo;
+        const hasFileLists = !!cfg.fileLists.length;
         if (!fileGlobs.length && !hasFileLists) {
             // Nothing to do.
             return runResult();
@@ -248,28 +237,10 @@ export async function runLint(cfg: LintRequest): Promise<RunResult> {
 
         // Get Exclusions from the config files.
         const { root } = cfg;
-        const globsToExclude = (configInfo.config.ignorePaths || []).concat(excludeGlobs);
-        const globMatcher = buildGlobMatcher(globsToExclude, root, true);
-        const ignoreGlobs = extractGlobsFromMatcher(globMatcher);
-        // cspell:word nodir
-        const globOptions: GlobOptions = {
-            root,
-            cwd: root,
-            ignore: ignoreGlobs.concat(normalizedExcludes),
-            nodir: true,
-        };
-        const enableGlobDot = cfg.enableGlobDot ?? configInfo.config.enableGlobDot;
-        if (enableGlobDot !== undefined) {
-            globOptions.dot = enableGlobDot;
-        }
 
         try {
             const cacheSettings = await calcCacheSettings(configInfo.config, cfg.options, root);
-            const foundFiles = await (hasFileLists
-                ? useFileLists(fileLists, allGlobs, root, enableGlobDot)
-                : findFiles(fileGlobs, globOptions));
-            const filtered = gitIgnore ? await gitIgnore.filterOutIgnored(foundFiles) : foundFiles;
-            const files = filterFiles(filtered, globMatcher);
+            const files = await determineFilesToCheck(configInfo, cfg, reporter, globInfo);
 
             return await processFiles(files, configInfo, cacheSettings);
         } catch (e) {
@@ -297,6 +268,73 @@ Options:
             MessageTypes.Info
         );
     }
+}
+
+interface AppGlobInfo {
+    /** globs from cli or config.files */
+    allGlobs: Glob[];
+    /** GitIgnore config to use. */
+    gitIgnore: GitIgnore | undefined;
+    /** file globs used to search for matching files. */
+    fileGlobs: string[];
+    /** globs to exclude files found. */
+    excludeGlobs: string[];
+    /** normalized cli exclude globs */
+    normalizedExcludes: string[];
+}
+
+async function determineGlobs(configInfo: ConfigInfo, cfg: LintRequest): Promise<AppGlobInfo> {
+    const useGitignore = cfg.options.gitignore ?? configInfo.config.useGitignore ?? false;
+    const gitignoreRoots = cfg.options.gitignoreRoot ?? configInfo.config.gitignoreRoot;
+    const gitIgnore = useGitignore ? await generateGitIgnore(gitignoreRoots) : undefined;
+
+    const cliGlobs: Glob[] = cfg.fileGlobs;
+    const allGlobs: Glob[] = cliGlobs.length ? cliGlobs : configInfo.config.files || [];
+    const combinedGlobs = normalizeGlobsToRoot(allGlobs, cfg.root, false);
+    const cliExcludeGlobs = extractPatterns(cfg.excludes).map((p) => p.glob);
+    const normalizedExcludes = normalizeGlobsToRoot(cliExcludeGlobs, cfg.root, true);
+    const includeGlobs = combinedGlobs.filter((g) => !g.startsWith('!'));
+    const excludeGlobs = combinedGlobs.filter((g) => g.startsWith('!')).concat(normalizedExcludes);
+    const fileGlobs: string[] = includeGlobs;
+
+    return { allGlobs, gitIgnore, fileGlobs, excludeGlobs, normalizedExcludes };
+}
+
+async function determineFilesToCheck(
+    configInfo: ConfigInfo,
+    cfg: LintRequest,
+    reporter: CSpellReporter,
+    globInfo: AppGlobInfo
+): Promise<string[]> {
+    async function _determineFilesToCheck(): Promise<string[]> {
+        const { fileLists } = cfg;
+        const hasFileLists = !!fileLists.length;
+        const { allGlobs, gitIgnore, fileGlobs, excludeGlobs, normalizedExcludes } = globInfo;
+
+        // Get Exclusions from the config files.
+        const { root } = cfg;
+        const globsToExclude = (configInfo.config.ignorePaths || []).concat(excludeGlobs);
+        const globMatcher = buildGlobMatcher(globsToExclude, root, true);
+        const ignoreGlobs = extractGlobsFromMatcher(globMatcher);
+        // cspell:word nodir
+        const globOptions: GlobOptions = {
+            root,
+            cwd: root,
+            ignore: ignoreGlobs.concat(normalizedExcludes),
+            nodir: true,
+        };
+        const enableGlobDot = cfg.enableGlobDot ?? configInfo.config.enableGlobDot;
+        if (enableGlobDot !== undefined) {
+            globOptions.dot = enableGlobDot;
+        }
+
+        const foundFiles = await (hasFileLists
+            ? useFileLists(fileLists, allGlobs, root, enableGlobDot)
+            : findFiles(fileGlobs, globOptions));
+        const filtered = gitIgnore ? await gitIgnore.filterOutIgnored(foundFiles) : foundFiles;
+        const files = filterFiles(filtered, globMatcher);
+        return files;
+    }
 
     function isExcluded(filename: string, globMatcherExclude: GlobMatcher) {
         if (cspell.isBinaryFile(URI.file(filename))) {
@@ -317,14 +355,6 @@ Options:
         return r.matched;
     }
 
-    function extractGlobSource(g: GlobPatternWithRoot | GlobPatternNormalized) {
-        const { glob, rawGlob, source } = <GlobPatternNormalized>g;
-        return {
-            glob: rawGlob || glob,
-            source,
-        };
-    }
-
     function filterFiles(files: string[], globMatcherExclude: GlobMatcher): string[] {
         const patterns = globMatcherExclude.patterns;
         const excludeInfo = patterns
@@ -335,6 +365,7 @@ Options:
         const result = files.filter(util.uniqueFn()).filter((filename) => !isExcluded(filename, globMatcherExclude));
         return result;
     }
+    return _determineFilesToCheck();
 }
 
 function extractContext(
@@ -372,6 +403,14 @@ function extractContext(
         offset: left + line.offset,
     };
     return context;
+}
+
+function extractGlobSource(g: GlobPatternWithRoot | GlobPatternNormalized) {
+    const { glob, rawGlob, source } = <GlobPatternNormalized>g;
+    return {
+        glob: rawGlob || glob,
+        source,
+    };
 }
 
 function runResult(init: Partial<RunResult> = {}): RunResult {
