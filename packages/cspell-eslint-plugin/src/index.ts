fe4bf32b6ce9003376ce9dbd7049cbc156b5c01c
Jason Dent <jason@streetsidesoftware.nl>
2022-02-11 21:41:31 +0100

    dev: Experimenting with eslint plugin

--- packages/cspell-eslint-plugin/src/index.ts
+++ packages/cspell-eslint-plugin/src/index.ts
@@ -1,6 +1,8 @@
+// cspell:ignore TSESTree
+import type { TSESTree } from '@typescript-eslint/types';
 import type { Rule } from 'eslint';
 // eslint-disable-next-line node/no-missing-import
-import type { Literal, TemplateElement, Identifier, Node, Comment } from 'estree';
+import type { Comment, Identifier, Literal, Node, TemplateElement } from 'estree';
 import { format } from 'util';
 
 interface PluginRules {
@@ -14,7 +16,16 @@ const meta: Rule.RuleMetaData = {
 };
 
 function create(context: Rule.RuleContext): Rule.RuleListener {
+    console.log('Source code: \n ************************ \n\n');
     console.log(context.getSourceCode().getText());
+    console.log(`
+
+id: ${context.id}
+cwd: ${context.getCwd()}
+filename: ${context.getFilename()}
+physicalFilename: ${context.getPhysicalFilename()}
+scope: ${context.getScope().type}
+`);
 
     function checkLiteral(node: Literal & Rule.NodeParentExtension) {
         const val = format('%o', node.value);
@@ -48,18 +59,32 @@ function create(context: Rule.RuleContext): Rule.RuleListener {
         TemplateElement: checkTemplateElement,
         Identifier: checkIdentifier,
     };
-}
 
-type AstNode = Node & Rule.NodeParentExtension;
-
-function inheritance(node: AstNode) {
-    const parts: string[] = [];
-
-    for (let n: AstNode | undefined = node; n; n = n.parent) {
-        parts.push(n.type);
+    function mapNode(node: Node | TSESTree.Node, index: number, nodes: Node[]): string {
+        const child = nodes[index + 1];
+        if (node.type === 'ImportSpecifier') {
+            const extra = node.imported === child ? '.imported' : node.local === child ? '.local' : '';
+            return node.type + extra;
+        }
+        if (node.type === 'ImportDeclaration') {
+            const extra = node.source === child ? '.source' : '';
+            return node.type + extra;
+        }
+        if (node.type === 'Property') {
+            const extra = node.key === child ? 'key' : node.value === child ? 'value' : '';
+            return [node.type, node.kind, extra].join('.');
+        }
+        if (node.type === 'MemberExpression') {
+            const extra = node.property === child ? 'property' : node.object === child ? 'object' : '';
+            return [node.type, extra].join('.');
+        }
+        return node.type;
     }
 
-    return parts.reverse().join('.');
+    function inheritance(node: Node) {
+        const a = [...context.getAncestors(), node];
+        return a.map(mapNode).join('.');
+    }
 }
 
 export const rules: PluginRules = {
