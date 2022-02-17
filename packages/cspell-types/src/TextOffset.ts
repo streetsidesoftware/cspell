d58f7ee579fc4eedf4b8d34bf521924c8404efce
Jason Dent <jason@streetsidesoftware.nl>
2022-02-17 16:48:54 +0100

    dev: Extend TextOffset to support transformed text.

--- packages/cspell-types/src/TextOffset.ts
+++ packages/cspell-types/src/TextOffset.ts
@@ -1,6 +1,17 @@
 export interface TextOffset {
+    /**
+     * The text found at the offset. If the text has been transformed, then the length might not match `length`.
+     * Example: Original: `cafe\u0301`, text: `café`
+     */
     text: string;
+    /**
+     * The offset into the document.
+     */
     offset: number;
+    /**
+     * Assumed to match `text.length` if the text has not been transformed.
+     */
+    length?: number;
 }
 
 export interface TextDocumentOffset extends TextOffset {
