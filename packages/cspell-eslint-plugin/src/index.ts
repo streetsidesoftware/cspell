2a9836e0c10a562bfc7325e15731b8d75d120a6d
Jason Dent <Jason3S@users.noreply.github.com>
2022-03-09 11:54:39 +0100

    dev: Prepare for alpha release of ESLint Plugin. (#2560)
    * dev: eslint-plugin - a bit of clean up and more testing
    
    * dev: Prepare to publish the eslint plugin.

--- packages/cspell-eslint-plugin/src/index.ts
+++ packages/cspell-eslint-plugin/src/index.ts
@@ -5,7 +5,7 @@ import type { Rule } from 'eslint';
 // eslint-disable-next-line node/no-missing-import
 import type { Comment, Identifier, Literal, Node, TemplateElement } from 'estree';
 import { format } from 'util';
-import { createTextDocument, DocumentValidator, ValidationIssue } from 'cspell-lib';
+import { createTextDocument, DocumentValidator, ValidationIssue, CSpellSettings } from 'cspell-lib';
 
 interface PluginRules {
     ['cspell']: Rule.RuleModule;
@@ -17,13 +17,26 @@ const meta: Rule.RuleMetaData = {
     },
 };
 
+type ASTNode = Node | Comment;
+
+const defaultSettings: CSpellSettings = {
+    patterns: [
+        // @todo: be able to use cooked / transformed strings.
+        // {
+        //     // Do not block unicode escape sequences.
+        //     name: 'js-unicode-escape',
+        //     pattern: /$^/g,
+        // },
+    ],
+};
+
 const isDebugMode = false;
 
 const log: typeof console.log = isDebugMode ? console.log : () => undefined;
 
 function create(context: Rule.RuleContext): Rule.RuleListener {
     const doc = createTextDocument({ uri: context.getFilename(), content: context.getSourceCode().getText() });
-    const validator = new DocumentValidator(doc, {}, {});
+    const validator = new DocumentValidator(doc, {}, defaultSettings);
     validator.prepareSync();
 
     log('Source code: \n ************************ \n\n');
@@ -46,6 +59,7 @@ scope: ${context.getScope().type}
 
     function checkTemplateElement(node: TemplateElement & Rule.NodeParentExtension) {
         debugNode(node, node.value);
+        // console.log('Template: %o', node.value);
         checkNodeText(node, node.value.cooked || node.value.raw);
     }
 
@@ -60,13 +74,16 @@ scope: ${context.getScope().type}
         log(`Comment: ${val}`);
     }
 
-    function checkNodeText(node: Node | Comment, text: string) {
+    function checkNodeText(node: ASTNode, text: string) {
         if (!node.range) return;
 
+        const adj = node.type === 'Literal' ? 1 : 0;
+        const range = [node.range[0] + adj, node.range[1] - adj] as const;
+
         const scope = inheritance(node);
-        const result = validator.checkText(node.range, text, scope);
+        const result = validator.checkText(range, text, scope);
         if (result.length) {
-            console.error('%o', result);
+            log('%o', result);
         }
         result.forEach((issue) => reportIssue(issue));
     }
@@ -100,7 +117,7 @@ scope: ${context.getScope().type}
         Identifier: checkIdentifier,
     };
 
-    function mapNode(node: Node | TSESTree.Node | Comment, index: number, nodes: (Node | Comment)[]): string {
+    function mapNode(node: ASTNode | TSESTree.Node, index: number, nodes: ASTNode[]): string {
         const child = nodes[index + 1];
         if (node.type === 'ImportSpecifier') {
             const extra = node.imported === child ? '.imported' : node.local === child ? '.local' : '';
@@ -136,23 +153,23 @@ scope: ${context.getScope().type}
         return node.type;
     }
 
-    function inheritance(node: Node | Comment) {
+    function inheritance(node: ASTNode) {
         const a = [...context.getAncestors(), node];
         return a.map(mapNode);
     }
 
-    function inheritanceSummary(node: Node | Comment) {
+    function inheritanceSummary(node: ASTNode) {
         return inheritance(node).join(' ');
     }
 
-    function debugNode(node: Node | Comment, value: unknown) {
+    function debugNode(node: ASTNode, value: unknown) {
         if (!isDebugMode) return;
         const val = format('%o', value);
         log(`${inheritanceSummary(node)}: ${val}`);
     }
 }
 
-function tagLiteral(node: Node | TSESTree.Node): string {
+function tagLiteral(node: ASTNode | TSESTree.Node): string {
     assert(node.type === 'Literal');
     const kind = typeof node.value;
     const extra =
