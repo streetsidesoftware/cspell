8c09bc466a8d398e875531a46317fc4382036f10
Jason Dent <jason@streetsidesoftware.nl>
2022-03-09 14:26:25 +0100

    dev: setup a test-package for the eslint-plugin

---
+++ test-packages/test-cspell-eslint-plugin/.eslintrc.js
@@ -0,0 +1,68 @@
+/**
+ * @type { import("eslint").Linter.Config }
+ */
+const config = {
+    env: {
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:node/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:promise/recommended',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: [
+        '**/*.d.ts',
+        '**/*.map',
+        '**/coverage/**',
+        '**/dist/**',
+        '**/node_modules/**',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    overrides: [
+        {
+            files: '**/*.ts',
+            extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint'],
+            rules: {
+                '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+                'node/no-missing-import': [
+                    'error',
+                    {
+                        tryExtensions: ['.js', '.d.ts', '.ts'],
+                    },
+                ],
+                'node/no-unsupported-features/es-syntax': [
+                    'error',
+                    {
+                        ignores: ['modules'],
+                    },
+                ],
+            },
+        },
+        {
+            files: ['**/*.test.ts', '**/*.spec.ts'],
+            extends: 'plugin:jest/recommended',
+            env: {
+                jest: true,
+            },
+        },
+        {
+            files: ['**/*.ts', '**/*.js'],
+            plugins: ['@cspell'],
+            rules: {
+                '@cspell/cspell': 'warn',
+            },
+        },
+    ],
+    root: true,
+};
+
+module.exports = config;
---
+++ test-packages/test-cspell-eslint-plugin/README.md
@@ -0,0 +1,5 @@
+# Integration Test of @cspell/eslint-plugin-cspell
+
+This package is NOT to be published.
+
+It purpose is to have a test case using the eslint-plugin.
