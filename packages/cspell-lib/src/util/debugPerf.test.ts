14e9add94164b5e12c4927d214a0a8e806d2d6d6
Jason Dent <jason@streetsidesoftware.nl>
2022-03-23 16:45:32 +0100

    Update debugPerf.test.ts

--- packages/cspell-lib/src/util/debugPerf.test.ts
+++ packages/cspell-lib/src/util/debugPerf.test.ts
@@ -1,9 +1,16 @@
 import { perfFn } from './debugPerf';
 
 describe('debugPerf', () => {
-    test('perfFn', () => {
+    test('perfFn with callback', () => {
         const mock = jest.fn();
         const fn = perfFn(() => undefined, 'message', mock);
+        fn();
+        expect(mock).toHaveBeenCalledWith(expect.stringContaining('message'), expect.any(Number));
+    });
+
+    test('perfFn default callback', () => {
+        const mock = jest.spyOn(console, 'error').mockImplementation();
+        const fn = perfFn(() => undefined, 'message');
         fn();
         expect(mock).toHaveBeenCalledWith(expect.stringContaining('message'));
     });
