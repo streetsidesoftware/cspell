631073b42f24dec00eed9740cb8ee2e5bce4b07f
Jason Dent <Jason3S@users.noreply.github.com>
2022-04-08 15:51:46 +0200

    fix: add --cache-reset option (#2677)
    * fix: add --cache-reset option
    * Update app.test.ts.snap

--- packages/cspell/src/util/cache/hash.ts
+++
@@ -1,9 +0,0 @@
-// cspell:word imurmurhash
-import murmur from 'imurmurhash';
-
-/**
- * Hash the given string using MurmurHash3
- */
-export function hash(str: string): string {
-    return murmur(str).result().toString(36);
-}
