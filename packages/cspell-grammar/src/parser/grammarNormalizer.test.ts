2057e228ff93dfdcdd810fd542e58d51f6b889fe
Jason Dent <jason@streetsidesoftware.nl>
2022-04-15 21:35:52 +0200

    Experiment with parsing

--- packages/cspell-grammar/src/parser/grammarNormalizer.test.ts
+++ packages/cspell-grammar/src/parser/grammarNormalizer.test.ts
@@ -12,11 +12,11 @@ describe('grammarNormalizer', () => {
     test.each`
         line                               | offset | expectedScope                                          | expectedMatch
         ${''}                              | ${0}   | ${undefined}                                           | ${undefined}
-        ${"import * as p from 'path';\n"}  | ${0}   | ${['string.quoted.single.ts', 'code.ts', 'source.ts']} | ${oc({ index: 19, matches: ["'"] })}
-        ${`x = "a's" + 'b'; // comment\n`} | ${0}   | ${['string.quoted.double.ts', 'code.ts', 'source.ts']} | ${oc({ index: 4, matches: ['"'] })}
-        ${`x = "a's" + 'b'; // comment\n`} | ${9}   | ${['string.quoted.single.ts', 'code.ts', 'source.ts']} | ${oc({ index: 12, matches: ["'"] })}
-        ${`x = "a's" + 'b'; // comment\n`} | ${15}  | ${['comment.line.ts', 'code.ts', 'source.ts']}         | ${oc({ index: 17, match: '//' })}
-    `('normalizeGrammar.exec', ({ line, offset, expectedScope, expectedMatch }) => {
+        ${"import * as p from 'path';\n"}  | ${19}  | ${['string.quoted.single.ts', 'code.ts', 'source.ts']} | ${oc({ index: 19, matches: ["'"] })}
+        ${`x = "a's" + 'b'; // comment\n`} | ${4}   | ${['string.quoted.double.ts', 'code.ts', 'source.ts']} | ${oc({ index: 4, matches: ['"'] })}
+        ${`x = "a's" + 'b'; // comment\n`} | ${12}  | ${['string.quoted.single.ts', 'code.ts', 'source.ts']} | ${oc({ index: 12, matches: ["'"] })}
+        ${`x = "a's" + 'b'; // comment\n`} | ${17}  | ${['comment.line.ts', 'code.ts', 'source.ts']}         | ${oc({ index: 17, match: '//' })}
+    `('normalizeGrammar.exec $line $offset', ({ line, offset, expectedScope, expectedMatch }) => {
         const grammar = normalizeGrammar(grammarTS);
         const lineOff: LineOffsetAnchored = { text: line, offset, lineNumber: 5, anchor: -1 };
         const rule = grammar.begin(undefined);
