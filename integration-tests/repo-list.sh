be5fb2decaef51d71609bbf79b47f569be046adb
Jason Dent <jason@streetsidesoftware.nl>
2022-05-23 08:17:15 +0200

    ci: improve generating integrations list

--- integration-tests/repo-list.sh
+++ integration-tests/repo-list.sh
@@ -1,4 +1,6 @@
 # output the list of repositories sorted
-cat config/config.json | jq -r ".repositories | sort_by(.path | ascii_downcase) | .[].path  | select(startswith(\"streetside\") | not) | \"          - \" + ."
+cat config/config.json \
+| jq ".repositories | sort_by(.path | ascii_downcase) | .[].path  | select(startswith(\"streetside\") | not)" \
+| jq -s '.' > ../.github/integrations.json
 
 # cspell:ignore downcase startswith streetside
