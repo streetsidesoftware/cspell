1bf49f13397923e36b5076bb33e489c4d79d8081
Jason Dent <jason@streetsidesoftware.nl>
2022-05-29 20:00:04 +0200

    Update api.d.ts

--- packages/cspell-lib/api/api.d.ts
+++ packages/cspell-lib/api/api.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
-import { Glob, CSpellSettingsWithSourceTrace, TextOffset, TextDocumentOffset, ImportFileRef, CSpellUserSettings, LocaleId, CSpellSettings } from '@cspell/cspell-types';
+import { Glob, CSpellSettingsWithSourceTrace, ReplaceMap, DictionaryInformation, DictionaryDefinitionPreferred, DictionaryDefinitionAugmented, DictionaryDefinitionCustom, TextOffset, TextDocumentOffset, PnPSettings as PnPSettings$1, ImportFileRef, CSpellUserSettings, LocaleId, CSpellSettings } from '@cspell/cspell-types';
 export * from '@cspell/cspell-types';
-import { CompoundWordsMethod, SuggestionResult, SuggestionCollector } from 'cspell-trie-lib';
+import { CompoundWordsMethod, SuggestionResult, SuggestionCollector, WeightMap } from 'cspell-trie-lib';
 export { CompoundWordsMethod, SuggestionCollector, SuggestionResult } from 'cspell-trie-lib';
 export * from 'cspell-io';
 import { URI } from 'vscode-uri';
@@ -208,6 +208,20 @@ declare class SpellingDictionaryCollection implements SpellingDictionary {
     private _isNoSuggestWord;
 }
 
+/**
+ * The keys of an object where the values cannot be undefined.
+ */
+declare type OptionalKeys<T> = Exclude<{
+    [P in keyof T]: T[P] extends Exclude<T[P], undefined> ? never : P;
+}[keyof T], undefined>;
+/**
+ * Allow undefined in optional fields
+ */
+declare type OptionalOrUndefined<T> = {
+    [P in keyof T]: P extends OptionalKeys<T> ? T[P] | undefined : T[P];
+};
+
+declare const SymbolCSpellSettingsInternal: unique symbol;
 interface CSpellSettingsInternal extends Omit<CSpellSettingsWithSourceTrace, 'dictionaryDefinitions'> {
     [SymbolCSpellSettingsInternal]: true;
     dictionaryDefinitions?: DictionaryDefinitionInternal[];
@@ -216,6 +230,15 @@ interface CSpellSettingsInternalFinalized extends CSpellSettingsInternal {
     finalized: true;
     ignoreRegExpList: RegExp[];
     includeRegExpList: RegExp[];
+}
+declare type DictionaryDefinitionCustomUniqueFields = Omit<DictionaryDefinitionCustom, keyof DictionaryDefinitionPreferred>;
+interface DictionaryDefinitionInternal extends Readonly<DictionaryDefinitionPreferred>, Readonly<Partial<DictionaryDefinitionCustomUniqueFields>>, Readonly<DictionaryDefinitionAugmented> {
+    /**
+     * Optional weight map used to improve suggestions.
+     */
+    readonly weightMap?: WeightMap | undefined;
+    /** The path to the config file that contains this dictionary definition */
+    readonly __source?: string | undefined;
 }
 
 declare function refreshDictionaryCache(maxAge?: number): Promise<void>;
@@ -348,11 +371,15 @@ declare namespace text_d {
 declare type LanguageId = string;
 declare function getLanguagesForExt(ext: string): string[];
 
-declare type DocumentUri = Uri.Uri;
+declare type DocumentUri = URI;
 interface Position {
     line: number;
     character: number;
 }
+/**
+ * Range offset tuple.
+ */
+declare type SimpleRange$1 = [start: number, end: number];
 interface TextDocumentLine {
     readonly text: string;
     readonly offset: number;
@@ -400,7 +427,7 @@ interface CreateTextDocumentParams {
     version?: number | undefined;
 }
 interface TextDocumentContentChangeEvent {
-    range?: SimpleRange;
+    range?: SimpleRange$1;
     text: string;
 }
 declare function createTextDocument({ uri, content, languageId, locale, version, }: CreateTextDocumentParams): TextDocument;
@@ -414,7 +441,7 @@ declare type LoaderResult = URI | undefined;
 
 declare type CSpellSettingsWST$1 = CSpellSettingsWithSourceTrace;
 declare type CSpellSettingsI$1 = CSpellSettingsInternal;
-declare type PnPSettings = OptionalOrUndefined<PnPSettingsStrict>;
+declare type PnPSettings = OptionalOrUndefined<PnPSettings$1>;
 declare const sectionCSpell = "cSpell";
 declare const defaultFileName = "cspell.json";
 declare const defaultConfigFilenames: readonly string[];
@@ -494,15 +521,35 @@ declare class ImportError extends Error {
 
 declare function combineTextAndLanguageSettings(settings: CSpellUserSettings, text: string, languageId: string | string[]): CSpellSettingsInternal;
 
+interface MatchRange {
+    startPos: number;
+    endPos: number;
+}
+
+declare type TextOffsetRO = Readonly<TextOffset>;
+interface ValidationOptions extends IncludeExcludeOptions {
+    maxNumberOfProblems?: number;
+    maxDuplicateProblems?: number;
+    minWordLength?: number;
+    flagWords?: string[];
+    allowCompoundWords?: boolean;
+    /** ignore case when checking words against dictionary or ignore words list */
+    ignoreCase: boolean;
+}
 interface IncludeExcludeOptions {
     ignoreRegExpList?: RegExp[];
     includeRegExpList?: RegExp[];
 }
-interface ValidationResult extends TextOffsetRW {
-    line: TextOffsetRW;
+interface ValidationResult extends TextOffset {
+    line: TextOffset;
     isFlagged?: boolean;
     isFound?: boolean;
 }
+interface LineSegment {
+    line: TextOffsetRO;
+    segment: TextOffsetRO;
+}
+declare type LineValidator = (line: LineSegment) => Iterable<ValidationResult>;
 
 interface ValidationIssue extends ValidationResult {
     suggestions?: string[];
@@ -531,6 +578,7 @@ declare enum IncludeExcludeFlag {
 }
 declare function checkText(text: string, settings: CSpellUserSettings): Promise<CheckTextInfo>;
 
+declare type Offset = number;
 declare type SimpleRange = readonly [Offset, Offset];
 interface ParsedText {
     /**
