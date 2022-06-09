a6b2a89ef3a05557764f0f5e1a0e04a9efd175fd
Jason Dent <jason@streetsidesoftware.nl>
2022-06-09 12:39:40 +0200

    ci: Use static integration list

--- integration-tests/repo-list.sh
+++ integration-tests/repo-list.sh
@@ -1,6 +1,6 @@
 # output the list of repositories sorted
 cat config/config.json \
 | jq ".repositories | sort_by(.path | ascii_downcase) | .[].path  | select(startswith(\"streetside\") | not)" \
-| jq -s '.' > ../.github/integrations.json
+| jq -s '.' | tee ../.github/integrations.json
 
 # cspell:ignore downcase startswith streetside
