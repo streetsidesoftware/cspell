d60f3f5f87bbdca54d051126e258926678adb5a0
Jason Dent <jason@streetsidesoftware.nl>
2022-06-14 10:18:18 -0700

    support merging strings

---
+++ packages/cspell-grammar/src/mappers/appendMappedText.ts
@@ -0,0 +1,42 @@
+import assert from 'assert';
+import { MappedText } from './types';
+
+export function appendMappedText(a: MappedText, b: MappedText): MappedText {
+    if (!a.map && !b.map) {
+        return { text: a.text + b.text };
+    }
+    const aLen = a.text.length;
+    const bLen = b.text.length;
+    const aMap = [0, 0].concat(a.map || [0, 0, aLen, aLen]);
+    const bMap = [0, 0].concat(b.map || [0, 0, bLen, bLen]);
+
+    assert(aMap[aMap.length - 1] === aLen);
+    assert(bMap[bMap.length - 1] === bLen);
+    assert((aMap.length & 1) === 0);
+    assert((bMap.length & 1) === 0);
+    return {
+        text: a.text + b.text,
+        map: joinMaps(aMap, bMap),
+    };
+}
+
+function joinMaps(aMap: number[], bMap: number[]): number[] {
+    const n = aMap.length - 1;
+    const offsets = [aMap[n - 1], aMap[n]];
+    const ab = aMap.concat(bMap.map((v, i) => v + offsets[i & 1]));
+    // Normalize the map by removing duplicate entries
+    const r = [0, 0];
+    let last0 = 0,
+        last1 = 0;
+    for (let i = 0; i < ab.length; i += 2) {
+        const v0 = ab[i];
+        const v1 = ab[i + 1];
+        if (v0 === last0 && v1 === last1) {
+            continue;
+        }
+        r.push(v0, v1);
+        last0 = v0;
+        last1 = v1;
+    }
+    return r;
+}
--- packages/cspell-grammar/src/parsers/typescript/index.ts
+++ packages/cspell-grammar/src/parsers/typescript/index.ts
@@ -2,10 +2,11 @@ import { grammar } from '../../grammars/typescript';
 import { compileGrammar } from '../..';
 import { createParser } from '../../parser/parser';
 import { TokenizedLine } from '../../parser/types';
-import { ParseResult, Scope } from '@cspell/cspell-types/Parser';
+import { ParsedText, ParseResult, Scope } from '@cspell/cspell-types/Parser';
 import { pipeSync as pipe } from '@cspell/cspell-pipe';
 import { mapRawString } from '../../mappers/typescript';
 import { ScopePool } from '../../parser/scope';
+import { appendMappedText } from '../../mappers/appendMappedText';
 
 const tsGrammar = compileGrammar(grammar);
 
@@ -28,6 +29,44 @@ function* transform(texts: ParseResult['parsedTexts']): ParseResult['parsedTexts
     }
 }
 
+function* mergeStringResults(results: Iterable<ParsedText>): Iterable<ParsedText> {
+    let last: ParsedText | undefined;
+    for (const next of results) {
+        if (!doesScopeMatch(next.scope, 'string.')) {
+            if (last) {
+                yield last;
+                last = undefined;
+            }
+            yield next;
+            continue;
+        }
+        if (!last) {
+            last = next;
+            continue;
+        }
+        if (next.scope !== last.scope || last.range[1] !== next.range[0]) {
+            yield last;
+            last = next;
+            continue;
+        }
+        last = mergeParsedText(last, next);
+    }
+    if (last) yield last;
+}
+
+function mergeParsedText(a: ParsedText, b: ParsedText): ParsedText {
+    const abT = appendMappedText(a, b);
+    const ab: ParsedText = {
+        text: abT.text,
+        scope: a.scope,
+        range: [a.range[0], b.range[1]],
+        map: abT.map,
+        delegate: a.delegate,
+    };
+
+    return ab;
+}
+
 function mapTokenizedLine(tl: TokenizedLine): ParseResult['parsedTexts'] {
     return pipe(
         tl.tokens
@@ -37,7 +76,8 @@ function mapTokenizedLine(tl: TokenizedLine): ParseResult['parsedTexts'] {
                 range: [tl.offset + t.range[0], tl.offset + t.range[1]] as const,
                 scope: t.scope,
             })),
-        transform
+        transform,
+        mergeStringResults
     );
 }
 
