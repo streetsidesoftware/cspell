a81bd7a07e13985bda9b899ce64548d9dec58f97
Jason Dent <jason@streetsidesoftware.nl>
2022-07-29 11:28:31 +0200

    fix: Add encoding to reads

--- packages/cspell-io/src/requests/RequestFsReadFile.ts
+++ packages/cspell-io/src/requests/RequestFsReadFile.ts
@@ -3,5 +3,6 @@ import { requestFactory } from '@cspell/cspell-service-bus';
 const RequestType = 'fs:readFile' as const;
 interface RequestParams {
     readonly url: URL;
+    readonly encoding: BufferEncoding;
 }
 export const RequestFsReadFile = requestFactory<typeof RequestType, RequestParams, Promise<string>>(RequestType);
