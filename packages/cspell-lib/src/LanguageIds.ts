e214ecec4aa3180cbdc4a7f19066d601927c0133
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2022-09-12 14:48:04 +0900

    feat: add awareness of CMake (#3560)
    * feat: add awareness of CMake
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    
    * Tell cspell to ignore `cmake` word.
    
    Adding it to software terms: https://github.com/streetsidesoftware/cspell-dicts/pull/1442
    
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>
    Co-authored-by: Jason Dent <Jason3S@users.noreply.github.com>

--- packages/cspell-lib/src/LanguageIds.ts
+++ packages/cspell-lib/src/LanguageIds.ts
@@ -32,6 +32,8 @@ export const languageExtensionDefinitions: LanguageDefinitions = [
     { id: 'clojure', extensions: ['.clj', '.cljs', '.cljx', '.clojure', '.edn'] },
     { id: 'coffeescript', extensions: ['.coffee', '.cson'] },
     { id: 'c', extensions: ['.c'] },
+    // cspell:ignore cmake
+    { id: 'cmake', extensions: ['.cmake'], filenames: ['CMakeLists.txt'] },
     {
         id: 'cpp',
         extensions: ['.cpp', '.cc', '.cxx', '.hpp', '.hh', '.hxx', '.h', '.mm', '.ino', '.inl'],
