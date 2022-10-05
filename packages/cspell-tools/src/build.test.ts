feb16fdccaaff396404c2725f9e65ba55238b22f
Jason Dent <jason@streetsidesoftware.nl>
2022-10-05 20:00:13 +0200

    test: add more build tests

--- packages/cspell-tools/src/build.test.ts
+++ packages/cspell-tools/src/build.test.ts
@@ -1,16 +1,34 @@
 import { build } from './build';
+import { readTextFile } from './compiler/readTextFile';
 import { createTestHelper } from './test/TestHelper';
 
 const helper = createTestHelper(__filename);
 
 describe('build action', () => {
     test.each`
-        currentDir                                           | config
-        ${helper.resolveFixture('build-single-target-json')} | ${undefined}
-        ${'.'}                                               | ${helper.resolveFixture('build-single-target-yaml/cspell-tools.config.yaml')}
-    `('build', async ({ currentDir, config }) => {
+        currentDir                       | config                                                    | target
+        ${f('build-single-target-json')} | ${undefined}                                              | ${t('build-single-target-json/colors.txt')}
+        ${'.'}                           | ${f('build-single-target-yaml/cspell-tools.config.yaml')} | ${t('build-single-target-yaml/colors.txt')}
+        ${f('build-single-trie')}        | ${undefined}                                              | ${t('build-single-trie/cities.trie')}
+        ${f('build-source-list')}        | ${undefined}                                              | ${t('build-source-list/source-list.txt')}
+    `('build', async ({ currentDir, config, target }) => {
         helper.mkdir(currentDir);
         helper.cd(currentDir);
         await expect(build(undefined, { config })).resolves.toBeUndefined();
+        const content = await readTextFile(target);
+        expect(content).toMatchSnapshot();
     });
 });
+
+/**
+ * resolve build target
+ * @param parts
+ * @returns
+ */
+function t(...parts: string[]): string {
+    return helper.packageTemp('builds', ...parts);
+}
+
+function f(...parts: string[]): string {
+    return helper.resolveFixture(...parts);
+}
--- packages/cspell-tools/src/test/TestHelper.ts
+++ packages/cspell-tools/src/test/TestHelper.ts
@@ -10,6 +10,9 @@ export interface TestHelper {
     readonly repoRoot: string;
     readonly tempDir: string;
 
+    /**
+     * delete the contents of the temp directory for the current test.
+     */
     clearTempDir(): void;
     /**
      * Resolves the parts to be an absolute path in the temp directory
@@ -39,6 +42,8 @@ export interface TestHelper {
     cd(dir: string): void;
 
     cdToTempDir(): void;
+
+    packageTemp(...parts: string[]): string;
 }
 
 export function createTestHelper(testFilename: string): TestHelper {
@@ -56,35 +61,73 @@ class TestHelperImpl implements TestHelper {
         this.fixtureDir = path.join(packageRoot, 'fixtures');
     }
 
+    /**
+     * delete the contents of the temp directory for the current test.
+     */
     clearTempDir(): void {
         shell.rm('-rf', this.resolveTemp());
     }
 
+    /**
+     * resolve a path relative to the
+     * @param parts
+     * @returns
+     */
     resolveTemp(...parts: string[]): string {
         const testName = (expect.getState().currentTestName || '').replace(/[^\w_.-]/g, '_');
         return path.resolve(this.tempDir, testName, ...parts);
     }
 
+    /**
+     * make a directory. It is ok to make the same directory multiple times.
+     * @param parts
+     */
     mkdir(...parts: string[]): void {
         const pTemp = this.resolveTemp(...parts);
         shell.mkdir('-p', pTemp);
     }
 
-    cp(from: string, to: string): void {
-        shell.cp(this.resolveTemp(from), this.resolveTemp(to));
+    /**
+     * Copy files from src to dest
+     * @param src - glob
+     * @param dest - directory or file
+     */
+    cp(src: string, dest: string): void {
+        shell.cp(this.resolveTemp(src), this.resolveTemp(dest));
     }
 
+    /**
+     * Change the current directory
+     * @param dir
+     */
     cd(dir: string): void {
         shell.cd(this.resolveTemp(dir));
     }
 
+    /**
+     * Change dir to temp directory unique to the current test.
+     */
     cdToTempDir(): void {
         this.createTempDir();
         this.cd('.');
     }
 
+    /**
+     * resolve a path to the fixtures.
+     * @param parts
+     * @returns
+     */
     resolveFixture(...parts: string[]): string {
         return path.resolve(this.fixtureDir, ...parts);
+    }
+
+    /**
+     * calc a path relative to the package temp directory.
+     * @param parts - optional path segments
+     * @returns
+     */
+    packageTemp(...parts: string[]): string {
+        return path.resolve(tempDirBase, ...parts);
     }
 
     readonly createTempDir = this.mkdir;
