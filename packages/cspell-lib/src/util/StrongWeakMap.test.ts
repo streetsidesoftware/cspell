b62fd9cf0b87c76d31da66945d5a4ec6dbe5ef1c
Jason Dent <Jason3S@users.noreply.github.com>
2022-11-03 13:04:07 +0100

    feat: Improve Memory Usage (#3804)
    Use Weak References to dictionaries to allow them to be garbage collected.

---
+++ packages/cspell-lib/src/util/StrongWeakMap.test.ts
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { StrongWeakMap } from './StrongWeakMap';
+
+describe('StrongWeakMap', () => {
+    test.each`
+        init                                     | add                                      | remove       | expected
+        ${undefined}                             | ${undefined}                             | ${undefined} | ${[]}
+        ${undefined}                             | ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${undefined} | ${[['a', 'aa'], ['b', 'b']]}
+        ${undefined}                             | ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${['b']}     | ${[['a', 'aa']]}
+        ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${undefined}                             | ${['b']}     | ${[['a', 'aa']]}
+    `('constructor set/delete', ({ init, add, remove, expected }) => {
+        const map = new StrongWeakMap(safeBoxKeyValues(init));
+        addAll(map, add);
+        deleteAll(map, remove);
+        const boxedExpected = boxKeyValues(expected);
+        expect([...map]).toEqual(boxedExpected);
+        expect(map.size).toBe(expected.length);
+        expect([...map.entries()]).toEqual(boxedExpected);
+        expect([...map.keys()]).toEqual(boxedExpected.map(([k]) => k));
+        expect([...map.values()]).toEqual(boxedExpected.map(([_, v]) => v));
+    });
+
+    test.each`
+        init                                     | add                                      | remove       | expected
+        ${undefined}                             | ${undefined}                             | ${undefined} | ${[]}
+        ${undefined}                             | ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${undefined} | ${[]}
+        ${undefined}                             | ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${['b']}     | ${[]}
+        ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${undefined}                             | ${['b']}     | ${[]}
+    `('clear', ({ init, add, remove, expected }) => {
+        const map = new StrongWeakMap(safeBoxKeyValues(init));
+        addAll(map, add);
+        deleteAll(map, remove);
+        map.clear();
+        expect([...map]).toEqual(boxKeyValues(expected));
+        expect(map.size).toBe(expected.length);
+    });
+
+    test.each`
+        init                                     | has     | expected
+        ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${'a'}  | ${true}
+        ${[['a', 'a'], ['b', 'b'], ['a', 'aa']]} | ${'aa'} | ${false}
+    `('has', ({ init, has, expected }) => {
+        const map = new StrongWeakMap(safeBoxKeyValues(init));
+        expect(map.has(has)).toBe(expected);
+    });
+
+    test('toStringTag', () => {
+        const map = new StrongWeakMap<string, Boxed<string>>();
+        expect(map[Symbol.toStringTag]).toBe('StrongWeakMap');
+    });
+
+    test('forEach', () => {
+        const entries = boxKeyValues([
+            ['a', 'a'],
+            ['b', 'b'],
+        ]);
+        const map = new StrongWeakMap(entries);
+
+        const thisArg = {};
+        const _entries: typeof entries = [];
+
+        function cb(this: typeof thisArg, v: Boxed<string>, k: string, m: Map<string, Boxed<string>>) {
+            _entries.push([k, v]);
+            expect(m).toBe(map);
+            expect(this).toBe(thisArg);
+        }
+
+        map.forEach(cb, thisArg);
+        expect(_entries).toEqual(entries);
+    });
+});
+
+type Boxed<T> = T extends object ? T : { value: T };
+
+function box<T>(value: T): Boxed<T> {
+    if (value && typeof value === 'object') {
+        return value as Boxed<T>;
+    }
+    return { value } as Boxed<T>;
+}
+
+function boxKeyValue<K, V>([k, v]: [K, V]): [K, Boxed<V>] {
+    return [k, box(v)];
+}
+
+function boxKeyValues<K, V>(entries: [K, V][]) {
+    return entries.map(boxKeyValue);
+}
+
+function safeBoxKeyValues<K, V>(entries: [K, V][] | undefined) {
+    return entries && boxKeyValues(entries);
+}
+
+function addAll<K, V>(map: Map<K, Boxed<V>>, entries: [key: K, value: V][] | undefined) {
+    entries?.map(boxKeyValue).forEach(([k, v]) => map.set(k, v));
+}
+
+function deleteAll<K, V>(map: Map<K, Boxed<V>>, keys: K[] | undefined) {
+    keys?.forEach((k) => map.delete(k));
+}
---
+++ packages/cspell-lib/src/util/StrongWeakMap.ts
@@ -0,0 +1,106 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+export class StrongWeakMap<K, V extends {}> implements Map<K, V> {
+    private map: Map<K, WeakRef<V>>;
+
+    constructor(init?: [K, V][]) {
+        this.map = new Map(init?.map(([k, v]) => [k, new WeakRef(v)]));
+    }
+
+    clear(): void {
+        this.map.clear();
+    }
+    /**
+     * @returns true if an element in the Map existed and has been removed, or false if the element does not exist.
+     */
+    delete(key: K): boolean {
+        return this.map.delete(key);
+    }
+
+    /**
+     * Executes a provided function once per each key/value pair in the Map, in insertion order.
+     */
+    forEach(
+        callbackfn: (value: V, key: K, map: Map<K, V>) => void,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        thisArg?: any
+    ): void {
+        if (thisArg) {
+            callbackfn = callbackfn.bind(thisArg);
+        }
+        for (const [key, value] of this) {
+            callbackfn(value, key, this);
+        }
+    }
+    /**
+     * Returns a specified element from the Map object. If the value that is associated to the provided key is an object, then you will get a reference to that object and any change made to that object will effectively modify it inside the Map.
+     * @returns Returns the element associated with the specified key. If no element is associated with the specified key, undefined is returned.
+     */
+    get(key: K): V | undefined {
+        const ref = this.map.get(key);
+        if (!ref) return undefined;
+        const value = ref.deref();
+        if (!value) {
+            this.map.delete(key);
+            return undefined;
+        }
+        return value;
+    }
+    /**
+     * Note: has will cause the value object to live longer.
+     * See: [WeakRef - JavaScript | MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakRef#notes_on_weakrefs)
+     * @returns boolean indicating whether an element with the specified key exists or not.
+     */
+    has(key: K): boolean {
+        const value = this.get(key);
+        return !!value;
+    }
+    /**
+     * Adds a new element with a specified key and value to the Map. If an element with the same key already exists, the element will be updated.
+     */
+    set(key: K, value: V): this {
+        this.map.set(key, new WeakRef(value));
+        return this;
+    }
+    /**
+     * @returns the number of elements in the Map. Note: it is possible that some of the values have been dereferenced
+     */
+    get size(): number {
+        return this.map.size;
+    }
+
+    /** Returns an iterable of entries in the map. */
+    [Symbol.iterator](): IterableIterator<[K, V]> {
+        return this.entries();
+    }
+
+    /**
+     * Returns an iterable of key, value pairs for every entry in the map.
+     */
+    *entries(): IterableIterator<[K, V]> {
+        for (const key of this.map.keys()) {
+            const value = this.get(key);
+            if (!value) continue;
+            yield [key, value];
+        }
+    }
+
+    /**
+     * Returns an iterable of keys in the map
+     *
+     * Note: It is possible that the value associated with the key was released.
+     */
+    keys(): IterableIterator<K> {
+        return this.map.keys();
+    }
+
+    /**
+     * Returns an iterable of values in the map
+     */
+    *values(): IterableIterator<V> {
+        for (const [_, value] of this) {
+            yield value;
+        }
+    }
+
+    readonly [Symbol.toStringTag]: string = 'StrongWeakMap';
+}
