22b8393e298f2fa823b1045c6a9a99458297a086
Jason Dent <jason@streetsidesoftware.nl>
2022-11-23 16:29:08 -0700

    Update ForbiddenWordsDictionary.ts

--- packages/cspell-dictionary/src/SpellingDictionary/ForbiddenWordsDictionary.ts
+++ packages/cspell-dictionary/src/SpellingDictionary/ForbiddenWordsDictionary.ts
@@ -53,68 +53,6 @@ class ForbiddenWordsDictionaryTrie extends SpellingDictionaryFromTrie {
     readonly isDictionaryCaseSensitive: boolean = true;
 }
 
-// class ForbiddenWordsDictionary implements SpellingDictionary {
-//     private dict: Set<string>;
-//     private dictIgnore: Set<string>;
-//     readonly containsNoSuggestWords = false;
-//     readonly options: SpellingDictionaryOptions = {};
-//     readonly type = 'forbidden';
-//     constructor(readonly name: string, readonly source: string, words: string[]) {
-//         this.dict = new Set(words);
-//         this.dictIgnore = new Set(words.filter((w) => w.startsWith('!')).map((w) => w.slice(1)));
-//     }
-
-//     /**
-//      * A Forbidden word list does not "have" valid words.
-//      * Therefore it always returns false.
-//      * @param _word - the word
-//      * @param _options - options
-//      * @returns always false
-//      */
-//     has(_word: string, _options?: HasOptions): boolean {
-//         return false;
-//     }
-
-//     /** A more detailed search for a word, might take longer than `has` */
-//     find(word: string, _options?: SearchOptions): FindResult | undefined {
-//         const forbidden = this.isForbidden(word);
-//         return forbidden ? { found: word, forbidden, noSuggest: false } : undefined;
-//     }
-
-//     isForbidden(word: string): boolean {
-//         return (this.dict.has(word) || this.dict.has(word.toLowerCase())) && !this.dictIgnore.has(word);
-//     }
-
-//     isNoSuggestWord(_word: string, _options: HasOptions): boolean {
-//         return false;
-//     }
-
-//     suggest(
-//         word: string,
-//         numSuggestions?: number,
-//         compoundMethod?: CompoundWordsMethod,
-//         numChanges?: number,
-//         ignoreCase?: boolean
-//     ): SuggestionResult[];
-//     suggest(word: string, suggestOptions: SuggestOptions): SuggestionResult[];
-//     suggest() {
-//         return [];
-//     }
-//     genSuggestions(): void {
-//         return;
-//     }
-//     mapWord(word: string): string {
-//         return word;
-//     }
-//     get size() {
-//         return this.dict.size;
-//     }
-//     readonly isDictionaryCaseSensitive: boolean = true;
-//     getErrors?(): Error[] {
-//         return [];
-//     }
-// }
-
 /**
  * Create a dictionary where all words are to be forbidden.
  * @param wordList - list of words
