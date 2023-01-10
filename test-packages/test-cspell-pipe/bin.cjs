a029ba78764b6ddc66b8fe37eb0726434aa4e15e
Jason Dent <jason@streetsidesoftware.nl>
2023-01-10 13:54:15 +0100

    Update test-cspell-pipe

--- test-packages/test-cspell-pipe/bin.cjs
+++ test-packages/test-cspell-pipe/bin.cjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const assert = require('assert');
-const { sumValues } = require('./dist/index');
+const { sumValues, sumValuesSync } = require('./dist/index');
 
 assert(sumValues([1, 2, 3]) === 6);
+assert(sumValuesSync([1, 2, 3]) === 6);
--- test-packages/test-cspell-pipe/src/index.ts
+++ test-packages/test-cspell-pipe/src/index.ts
@@ -1,5 +1,10 @@
 import { reduce } from '@cspell/cspell-pipe/sync';
+import { reduceSync } from '@cspell/cspell-pipe';
 
 export function sumValues(numbers: Iterable<number>): number {
     return reduce(numbers, (a, b) => a + b, 0);
+}
+
+export function sumValuesSync(numbers: Iterable<number>): number {
+    return reduceSync(numbers, (a, b) => a + b, 0);
 }
