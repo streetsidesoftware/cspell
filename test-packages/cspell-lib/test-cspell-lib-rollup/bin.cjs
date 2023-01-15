22c5038dbeecc075d3304dcc85e9e77c9cf6264f
Jason Dent <jason@streetsidesoftware.nl>
2023-01-15 08:13:23 +0100

    test: Add test bin to package bin

--- test-packages/cspell-lib/test-cspell-lib-rollup/bin.cjs
+++ test-packages/cspell-lib/test-cspell-lib-rollup/bin.cjs
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable node/shebang */
 
 const { run } = require('./dist/index.cjs');
 run();
--- test-packages/cspell-types/test-cspell-types-rollup/bin.cjs
+++ test-packages/cspell-types/test-cspell-types-rollup/bin.cjs
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable node/shebang */
 
 const assert = require('assert');
 const { gatherIssues } = require('./dist/index.cjs');
