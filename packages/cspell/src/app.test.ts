8729f82388f820f221cb1c79f2e14e0ca1ab22e4
Jason Dent <jason@streetsidesoftware.nl>
2023-02-03 09:23:00 +0100

    rm `*` file.

--- packages/cspell/src/app.test.ts
+++ packages/cspell/src/app.test.ts
@@ -168,7 +168,7 @@ describe('Validate cli', () => {
         ${'--fail-fast with config'}                   | ${['-r', failFastRoot, '-c', failFastConfig, '*.txt']}                            | ${app.CheckFailed} | ${true}  | ${true}  | ${false}
         ${'--no-fail-fast with config'}                | ${['-r', failFastRoot, '--no-fail-fast', '-c', failFastConfig, '*.txt']}          | ${app.CheckFailed} | ${true}  | ${true}  | ${false}
         ${'issue-2998 --language-id'}                  | ${['-r', pathFix('issue-2998'), '-v', '--language-id=fix', 'fix-words.txt']}      | ${undefined}       | ${true}  | ${false} | ${true}
-        ${'Explicit file://'}                          | ${['-r', pathFix('misc'), 'file://star-*.md']}                                    | ${undefined}       | ${true}  | ${false} | ${false}
+        ${'Explicit file://'}                          | ${['-r', pathFix('misc'), 'file://star-not.md']}                                  | ${undefined}       | ${true}  | ${false} | ${false}
         ${'typos'}                                     | ${['-r', pathFix('features/typos'), '--no-progress', '--show-suggestions', '**']} | ${app.CheckFailed} | ${true}  | ${true}  | ${false}
     `('app $msg Expect Error: $errorCheck', async ({ testArgs, errorCheck, eError, eLog, eInfo }: TestCase) => {
         chalk.level = 1;
