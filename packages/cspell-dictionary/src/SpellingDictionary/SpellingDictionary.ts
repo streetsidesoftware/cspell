cd5b9ac7eb7a7e68e24634d6a488867fa212498a
Jason Dent <jason@streetsidesoftware.nl>
2023-02-13 12:30:53 +0100

    Update SpellingDictionary.ts

--- packages/cspell-dictionary/src/SpellingDictionary/SpellingDictionary.ts
+++ packages/cspell-dictionary/src/SpellingDictionary/SpellingDictionary.ts
@@ -23,25 +23,30 @@ export interface SuggestOptions {
      * `NONE` is the best option.
      */
     compoundMethod?: CompoundWordsMethod | undefined;
+
     /**
      * The limit on the number of suggestions to generate. If `allowTies` is true, it is possible
      * for more suggestions to be generated.
      */
     numSuggestions?: number | undefined;
+
     /**
      * Max number of changes / edits to the word to get to a suggestion matching suggestion.
      */
     numChanges?: number | undefined;
+
     /**
      * Allow for case-ingestive checking.
      */
     ignoreCase?: boolean | undefined;
+
     /**
      * If multiple suggestions have the same edit / change "cost", then included them even if
      * it causes more than `numSuggestions` to be returned.
      * @default false
      */
     includeTies?: boolean | undefined;
+
     /**
      * Maximum amount of time to allow for generating suggestions.
      */
