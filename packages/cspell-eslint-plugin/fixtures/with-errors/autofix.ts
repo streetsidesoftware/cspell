83bccac5b2a67927041ef125d6fca51c43bed417
Jason Dent <jason@streetsidesoftware.nl>
2023-02-13 17:58:52 +0100

    Add a fixture with errors

---
+++ packages/cspell-eslint-plugin/fixtures/with-errors/autofix.ts
@@ -0,0 +1,12 @@
+const bluelist = ['one', 'two', 'three'];
+
+/**
+ * This is a file with errors that can be autoFixed.
+ *
+ * Time to go to the cafe.
+ */
+export function calcBluelist(names: string[]) {
+    const bluelistSet = new Set(bluelist);
+
+    return names.filter(name => bluelistSet.has(name));
+}
