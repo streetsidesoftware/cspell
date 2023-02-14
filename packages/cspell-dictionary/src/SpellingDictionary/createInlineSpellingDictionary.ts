5c6b8eaec7a33c6b70c51748d1532cf6d23663f4
Jason Dent <jason@streetsidesoftware.nl>
2023-02-14 18:40:31 +0100

    Add SuggestDictionary type

--- packages/cspell-dictionary/src/SpellingDictionary/createInlineSpellingDictionary.ts
+++ packages/cspell-dictionary/src/SpellingDictionary/createInlineSpellingDictionary.ts
@@ -4,17 +4,19 @@ import { createFlagWordsDictionary } from './FlagWordsDictionary';
 import { createIgnoreWordsDictionary } from './IgnoreWordsDictionary';
 import type { DictionaryDefinitionInline, SpellingDictionary } from './SpellingDictionary';
 import { createCollection } from './SpellingDictionaryCollection';
+import { createSuggestDictionary } from './SuggestDictionary';
 
 export function createInlineSpellingDictionary(
     inlineDict: DictionaryDefinitionInline,
     source: string
 ): SpellingDictionary {
-    const { words, flagWords, ignoreWords, name } = inlineDict;
+    const { words, flagWords, ignoreWords, suggestWords, name } = inlineDict;
 
     const dictSources = [
         words && createSpellingDictionary(words, name + '-words', source, inlineDict),
         flagWords && createFlagWordsDictionary(flagWords, name + '-flag-words', source),
         ignoreWords && createIgnoreWordsDictionary(ignoreWords, name + '-ignore-words', source),
+        suggestWords && createSuggestDictionary(suggestWords, name + '-suggest', source),
     ].filter(isDefined);
 
     return createCollection(dictSources, name);
--- packages/cspell-dictionary/src/index.ts
+++ packages/cspell-dictionary/src/index.ts
@@ -21,4 +21,5 @@ export {
     createInlineSpellingDictionary,
     createSpellingDictionary,
     createSpellingDictionaryFromTrieFile,
+    createSuggestDictionary,
 } from './SpellingDictionary';
