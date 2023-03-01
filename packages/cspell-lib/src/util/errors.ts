b5e66e64130fc912205d43e752ecf1a43a5c18e4
Jason Dent <jason@streetsidesoftware.nl>
2023-03-01 16:24:22 +0100

    test: improve test coverage

--- packages/cspell-lib/src/util/errors.ts
+++ packages/cspell-lib/src/util/errors.ts
@@ -52,6 +52,51 @@ export class UnknownError extends Error {
     }
 }
 
+type ErrorHandler<T> = (err: unknown) => T | undefined;
+
+type ErrorHandlerP<T> = (err: unknown) => T | Promise<T> | undefined;
+export function catchPromiseError<T>(p: undefined, handler: ErrorHandlerP<T>): undefined;
+export function catchPromiseError<T>(p: Promise<T>, handler: ErrorHandlerP<T>): Promise<T | undefined>;
+export function catchPromiseError<T>(
+    p: Promise<T> | undefined,
+    handler: ErrorHandler<T>
+): Promise<T | undefined> | undefined;
+export function catchPromiseError<T>(
+    p: Promise<T> | undefined,
+    handler: ErrorHandler<T>
+): Promise<T | undefined> | undefined {
+    if (p === undefined) return undefined;
+    return _catchPromiseError(p, handler);
+}
+
+export function wrapCall<U>(fn: (...p: []) => U, handler: ErrorHandler<U>): (...p: []) => U | undefined;
+export function wrapCall<P0, U>(fn: (...p: [P0]) => U, handler: ErrorHandler<U>): (...p: [P0]) => U | undefined;
+export function wrapCall<P0, P1, U>(
+    fn: (...p: [P0, P1]) => U,
+    handler: ErrorHandler<U>
+): (...p: [P0, P1]) => U | undefined;
+export function wrapCall<P extends unknown[], U>(
+    fn: (...p: P) => U,
+    handler: ErrorHandler<U>
+): (...p: P[]) => U | undefined;
+export function wrapCall<P, U>(fn: (...p: P[]) => U, handler: ErrorHandler<U>): (...p: P[]) => U | undefined {
+    return (...p: P[]) => {
+        try {
+            return fn(...p);
+        } catch (e) {
+            return handler(e);
+        }
+    };
+}
+
+async function _catchPromiseError<T>(p: Promise<T>, handler: ErrorHandler<T>): Promise<T | undefined> {
+    try {
+        return await p;
+    } catch (e) {
+        return handler(e);
+    }
+}
+
 export const __testing__ = {
     getTypeOf,
 };
