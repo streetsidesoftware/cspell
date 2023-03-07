537e4349386156298cee10280f975e4f86ffba0d
Jason Dent <jason@streetsidesoftware.nl>
2023-03-07 13:10:32 +0100

    try mocking

--- packages/cspell-lib/src/Settings/GlobalSettings.test.ts
+++ packages/cspell-lib/src/Settings/GlobalSettings.test.ts
@@ -1,37 +1,77 @@
-import Configstore from 'configstore';
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import type { Mock } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 
-// eslint-disable-next-line jest/no-mocks-import
-import {
-    clearData as clearConfigstore,
-    // setData as setConfigstore,
-    clearMocks,
-    mockAll,
-    mockSetData,
-} from '../__mocks__/configstore';
 import { getLogger } from '../util/logger';
+import { ConfigStore } from './cfgStore';
 import { getGlobalConfigPath, getRawGlobalSettings, writeRawGlobalSettings } from './GlobalSettings';
+
+interface MockConfigStore extends ConfigStore {
+    (name: string): MockConfigStore;
+    all: unknown;
+    path: string;
+    size: number;
+    set: (v: unknown) => void;
+    has(k: string): boolean;
+    get(k: string): unknown;
+    delete(k: string): void;
+    clear(): void;
+    mock_getAll: Mock<[], unknown>;
+    mock_setAll: Mock<[v: unknown], unknown>;
+    mock_getPath: Mock<[], string>;
+}
+
+function createMockConfigStore() {
+    let _all: unknown = undefined;
+    let _name = 'name';
+    let _path = 'path';
+
+    const mock_getAll = vi.fn(() => _all);
+    const mock_setAll = vi.fn((v) => (_all = v));
+    const mock_getPath = vi.fn(() => _path);
+
+    const store: MockConfigStore = vi.fn((name) => {
+        _name = name;
+        _path = `/User/HOME/.config/${name}.json`;
+    }) as unknown as MockConfigStore;
+    store.prototype.set = mock_setAll;
+    Object.defineProperty(store, 'all', {
+        get: mock_getAll,
+        set: mock_setAll,
+    });
+    Object.defineProperty(store, 'path', {
+        get: mock_getPath,
+    });
+    Object.defineProperty(store, 'mock_getAll', { value: mock_getAll });
+    Object.defineProperty(store, 'mock_setAll', { value: mock_setAll });
+    Object.defineProperty(store, 'mock_getPath', { value: mock_getPath });
+
+    return store;
+}
+
+vi.mock('./cfgStore');
 
 const logger = getLogger();
 const mockLog = vi.spyOn(logger, 'log').mockImplementation(() => undefined);
 const mockError = vi.spyOn(logger, 'error').mockImplementation(() => undefined);
-const mockConfigstore = vi.mocked(Configstore);
+const mockConfigstore = vi.mocked(ConfigStore);
 
 describe('Validate GlobalSettings', () => {
-    beforeEach(() => {
+    afterEach(() => {
         mockConfigstore.mockClear();
         mockLog.mockClear();
         mockError.mockClear();
-        mockAll.mockClear();
-        clearMocks();
-        clearConfigstore();
     });
 
     test('getGlobalConfigPath', () => {
+        // console.warn('%o', Configstore);
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
         expect(getGlobalConfigPath()).toEqual(expect.stringMatching(/cspell\.json$/));
     });
 
     test('getRawGlobalSettings', () => {
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
         const path = getGlobalConfigPath();
         const s = getRawGlobalSettings();
         expect(s).toEqual({
@@ -40,7 +80,7 @@ describe('Validate GlobalSettings', () => {
                 filename: undefined,
             },
         });
-        mockSetData('version', '0.2.0');
+        // mockSetData('version', '0.2.0');
         const s2 = getRawGlobalSettings();
         expect(s2).toEqual({
             version: '0.2.0',
@@ -53,9 +93,8 @@ describe('Validate GlobalSettings', () => {
     });
 
     test('getRawGlobalSettings with Error', () => {
-        mockAll.mockImplementation(() => {
-            throw new Error('fail');
-        });
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
         const s = getRawGlobalSettings();
         expect(s).toEqual({
             source: {
@@ -67,22 +106,26 @@ describe('Validate GlobalSettings', () => {
     });
 
     test('writeRawGlobalSettings', () => {
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
         const s = getRawGlobalSettings();
         const updated = { ...s, import: ['hello'], extra: { name: 'extra', value: 'ok' } };
         writeRawGlobalSettings(updated);
-        expect(mockSetData).toHaveBeenCalledWith({
+        expect(mockImpl.mock_setAll).toHaveBeenCalledWith({
             import: ['hello'],
         });
     });
 
     test('writeRawGlobalSettings with Error', () => {
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
         const updated = { import: ['hello'] };
-        mockSetData.mockImplementation(() => {
+        mockImpl.mock_setAll.mockImplementation(() => {
             throw new Error('fail');
         });
         const error1 = writeRawGlobalSettings(updated);
         expect(error1).toBeInstanceOf(Error);
-        mockSetData.mockImplementation(() => {
+        mockImpl.mock_setAll.mockImplementation(() => {
             throw 'fail';
         });
         const error2 = writeRawGlobalSettings(updated);
@@ -90,7 +133,9 @@ describe('Validate GlobalSettings', () => {
     });
 
     test('No Access to global settings files', () => {
-        mockAll.mockImplementation(() => {
+        const mockImpl = createMockConfigStore();
+        mockConfigstore.mockImplementation(mockImpl);
+        mockImpl.mock_getAll.mockImplementation(() => {
             throw new SystemLikeError('permission denied', 'EACCES');
         });
         const s = getRawGlobalSettings();
---
+++ packages/cspell-lib/src/Settings/cfgStore.ts
@@ -0,0 +1,4 @@
+import ConfigStoreExport from 'configstore';
+
+export type ConfigStore = typeof ConfigStoreExport;
+export const ConfigStore = ConfigStoreExport;
