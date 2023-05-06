b410402dd9e42dbad8028eb466e3c5db09b5701a
Jason3S <Jason3S@users.noreply.github.com>
2023-05-06 10:09:17 +0000

    ci: Workflow Bot -- Update ALL Dependencies

--- packages/cspell/src/lib/pkgInfo.cjs
+++ packages/cspell/src/lib/pkgInfo.cjs
@@ -3,6 +3,6 @@ Object.defineProperty(exports, '__esModule', { value: true });
 exports.npmPackage = exports.pkgDir = void 0;
 const path_1 = require('path');
 exports.pkgDir = (0, path_1.join)(__dirname, '../..');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+
 exports.npmPackage = require('../../package.json');
 //# sourceMappingURL=pkgInfo.cjs.map
