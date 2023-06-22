c525ad0edc3925e4a480388479d3d4ee8a369cff
Jason Dent <jason@streetsidesoftware.nl>
2023-06-22 17:12:07 +0200

    Move collectSuggestions into its own file.

--- packages/cspell-trie-lib/src/lib/suggestions/suggestAStar.test.ts
+++ packages/cspell-trie-lib/src/lib/suggestions/suggestAStar.test.ts
@@ -5,9 +5,10 @@ import { mapDictionaryInformationToWeightMap } from '../mappers/mapDictionaryInf
 import { parseDictionaryLegacy } from '../SimpleDictionaryParser.js';
 import { TrieNodeTrie } from '../TrieNode/TrieNodeTrie.js';
 import { CompoundWordsMethod } from '../walker/index.js';
+import { collectSuggestions } from './collectSuggestions.js';
 import type { SuggestionOptions } from './genSuggestionsOptions.js';
 import * as Sug from './suggestAStar.js';
-import type { Progress, SuggestionGenerator, SuggestionResult } from './SuggestionTypes.js';
+import type { SuggestionResult } from './SuggestionTypes.js';
 
 // const oc = expect.objectContaining;
 // const ac = expect.arrayContaining;
@@ -316,38 +317,4 @@ function calcWeightMap(): WeightMap {
             },
         ],
     });
-}
-
-function isProgress(v: unknown | Progress): v is Progress {
-    if (!v || typeof v !== 'object') return false;
-    return (v as Progress).type === 'progress';
-}
-
-function collectSuggestions(sugGen: SuggestionGenerator, maxCost = 300, numSugs = 10): SuggestionResult[] {
-    const sugs = new Set<SuggestionResult>();
-    const sugMap = new Map<string, SuggestionResult>();
-
-    for (let n = sugGen.next(maxCost); !n.done; n = sugGen.next(maxCost)) {
-        const sug = n.value;
-        if (!sug || isProgress(sug) || sug.cost > maxCost) continue;
-        const pref = sugMap.get(sug.word);
-        if (pref && pref.cost <= sug.cost) continue;
-        if (pref) {
-            sugs.delete(pref);
-        }
-        sugMap.set(sug.word, sug);
-        sugs.add(sug);
-
-        if (sugs.size > numSugs) {
-            const sorted = [...sugs].sort(compare);
-            maxCost = sorted.slice(0, -1).reduce((a, b) => Math.max(a, b.cost), 0);
-            sugs.delete(sorted[sorted.length - 1]);
-        }
-    }
-
-    return [...sugs].sort(compare);
-
-    function compare(a: SuggestionResult, b: SuggestionResult): number {
-        return a.cost - b.cost;
-    }
 }
