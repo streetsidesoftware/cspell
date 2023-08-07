4f583fc4b5d49b3ebdc3687b8ed3ed56cb493de9
Jason Dent <jason@streetsidesoftware.nl>
2023-08-07 19:15:51 +0200

    Update options.test.ts

--- packages/cspell-eslint-plugin/src/common/options.test.ts
+++ packages/cspell-eslint-plugin/src/common/options.test.ts
@@ -1,14 +1,14 @@
+import type { CSpellSettings } from '@cspell/cspell-types';
+import assert from 'assert';
 import { describe, test } from 'mocha';
 
 import { defaultOptions, type Options } from './options.js';
-import type { CSpellSettings } from '@cspell/cspell-types';
-import assert from 'assert';
 
 describe('options', () => {
     test('Options are compatible with cspell-types', () => {
         const options: Options = { ...defaultOptions, cspell: { words: ['word'] } };
         assert(options.cspell);
         const settings: CSpellSettings = options.cspell;
-        assert(true, 'it is expected to compile.');
+        assert(settings, 'it is expected to compile.');
     });
 });
