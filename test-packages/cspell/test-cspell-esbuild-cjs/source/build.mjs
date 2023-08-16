ee9d5565174cb4e19b8f88b9bbdc9922773be849
Jason Dent <jason@streetsidesoftware.nl>
2023-08-16 11:40:28 +0200

    Add esbuild for cspell

---
+++ test-packages/cspell/test-cspell-esbuild-cjs/source/build.mjs
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+/* eslint-disable node/no-unpublished-import */
+
+import * as esbuild from 'esbuild';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+async function buildAll() {
+    // Note: cjs is the only possible option at this moment.
+    await esbuild.build({
+        absWorkingDir: __dirname,
+        entryPoints: ['src/index.ts'],
+        bundle: true,
+        platform: 'node',
+        outfile: '../bin/dist/index.cjs',
+    });
+}
+
+buildAll();
---
+++ test-packages/cspell/test-cspell-esbuild-cjs/source/src/index.ts
@@ -0,0 +1,22 @@
+import assert from 'assert';
+import { lint } from 'cspell';
+
+async function checkFile(fileGlob: string) {
+    const result = await lint([fileGlob], {});
+    return result;
+}
+
+export async function run() {
+    const fileGlob = process.argv[2];
+    assert(fileGlob, 'File to check expected.');
+
+    console.log(`Start: ${new Date().toISOString()}`);
+    console.log('Spell check file: %s', fileGlob);
+    console.log('%s', process.cwd());
+    const result = await checkFile(fileGlob);
+    console.log('%o', result);
+    assert(result.files === 1);
+    assert(result.issues === 0);
+    assert(result.errors === 0);
+    console.log(`End: ${new Date().toISOString()}`);
+}
