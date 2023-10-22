d12dc648e7b103c772ad73a47eabc13b646a5373
Jason Dent <jason@streetsidesoftware.nl>
2023-10-22 19:10:35 +0200

    fix: Add `makefile` dictionary

--- packages/cspell-bundled-dicts/cspell-default.config.ts
+++ packages/cspell-bundled-dicts/cspell-default.config.ts
@@ -150,6 +150,7 @@ const settings: AdvancedCSpellSettings = {
         '@cspell/dict-latex/cspell-ext.json',
         '@cspell/dict-lorem-ipsum/cspell-ext.json',
         '@cspell/dict-lua/cspell-ext.json',
+        '@cspell/dict-makefile/cspell-ext.json',
         '@cspell/dict-node/cspell-ext.json',
         '@cspell/dict-npm/cspell-ext.json',
         '@cspell/dict-php/cspell-ext.json',
