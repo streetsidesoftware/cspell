0956ab7fa7ffe8c06954af3bd22526d13b893a1e
Jason Dent <jason@streetsidesoftware.nl>
2023-11-07 11:45:02 +0100

    lint

---
+++ website/.eslintrc.js
@@ -0,0 +1,66 @@
+/**
+ * @type { import("eslint").Linter.Config }
+ */
+const config = {
+    root: true,
+    env: {
+        es2020: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        // 'plugin:node/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:promise/recommended',
+        'plugin:prettier/recommended',
+    ],
+    ignorePatterns: [
+        '**/[Ss]amples/**', // cspell:disable-line
+        '**/[Tt]emp/**',
+        '**/*.d.ts',
+        '**/*.map',
+        '**/coverage/**',
+        '**/dist/**',
+        '**/build/**',
+        '**/node_modules/**',
+        '**/.docusaurus/**',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    overrides: [
+        {
+            files: '**/*.ts',
+            extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint'],
+            rules: {
+                '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            },
+        },
+        {
+            files: '**/*.tsx',
+            extends: ['plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+            parser: '@typescript-eslint/parser',
+            plugins: ['@typescript-eslint'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'warn',
+                '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+                'import/no-unresolved': [2, { ignore: ['^@theme', '^@docusaurus', '^@site'] }],
+            },
+        },
+        {
+            files: ['**/*.test.ts', '**/*.spec.ts'],
+            extends: 'plugin:jest/recommended',
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
+
+module.exports = config;
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -8,7 +8,7 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -17,9 +17,7 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+          <Link className="button button--secondary button--lg" to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -29,11 +27,9 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
