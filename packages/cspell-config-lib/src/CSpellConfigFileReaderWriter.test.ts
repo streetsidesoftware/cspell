79c2b7cde4f5a0cd063ab2dc6ac11bcbc2a1f160
Jason Dent <jason@streetsidesoftware.nl>
2023-11-16 10:54:16 +0100

    improve coverage

--- packages/cspell-config-lib/src/CSpellConfigFileReaderWriter.test.ts
+++ packages/cspell-config-lib/src/CSpellConfigFileReaderWriter.test.ts
@@ -4,6 +4,8 @@ import { CSpellConfigFileReaderWriterImpl } from './CSpellConfigFileReaderWriter
 import type { IO } from './IO.js';
 import { defaultDeserializers } from './serializers/index.js';
 import { json } from './test-helpers/util.js';
+import { CSpellSettings } from '@cspell/cspell-types';
+import { CSpellConfigFile } from './CSpellConfigFile.js';
 
 const oc = expect.objectContaining;
 
@@ -46,4 +48,28 @@ describe('CSpellConfigFileReaderWriter', () => {
         await expect(rw.writeConfig(cf)).resolves.toBeUndefined();
         expect(io.writeFile).toHaveBeenCalledWith(new URL(uri), content);
     });
+
+    test('Fail to serialize', () => {
+        const io: IO = {
+            readFile: vi.fn(() => Promise.resolve('')),
+            writeFile: vi.fn(() => Promise.resolve()),
+        };
+
+        const rw = new CSpellConfigFileReaderWriterImpl(io, defaultDeserializers);
+        const cf = new Cfg(new URL('file:///cspell.js'), {});
+        expect(() => rw.writeConfig(cf)).toThrow('Unable to serialize config file: "file:///cspell.js"');
+    });
 });
+
+class Cfg extends CSpellConfigFile {
+    constructor(
+        public readonly url: URL,
+        public readonly settings: CSpellSettings = {},
+    ) {
+        super(url);
+    }
+
+    addWords(_words: string[]): this {
+        return this;
+    }
+}
--- packages/cspell-config-lib/src/IO.ts
+++ packages/cspell-config-lib/src/IO.ts
@@ -2,5 +2,3 @@ export interface IO {
     readFile(url: URL): Promise<string>;
     writeFile(url: URL, content: string): Promise<void>;
 }
-
-import.meta.resolve;
