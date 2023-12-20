b64462f2cedf66d33f965e341c4126c2c41079dc
Jason Dent <jason@streetsidesoftware.nl>
2023-12-20 17:39:14 +0100

    fix: Export types used by the VFS

--- packages/cspell-io/src/index.ts
+++ packages/cspell-io/src/index.ts
@@ -14,8 +14,16 @@ export {
 } from './file/index.js';
 export type { BufferEncoding, TextEncoding } from './models/BufferEncoding.js';
 export type { Stats } from './models/Stats.js';
+export { FileType as VFileType } from './models/Stats.js';
 export { encodeDataUrl, toDataUrl } from './node/dataUrl.js';
 export { isFileURL, isUrlLike, toFileURL, toURL, urlBasename, urlDirname } from './node/file/url.js';
-export type { VFileSystem as VFileSystem, VFileSystemProvider, VfsDirEntry, VfsStat, VirtualFS } from './VirtualFS.js';
+export type {
+    VFileSystem as VFileSystem,
+    VFileSystemProvider,
+    VfsDirEntry,
+    VfsStat,
+    VirtualFS,
+    VProviderFileSystem,
+} from './VirtualFS.js';
 export { createVirtualFS, FSCapabilityFlags, getDefaultVirtualFs } from './VirtualFS.js';
 export { createRedirectProvider } from './VirtualFS/redirectProvider.js';
