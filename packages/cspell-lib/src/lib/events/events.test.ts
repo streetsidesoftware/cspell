edf6b508c4bd8f60d3b71620540978e547ef08fc
Jason Dent <Jason3S@users.noreply.github.com>
2024-04-16 14:52:06 +0200

    fix: Stop using EventTarget (#5490)

--- packages/cspell-lib/src/lib/events/events.test.ts
+++ packages/cspell-lib/src/lib/events/events.test.ts
@@ -1,50 +1,78 @@
 import { describe, expect, test, vi } from 'vitest';
 
-import { addEventListener, ClearCacheEvent, dispatchClearCache, dispatchEvent, onClearCache } from './events.js';
+import { createEmitter, dispatchClearCache, EventEmitter, onClearCache } from './events.js';
 
 describe('events', () => {
-    describe('addEventListener', () => {
-        test('should add an event listener for ClearCacheEvent', () => {
+    describe('EventEmitter', () => {
+        test('should add and remove listeners', () => {
+            const emitter = createEmitter<string>('test');
+            expect(emitter).toBeInstanceOf(EventEmitter);
             const listener = vi.fn();
-            const disposableListener = addEventListener('clear-cache', listener);
-
-            expect(disposableListener).toBeDefined();
-
-            const event = new ClearCacheEvent();
-            dispatchEvent(event);
-
-            expect(listener).toHaveBeenCalledWith(event);
+            const listener2 = vi.fn();
+            const d1 = emitter.on(listener);
+            const d2 = emitter.on(listener2);
+            emitter.fire('test');
             expect(listener).toHaveBeenCalledTimes(1);
+            expect(listener).toHaveBeenLastCalledWith('test');
+            expect(listener2).toHaveBeenCalledTimes(1);
+            expect(listener2).toHaveBeenLastCalledWith('test');
+            const d1a = emitter.on(listener);
+            emitter.fire('test2');
+            expect(listener).toHaveBeenCalledTimes(2);
+            expect(listener).toHaveBeenLastCalledWith('test2');
+            expect(listener2).toHaveBeenCalledTimes(2);
+            expect(listener2).toHaveBeenLastCalledWith('test2');
+            d1.dispose();
+            emitter.fire('test3');
+            expect(listener).toHaveBeenCalledTimes(2);
+            expect(listener).toHaveBeenLastCalledWith('test2');
+            expect(listener2).toHaveBeenCalledTimes(3);
+            expect(listener2).toHaveBeenLastCalledWith('test3');
+            d1a.dispose();
+            d2.dispose();
+            emitter.dispose();
+        });
 
-            disposableListener.dispose();
-
-            const event2 = new ClearCacheEvent();
-            dispatchEvent(event2);
+        test('should handle errors in listeners', () => {
+            const emitter = createEmitter<string>('test');
+            expect(emitter).toBeInstanceOf(EventEmitter);
+            const listener = vi.fn();
+            const listener2 = vi.fn(() => {
+                throw new Error('test error');
+            });
+            emitter.on(listener);
+            emitter.on(listener2);
+            let r = emitter.fire('test');
             expect(listener).toHaveBeenCalledTimes(1);
+            expect(listener).toHaveBeenLastCalledWith('test');
+            expect(listener2).toHaveBeenCalledTimes(1);
+            expect(listener2).toHaveBeenLastCalledWith('test');
+            expect(r).toHaveLength(1);
+            r = emitter.fire('test2');
+            expect(listener).toHaveBeenCalledTimes(2);
+            expect(listener).toHaveBeenLastCalledWith('test2');
+            expect(listener2).toHaveBeenCalledTimes(2);
+            expect(listener2).toHaveBeenLastCalledWith('test2');
+            expect(r).toHaveLength(1);
+            emitter.dispose();
         });
     });
 
-    describe('ClearCacheEvent', () => {
-        test('should have the correct event name', () => {
-            expect(ClearCacheEvent.eventName).toBe('clear-cache');
-        });
-
-        test('onClearCache should add an event listener for ClearCacheEvent', () => {
+    describe('onClearCache', () => {
+        test('should add an event listener for ClearCacheEvent', () => {
             const listener = vi.fn();
-
             const disposableListener = onClearCache(listener);
 
             expect(disposableListener).toBeDefined();
 
             dispatchClearCache();
+
             expect(listener).toHaveBeenCalledTimes(1);
 
-            dispatchClearCache();
-            expect(listener).toHaveBeenCalledTimes(2);
-
             disposableListener.dispose();
+
             dispatchClearCache();
-            expect(listener).toHaveBeenCalledTimes(2);
+            expect(listener).toHaveBeenCalledTimes(1);
         });
     });
 });
