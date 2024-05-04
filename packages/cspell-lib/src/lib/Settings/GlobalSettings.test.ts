a9c1e131cfe8fde60dd1184a6994ce378de11aa3
Jason Dent <jason@streetsidesoftware.nl>
2024-05-04 22:48:47 +0200

    fix: Drop dependency upon ConfigStore
    ConfigStore hasn't been updated in a while and its dependencies have security issues.

--- packages/cspell-lib/src/lib/Settings/GlobalSettings.test.ts
+++ packages/cspell-lib/src/lib/Settings/GlobalSettings.test.ts
@@ -1,11 +1,8 @@
-import path from 'node:path';
+import fs from 'node:fs/promises';
 
 import { CSpellConfigFileInMemory, CSpellConfigFileJson } from 'cspell-config-lib';
-import type { Mock } from 'vitest';
-import { afterEach, describe, expect, test, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
-import { getLogger } from '../util/logger.js';
-import { ConfigStore } from './cfgStore.js';
 import {
     getGlobalConfig,
     getGlobalConfigPath,
@@ -13,85 +10,33 @@ import {
     writeRawGlobalSettings,
 } from './GlobalSettings.js';
 
-interface MockConfigStore extends ConfigStore {
-    (name: string): MockConfigStore;
-    all: unknown;
-    path: string;
-    size: number;
-    set(v: Record<string, unknown>): void;
-    set(k: string, v: unknown): void;
-    has(k: string): boolean;
-    get(k: string): unknown;
-    delete(k: string): void;
-    clear(): void;
-    mock_getAll: Mock<[], unknown>;
-    mock_setAll: Mock<[v: unknown], unknown>;
-    mock_getPath: Mock<[], string>;
-}
+const fsData = new Map<string, string>();
+
+vi.mock('node:fs/promises', async (_importOriginal) => ({
+    // default: (await _importOriginal<{ default: object }>()).default,
+    default: {
+        readFile: vi.fn(async (filename) => fsData.get(filename)),
+        writeFile: vi.fn(async (filename, data) => fsData.set(filename, data)),
+        mkdir: vi.fn(async () => undefined),
+    },
+}));
 
 const oc = expect.objectContaining;
 
-function createMockConfigStore() {
-    let _all: Record<string, unknown> | undefined = undefined;
-    let _path = 'path';
-
-    const mock_getAll = vi.fn(() => _all);
-    const mock_set = vi.fn((vk: string | Record<string, unknown>, v?: unknown) => {
-        if (typeof vk === 'string') {
-            (_all = _all || {})[vk] = v;
-            return _all;
-        }
-        _all = vk;
-        return _all;
-    });
-    const mock_getPath = vi.fn(() => {
-        return _path;
-    });
-
-    const store: MockConfigStore = vi.fn((id) => {
-        _path = path.resolve(`/User/local/data/.config/configstore/${id}.json`);
-        return store;
-    }) as unknown as MockConfigStore;
-    store.set = mock_set;
-    Object.defineProperty(store, 'all', {
-        get: mock_getAll,
-        set: mock_set,
-    });
-    Object.defineProperty(store, 'path', {
-        get: mock_getPath,
-    });
-    Object.defineProperty(store, 'mock_getAll', { value: mock_getAll });
-    Object.defineProperty(store, 'mock_setAll', { value: mock_set });
-    Object.defineProperty(store, 'mock_getPath', { value: mock_getPath });
-
-    return store;
-}
-
-vi.mock('./cfgStore');
-
-const logger = getLogger();
-const mockLog = vi.spyOn(logger, 'log').mockImplementation(() => undefined);
-const mockError = vi.spyOn(logger, 'error').mockImplementation(() => undefined);
-const mockConfigstore = vi.mocked(ConfigStore);
-
 describe('Validate GlobalSettings', () => {
+    beforeEach(() => {});
+
     afterEach(() => {
-        mockConfigstore.mockClear();
-        mockLog.mockClear();
-        mockError.mockClear();
+        fsData.clear();
+        vi.restoreAllMocks();
     });
 
     test('getGlobalConfigPath', () => {
         // console.warn('%o', Configstore);
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
         expect(getGlobalConfigPath()).toEqual(expect.stringMatching(/cspell\.json$/));
     });
 
     test('getRawGlobalSettings', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
-        const path = getGlobalConfigPath();
         const s = getRawGlobalSettings();
         await expect(s).resolves.toEqual(
             oc({
@@ -100,53 +45,16 @@ describe('Validate GlobalSettings', () => {
                 }),
             }),
         );
-        mockImpl.set('version', '0.2.0');
-        const s2 = getRawGlobalSettings();
-        await expect(s2).resolves.toEqual(
-            oc({
-                version: '0.2.0',
-                source: oc({
-                    name: 'CSpell Configstore',
-                    filename: path,
-                }),
-            }),
-        );
-        expect(mockLog).not.toHaveBeenCalled();
-    });
-
-    test('getRawGlobalSettings with Error', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
-        mockImpl.mock_getAll.mockImplementation(() => {
-            throw new Error('fail');
-        });
-        const s = getRawGlobalSettings();
-        await expect(s).resolves.toEqual(
-            oc({
-                import: [],
-                source: oc({
-                    name: 'CSpell Configstore',
-                }),
-            }),
-        );
-        expect(mockError).toHaveBeenCalledWith(expect.any(Error));
     });
 
     test('writeRawGlobalSettings', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
         const s = await getRawGlobalSettings();
         const updated = { ...s, import: ['hello'], extra: { name: 'extra', value: 'ok' } };
         await writeRawGlobalSettings(updated);
-        expect(mockImpl.mock_setAll).toHaveBeenCalledWith({
-            import: ['hello'],
-        });
+        expect(vi.mocked(fs.writeFile)).toHaveBeenCalledWith(expect.any(String), stringify({ import: ['hello'] }));
     });
 
     test('writeRawGlobalSettings then read', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
-
         const cfBefore = await getGlobalConfig();
 
         expect(cfBefore).toBeInstanceOf(CSpellConfigFileInMemory);
@@ -156,45 +64,10 @@ describe('Validate GlobalSettings', () => {
 
         const cfAfter = await getGlobalConfig();
         expect(cfAfter).toBeInstanceOf(CSpellConfigFileJson);
-    });
-
-    test('writeRawGlobalSettings with Error', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
-        const updated = { import: ['hello'] };
-        const mockSet = vi.mocked(mockImpl.set);
-        mockSet.mockImplementation(() => {
-            throw new Error('fail');
-        });
-        const error1 = writeRawGlobalSettings(updated);
-        await expect(error1).rejects.toBeInstanceOf(Error);
-    });
-
-    test('No Access to global settings files', async () => {
-        const mockImpl = createMockConfigStore();
-        mockConfigstore.mockImplementation(mockImpl);
-        mockImpl.mock_getAll.mockImplementation(() => {
-            throw new SystemLikeError('permission denied', 'EACCES');
-        });
-        const s = getRawGlobalSettings();
-        await expect(s).resolves.toEqual(
-            oc({
-                import: [],
-                source: oc({
-                    name: 'CSpell Configstore',
-                }),
-            }),
-        );
-        expect(mockError).toHaveBeenCalledTimes(0);
-        expect(mockLog).toHaveBeenCalledTimes(0);
+        expect(cfAfter.settings.import).toEqual(['hello']);
     });
 });
 
-class SystemLikeError extends Error {
-    constructor(
-        msg: string,
-        readonly code: string,
-    ) {
-        super(msg);
-    }
+function stringify(obj: object) {
+    return JSON.stringify(obj, undefined, 2) + '\n';
 }
--- packages/cspell-lib/src/lib/Settings/cfgStore.ts
+++ packages/cspell-lib/src/lib/Settings/cfgStore.ts
@@ -1,4 +1,78 @@
-import ConfigStoreExport from 'configstore';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
-export type ConfigStore = typeof ConfigStoreExport;
-export const ConfigStore = ConfigStoreExport;
+import { CSpellSettings } from '@cspell/cspell-types';
+import envPaths from 'env-paths';
+import { xdgConfig } from 'xdg-basedir';
+
+import { isDefined } from '../util/util.js';
+
+const packageName = 'cspell';
+
+export const legacyLocationDir = xdgConfig ? path.join(xdgConfig, 'configstore') : undefined;
+export const cspellGlobalLocationDir = envPaths(packageName, { suffix: '' }).config;
+
+export const defaultConfigFileName = 'cspell.json';
+
+const searchOrder = [cspellGlobalLocationDir, legacyLocationDir].filter(isDefined);
+
+interface GlobalConfigFile {
+    filename: string;
+    config: CSpellSettings;
+}
+export class GlobalConfigStore {
+    #foundLocation: string | undefined;
+    #baseFilename: string;
+
+    constructor(filename: string = defaultConfigFileName) {
+        this.#baseFilename = filename;
+    }
+
+    async #readConfigFile(location: string): Promise<GlobalConfigFile | undefined> {
+        try {
+            const json = await fs.readFile(location, 'utf8');
+            return { filename: location, config: JSON.parse(json) };
+        } catch {
+            return undefined;
+        }
+    }
+
+    async readConfigFile(): Promise<GlobalConfigFile | undefined> {
+        if (this.#foundLocation) {
+            const found = await this.#readConfigFile(this.#foundLocation);
+            if (found) return found;
+        }
+        const firstFile = path.resolve(cspellGlobalLocationDir, this.#baseFilename);
+        const possibleLocations = new Set([
+            firstFile,
+            ...searchOrder.map((p) => path.resolve(p, defaultConfigFileName)),
+        ]);
+
+        for (const filename of possibleLocations) {
+            const found = await this.#readConfigFile(filename);
+            if (found) {
+                this.#foundLocation = found.filename;
+                return found;
+            }
+        }
+
+        return undefined;
+    }
+
+    async writeConfigFile(cfg: CSpellSettings): Promise<string> {
+        this.#foundLocation ??= path.join(cspellGlobalLocationDir, this.#baseFilename);
+        await fs.mkdir(path.dirname(this.#foundLocation), { recursive: true });
+        await fs.writeFile(this.#foundLocation, JSON.stringify(cfg, undefined, 2) + '\n');
+        return this.#foundLocation;
+    }
+
+    get location(): string | undefined {
+        return this.#foundLocation;
+    }
+
+    static create() {
+        return new this();
+    }
+
+    static defaultLocation: string = path.join(cspellGlobalLocationDir, defaultConfigFileName);
+}
