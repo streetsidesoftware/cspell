4bc0e0f9150ea211538bd2d3b860a5d85664966c
Jason Dent <jason@streetsidesoftware.nl>
2024-05-10 12:00:32 +0200

    lint

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+// import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
