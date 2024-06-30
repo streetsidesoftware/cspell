dca4af8f0a753386176151e9b34593fb588c64e1
Jason Dent <jason@streetsidesoftware.nl>
2024-06-30 10:04:24 +0200

    chore: Remove duplicate findUp

--- packages/cspell-io/src/index.ts
+++ packages/cspell-io/src/index.ts
@@ -18,7 +18,15 @@ export type { Stats } from './models/Stats.js';
 export { FileType as VFileType } from './models/Stats.js';
 export { encodeDataUrl, toDataUrl } from './node/dataUrl.js';
 export { isFileURL, isUrlLike, toFileURL, toURL, urlBasename, urlDirname } from './node/file/url.js';
-export type { VFileSystem, VfsDirEntry, VfsStat } from './VFileSystem.js';
+export type {
+    VFileSystem,
+    VFileSystemCore,
+    VFindEntryType,
+    VFindUpPredicate,
+    VFindUpURLOptions,
+    VfsDirEntry,
+    VfsStat,
+} from './VFileSystem.js';
 export { FSCapabilityFlags } from './VFileSystem.js';
 export type { VFileSystemProvider, VirtualFS, VProviderFileSystem } from './VirtualFS.js';
 export { createRedirectProvider } from './VirtualFS/redirectProvider.js';
