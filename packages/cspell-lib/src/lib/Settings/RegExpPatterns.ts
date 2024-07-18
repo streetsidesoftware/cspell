26b8ee8113182b1f825d17f8cc607570edbd9b95
Jason Dent <jason@streetsidesoftware.nl>
2024-07-18 13:37:25 +0200

    fix: ignore `_` in hex values

--- packages/cspell-lib/src/lib/Settings/RegExpPatterns.ts
+++ packages/cspell-lib/src/lib/Settings/RegExpPatterns.ts
@@ -6,7 +6,7 @@ export const regExMatchCommonHexFormats =
     /(?:#[0-9a-f]{3,8})|(?:0x[0-9a-f]+)|(?:\\u[0-9a-f]{4})|(?:\\x\{[0-9a-f]{4}\})/gi;
 export const regExCommitHash = /\b(?![a-f]+\b)(?:0x)?[0-9a-f]{7,}\b/gi; // Match Commit Hashes that contain at least one digit.
 export const regExCommitHashLink = /\[[0-9a-f]{7,}\]/gi; // Match Commit Hash Markdown link: [abcdef0].
-export const regExCStyleHexValue = /\b0x[0-9a-f]+\b/gi;
+export const regExCStyleHexValue = /\b0x[0-9a-f_]+\b/gi;
 export const regExCSSHexValue = /#[0-9a-f]{3,8}\b/gi;
 export const regExUUID = /\b[0-9a-fx]{8}-[0-9a-fx]{4}-[0-9a-fx]{4}-[0-9a-fx]{4}-[0-9a-fx]{12}\b/gi; // x - represents placeholder values
 export const regExUnicodeRef = /\bU\+[0-9a-f]{4,5}(?:-[0-9a-f]{4,5})?/gi;
