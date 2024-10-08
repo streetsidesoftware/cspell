c8f5efcc6ec41611a2d27c8f2904d9fbcbaee0bb
Jason Dent <jason@streetsidesoftware.nl>
2024-10-08 08:56:26 +0200

    fix bad cache return value.

--- scripts/clear-github-cach.sh
+++ scripts/clear-github-cach.sh
@@ -5,7 +5,7 @@
 gh extension install actions/gh-actions-cache
 
 echo "Fetching list of cache key"
-cacheKeysForPR=$(gh actions-cache list --order asc --sort last-used  -L 100 | cut -f 1 )
+cacheKeysForPR=$(gh actions-cache list --order asc --sort last-used -L 100 | cut -f 1 )
 
 ## Setting this to not fail the workflow while deleting cache keys.
 set +e
