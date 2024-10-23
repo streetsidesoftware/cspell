c6de619ad8bc16b413bf31559b701babf9cb025c
Jason Dent <jason@streetsidesoftware.nl>
2024-10-23 09:12:01 +0200

    fix: Use markdown dict from cspell-dict

--- packages/cspell-bundled-dicts/cspell-default.config.ts
+++ packages/cspell-bundled-dicts/cspell-default.config.ts
@@ -154,6 +154,7 @@ const settings: AdvancedCSpellSettings = {
         '@cspell/dict-lorem-ipsum/cspell-ext.json',
         '@cspell/dict-lua/cspell-ext.json',
         '@cspell/dict-makefile/cspell-ext.json',
+        '@cspell/dict-markdown/cspell-ext.json',
         '@cspell/dict-monkeyc/cspell-ext.json',
         '@cspell/dict-node/cspell-ext.json',
         '@cspell/dict-npm/cspell-ext.json',
