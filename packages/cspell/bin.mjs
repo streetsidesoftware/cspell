c670688d71f3103b0196c7ab6f1c9c995b6f6c70
Jason Dent <jason@streetsidesoftware.nl>
2025-01-13 18:30:47 +0100

    fix: Dump stack on error when verbose

--- packages/cspell/bin.mjs
+++ packages/cspell/bin.mjs
@@ -3,16 +3,17 @@ import { format } from 'node:util';
 
 import { CommanderError, program } from 'commander';
 
-import * as app from './dist/esm/app.mjs';
+import { ApplicationError, CheckFailed, run } from './dist/esm/app.mjs';
 
-app.run(program, process.argv).catch((e) => {
-    if (!(e instanceof CommanderError) && !(e instanceof app.CheckFailed)) {
-        const msg = e instanceof app.ApplicationError ? e.message : format(e);
+run(program, process.argv).catch((e) => {
+    if (!(e instanceof CommanderError) && !(e instanceof CheckFailed)) {
+        const verbose = process.argv.includes('--verbose') || process.argv.includes('-v');
+        const msg = !verbose && e instanceof ApplicationError ? e.message : format(e);
         process.stdout.write(msg + '\n');
         // It is possible an explicit exit code was set, use it if it was.
         process.exitCode = process.exitCode || 1;
     }
-    if (e instanceof app.CheckFailed) {
+    if (e instanceof CheckFailed) {
         process.exitCode = e.exitCode;
     }
 });
