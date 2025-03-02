84c18330133155ff74d2d1d4fcf79ae60d79cc62
Jason Dent <Jason3S@users.noreply.github.com>
2025-03-02 08:02:42 +0100

    test: Move test options to the right place. (#6969)

--- packages/cspell-lib/src/lib/test/golang.spec.ts
+++ packages/cspell-lib/src/lib/test/golang.spec.ts
@@ -12,23 +12,19 @@ const sampleFile = fsp.readFile(sampleFilename, 'utf8').then((buffer) => buffer.
 const timeout = 10_000;
 
 describe('Validate that Go files are correctly checked.', () => {
-    test(
-        'Tests the default configuration',
-        async () => {
-            const text = await sampleFile;
-            expect(Object.keys(text)).not.toHaveLength(0);
-            const ext = '.go';
-            const languageIds = cspell.getLanguagesForExt(ext);
-            const settings = await cspell.getDefaultBundledSettingsAsync();
-            const fileSettings = cspell.combineTextAndLanguageSettings(settings, text, languageIds);
-            // cspell:ignore weirdd garbbage longname
-            const results1 = await cspell.validateText('some weirdd garbbage', fileSettings);
-            expect(results1.map((t) => t.text)).toEqual(expect.arrayContaining(['weirdd']));
-            expect(results1.map((t) => t.text)).toEqual(expect.arrayContaining(['garbbage']));
-            const results = await cspell.validateText(text, fileSettings);
-            expect(results).toHaveLength(3);
-            expect(results.map((t) => t.text)).toEqual(expect.arrayContaining(['longname', 'garbbage']));
-        },
-        { timeout },
-    );
+    test('Tests the default configuration', { timeout }, async () => {
+        const text = await sampleFile;
+        expect(Object.keys(text)).not.toHaveLength(0);
+        const ext = '.go';
+        const languageIds = cspell.getLanguagesForExt(ext);
+        const settings = await cspell.getDefaultBundledSettingsAsync();
+        const fileSettings = cspell.combineTextAndLanguageSettings(settings, text, languageIds);
+        // cspell:ignore weirdd garbbage longname
+        const results1 = await cspell.validateText('some weirdd garbbage', fileSettings);
+        expect(results1.map((t) => t.text)).toEqual(expect.arrayContaining(['weirdd']));
+        expect(results1.map((t) => t.text)).toEqual(expect.arrayContaining(['garbbage']));
+        const results = await cspell.validateText(text, fileSettings);
+        expect(results).toHaveLength(3);
+        expect(results.map((t) => t.text)).toEqual(expect.arrayContaining(['longname', 'garbbage']));
+    });
 });
