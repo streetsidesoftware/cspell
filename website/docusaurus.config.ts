2bb8888d824412e9ce2e05529fe72d713c0a4066
Jason Dent <jason@streetsidesoftware.nl>
2025-03-09 16:52:47 +0100

    chore: switch website to Docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
     url: 'https://cspell.org',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/docsV2/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
