98bc21bead67ec400fa3661dfcce182cafa303a9
Jason Dent <Jason3S@users.noreply.github.com>
2025-06-04 18:57:39 +0200

    fix: Allow init of `cspell.config.yml` files (#7432)
    Add `yml` to the init command format list.
    
    ```
    cspell init --format=yml
    ```
    
    It will create a `cspell.config.yml` file.

--- packages/cspell/src/app/config/options.ts
+++ packages/cspell/src/app/config/options.ts
@@ -1,7 +1,7 @@
 export interface InitOptions {
     output?: string;
     import?: string[];
-    format?: 'yaml' | 'json' | 'jsonc';
+    format?: 'yaml' | 'yml' | 'json' | 'jsonc';
     locale?: string;
     comments?: boolean;
     schema?: boolean;
