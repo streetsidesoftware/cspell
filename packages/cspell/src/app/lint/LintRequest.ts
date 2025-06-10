2ff636bb813ebb7c9012122a0611794f1e202b26
Jason Dent <Jason3S@users.noreply.github.com>
2025-06-10 10:04:33 +0200

    Update packages/cspell/src/app/lint/LintRequest.ts
    Signed-off-by: Jason Dent <Jason3S@users.noreply.github.com>

--- packages/cspell/src/app/lint/LintRequest.ts
+++ packages/cspell/src/app/lint/LintRequest.ts
@@ -43,7 +43,7 @@ export class LintRequest {
         this.fileLists = (options.fileList ?? options.fileLists) || [];
         this.files = mergeFiles(options.file, options.files);
 
-        const noConfigSearch = options.configSearch === false ? true : options.configSearch === true ? false : false;
+        const noConfigSearch = options.configSearch === false ? true : options.configSearch === true ? false : undefined;
         const dictionaries = [
             ...(options.disableDictionary ?? []).map((d) => `!${d}`), // first disable dictionaries
             ...(options.dictionary ?? []).map((d) => `!!${d}`), // Use `!!` to ensure dictionaries are enabled
