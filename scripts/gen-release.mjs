6e50946d46537b0aecd81492c66c58730d22a9f6
Jason Dent <jason@streetsidesoftware.nl>
2025-06-13 08:40:35 +0200

    chore: support CHANGELOG update
    Support replacing existing tags in a CHANGELOG

--- scripts/gen-release.mjs
+++ scripts/gen-release.mjs
@@ -106,56 +106,45 @@ Options:
 /**
  *
  * @param {ReleaseData} releaseData
- * @returns
- */
-function injectReleaseData(releaseData) {
-    const notes = buildReleaseNotes(releaseData);
-
-    /**
-     *
-     * @param {Root} tree
-     * @returns {Root}
-     */
-    function inject(tree) {
-        visit(tree, (node, index, parent) => {
-            // Find the first top-level heading (depth 2)
-            // Add the release notes before it.
-            if (index && node.type === 'heading' && node.depth === 2 && parent?.type === 'root') {
-                // Inject the release notes after the first top-level heading
-                parent.children.splice(index, 0, ...notes.children);
-                return false; // Stop visiting further nodes
-            }
-        });
-        return tree;
-    }
-
-    return inject;
-}
-
-/**
- *
- * @param {ReleaseData} releaseData
  * @returns {Root}
  */
-function buildReleaseNotes(releaseData) {
+function buildReleaseNotesRoot(releaseData) {
     const injectNodes = remark().use(remarkGfm).parse(releaseData.body);
     const increaseDepth = transformerIncreaseHeadings();
     const date = new Date().toISOString().split('T')[0];
+    const name = releaseData.tag !== releaseData.name ? ` -- ${releaseData.name}` : '';
+    const headingText = `${releaseData.tag} (${date})${name}`;
     increaseDepth(injectNodes);
     /** @type {Heading} */
     const heading = {
         type: 'heading',
         depth: 2,
-        children: [
-            {
-                type: 'text',
-                value: `${releaseData.name} (${date})`,
-            },
-        ],
+        children: [{ type: 'text', value: headingText }],
     };
     injectNodes.children.unshift(heading);
 
     return injectNodes;
+}
+
+/**
+ *
+ * @param {ReleaseData} releaseData
+ * @returns {string}
+ */
+function buildReleaseNotes(releaseData) {
+    const root = buildReleaseNotesRoot(releaseData);
+    // Convert the MDAST to a string
+    const result = remark()
+        .data('settings', {
+            bullet: '-',
+            bulletOrdered: '.',
+            emphasis: '_',
+            strong: '*',
+            incrementListMarker: true,
+        })
+        .stringify(root)
+        .replaceAll(/^[*]{3}$/gm, '---');
+    return result.replace(/\n*$/, '\n\n');
 }
 
 /**
@@ -188,28 +177,98 @@ function transformerIncreaseHeadings() {
     return increaseHeadingDepth;
 }
 
+const regexReleaseNotesHeader = /^##\s+(v?\d+\.\d+\.\d+)\s+\([-\d]+\).*$/gm;
+
+/**
+ *
+ * @param {string} tag
+ * @returns {RegExp}
+ */
+function regexForTag(tag) {
+    const regexReleaseNotesHeaderTag = /^##\s+(tag)\s+\([-\d]+\).*$/gm;
+    const rTag = tag.replace('v', 'v?').replaceAll('.', '\\.');
+    const r = regexReleaseNotesHeaderTag.source.replace('tag', rTag);
+    return new RegExp(r, 'gm');
+}
+
+/**
+ *
+ * @param {string} content
+ * @param {string} tag
+ * @returns {{ tag: string, index: number } | undefined}
+ */
+function findReleaseNotesHeaderByTag(content, tag) {
+    const regex = regexForTag(tag);
+    const match = regex.exec(content);
+    if (!match) return undefined;
+    const index = match.index;
+    return { tag, index };
+}
+
+/**
+ *
+ * @param {string} content
+ * @param {number} [fromOffset]
+ * @returns {{ tag: string, index: number } | undefined}
+ */
+function findReleaseNotesHeader(content, fromOffset = 0) {
+    const regex = new RegExp(regexReleaseNotesHeader);
+    regex.lastIndex = fromOffset;
+    const match = regex.exec(content);
+    if (!match) return undefined;
+    const index = match.index;
+    return { tag: match[1], index };
+}
+
+const fileHeader = `\
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+`;
+
 /**
  *
  * @param {string} content
  * @param {ReleaseData} releaseData
- * @returns {Promise<string>}
+ * @return {{ content: string, index: number }}
  */
-async function processChangeLogContent(content, releaseData) {
-    const result = await remark()
-        .use(remarkGfm)
-        .use(injectReleaseData, releaseData)
-        .data('settings', {
-            bullet: '-',
-            bulletOrdered: '.',
-            emphasis: '_',
-            strong: '*',
-            incrementListMarker: false,
-        })
-        .process(content);
+function determineContentInjectionPoint(content, releaseData) {
+    const findTagHeader = findReleaseNotesHeaderByTag(content, releaseData.tag);
+    if (findTagHeader) {
+        const index = findTagHeader.index;
+        const nextHeader = findReleaseNotesHeader(content, index + 1);
+        const stopIndex = nextHeader?.index ?? content.length;
+        content = content.slice(0, index) + content.slice(stopIndex);
+        return { content, index };
+    }
 
-    const resultString = String(result).replaceAll(/^[*]{3}$/gm, '---');
+    const foundHeader = findReleaseNotesHeader(content);
+    return { content, index: foundHeader?.index || 0 };
+}
 
-    return resultString;
+/**
+ *
+ * @param {string} content
+ * @param {ReleaseData} releaseData
+ * @returns {string}
+ */
+function processChangeLogContent(content, releaseData) {
+    let notes = buildReleaseNotes(releaseData);
+
+    // Find the first top-level heading (depth 2)
+    // Check if it is duplicated, if so, remove it.
+    // and add the release notes before it.
+
+    const { content: newContent, index } = determineContentInjectionPoint(content, releaseData);
+
+    if (!index) {
+        // If no header found, prepend the file header
+        notes = fileHeader + notes;
+    }
+
+    return newContent.slice(0, index) + notes + newContent.slice(index);
 }
 
 /**
@@ -222,7 +281,7 @@ async function processChangeLog(url, releaseData) {
     const urlOut = releaseData.debug ? new URL('CHANGELOG-1.md', url) : url;
     const content = await fs.readFile(url, 'utf8');
 
-    const updatedChangeLog = await processChangeLogContent(content, releaseData);
+    const updatedChangeLog = processChangeLogContent(content, releaseData);
 
     await fs.writeFile(urlOut, updatedChangeLog, 'utf8');
 }
