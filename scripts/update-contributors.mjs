3ef0e85b1f2ae812ac30d4d780f505a9aada024b
Jason Dent <jason@streetsidesoftware.nl>
2025-06-24 07:47:56 +0200

    chore: try to keep all contributors over time

--- scripts/update-contributors.mjs
+++ scripts/update-contributors.mjs
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process';
-import { writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 
 import { fetchContributors } from './lib/fetch-contributors.mjs';
+
+/**
+ * @import { Contributor } from './lib/fetch-contributors.mjs';
+ */
 
 const token = process.argv[2] || (await getToken());
 
@@ -23,17 +27,27 @@ function contributorToMd(contributor) {
     return `[<img alt="Contributor ${contributor.login}" src="${contributor.avatar_url}&size=128" width=64>](${contributor.html_url})`;
 }
 
+const regExpContribUrl = /\((https:\/\/github\.com\/.*)\)/;
+
 /**
  *
  * @param {Contributor[]} contributors
+ * @param {string} currentContributorsMd
  * @returns string
  */
-function contributorsToMd(contributors) {
-    return (
-        '<!--- cspell:disable --->\n\n' +
-        contributors.map(contributorToMd).join('\n') +
-        '\n\n<!--- cspell:enable --->\n'
-    );
+function contributorsToMd(contributors, currentContributorsMd) {
+    const existingContributorsMd = currentContributorsMd
+        .split('\n')
+        .map((line) => [line.match(regExpContribUrl)?.[1], line])
+        .filter((line) => line[0]);
+    const existing = new Map(existingContributorsMd);
+
+    // Remove new ones from the existing list
+    contributors.forEach((contributor) => existing.delete(contributor.html_url));
+
+    const contributorsMd = [...contributors.map(contributorToMd), ...existing.values()];
+
+    return '<!--- cspell:disable --->\n\n' + contributorsMd.join('\n') + '\n\n<!--- cspell:enable --->\n';
 }
 
 async function run() {
@@ -43,7 +57,9 @@ async function run() {
 
     const contributors = await fetchContributors(token);
 
-    await writeFile(outputFileUrl, contributorsToMd(contributors));
+    const existingContributorsMd = await readFile(outputFileUrl, 'utf8').catch(() => '');
+
+    await writeFile(outputFileUrl, contributorsToMd(contributors, existingContributorsMd));
 }
 
 run();
