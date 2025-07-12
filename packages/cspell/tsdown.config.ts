6eba38b34f4bdd305878fbe78c7c2fccdcb3e547
Jason Dent <jason@streetsidesoftware.nl>
2025-07-12 14:45:42 +0200

    try again

--- packages/cspell/tsdown.config.ts
+++ packages/cspell/tsdown.config.ts
@@ -9,13 +9,4 @@ export default defineConfig([
         sourcemap: true,
         clean: true,
     },
-    {
-        // Use to generate types for the API on the website.
-        entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
-        outDir: 'api',
-        format: ['esm'],
-        dts: true,
-        sourcemap: false,
-        clean: true,
-    },
 ]);
