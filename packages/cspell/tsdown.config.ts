d87ec7813074c33f537751660e94c5ecdf297ed6
Jason Dent <jason@streetsidesoftware.nl>
2025-07-12 14:32:00 +0200

    Update tsdown.config.ts

--- packages/cspell/tsdown.config.ts
+++ packages/cspell/tsdown.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-    entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
-    outDir: 'dist/esm',
-    format: ['esm'],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-});
+export default defineConfig([
+    {
+        entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
+        outDir: 'dist/esm',
+        format: ['esm'],
+        dts: true,
+        sourcemap: true,
+        clean: true,
+    },
+    {
+        // Use to generate types for the API on the website.
+        entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
+        outDir: 'api',
+        format: ['esm'],
+        dts: true,
+        sourcemap: false,
+        clean: true,
+    },
+]);
