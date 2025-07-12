bd6677e36d45e393aa9d8c3fe614219bb65d6fa5
Jason Dent <jason@streetsidesoftware.nl>
2025-07-12 14:23:35 +0200

    fix fixture path

--- packages/cspell/src/test/test.helper.ts
+++ packages/cspell/src/test/test.helper.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, pathToFileURL } from 'node:url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
-export const pathPackageRoot: string = join(__dirname, '../../..');
+export const pathPackageRoot: string = join(__dirname, '../..');
 export const urlPackageRoot: string = pathToFileURL(pathPackageRoot).toString();
 
 export const pathFixtures: string = join(pathPackageRoot, 'fixtures');
