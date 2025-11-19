2cc491b1f236e1a80e60a26156fc9a841c43f785
Jason Dent <jason@streetsidesoftware.nl>
2025-11-19 14:17:24 +0100

    fix issue with tsdown

--- packages/cspell-trie-lib/tsdown.config.ts
+++ packages/cspell-trie-lib/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: ['src/index.ts'],
     outDir: 'dist',
     format: ['esm'],
+    fixedExtension: false,
     dts: true,
     sourcemap: true,
     clean: true,
--- packages/cspell/tsdown.config.ts
+++ packages/cspell/tsdown.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'tsdown';
 
-export default defineConfig([
-    {
-        entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
-        outDir: 'dist/esm',
-        format: ['esm'],
-        dts: true,
-        sourcemap: true,
-        clean: true,
-    },
-]);
+export default defineConfig({
+    entry: ['src/index.mts', 'src/app.mts', 'src/application.mts'],
+    outDir: 'dist/esm',
+    format: ['esm'],
+    fixedExtension: false,
+    dts: true,
+    sourcemap: true,
+    clean: true,
+});
