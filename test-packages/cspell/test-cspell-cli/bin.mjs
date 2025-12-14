4cb2137b97cb8730744cd7f5f21cbaf0f31f519e
Jason Dent <jason@streetsidesoftware.nl>
2025-12-14 17:28:57 +0100

    Update bin.mjs

--- test-packages/cspell/test-cspell-cli/bin.mjs
+++ test-packages/cspell/test-cspell-cli/bin.mjs
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-import {} from 'cspell/bin.mjs';
+import { version } from 'node:process';
+
+if (version < 'v22') {
+    process.argv.push('--exclude=fixtures');
+}
+
+await import('cspell/bin.mjs');
