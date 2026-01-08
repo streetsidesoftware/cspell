60916f63b006dfa99d912404c742e6137cead35f
Jason Dent <Jason3S@users.noreply.github.com>
2026-01-08 15:23:41 +0100

    chore: Improve PR Perf Report (#8321)

--- tools/perf-chart/src/app.ts
+++ tools/perf-chart/src/app.ts
@@ -1,12 +1,12 @@
 import { program } from 'commander';
 
-import { perfReport } from './perfChart.js';
+import { perfReportMd } from './perfChart.ts';
 
 program
     .argument('<file>', 'path to perf data file')
     .description('Generate a min/max chart of the perf data')
     .action(async (file) => {
-        const chart = await perfReport(file);
+        const chart = await perfReportMd(file);
         console.log(chart);
     })
     .parseAsync();
--- tools/perf-chart/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown';
-
-export default defineConfig({
-    entry: ['src/app.ts'],
-    outDir: 'lib',
-    format: ['esm'],
-    fixedExtension: false,
-    clean: true,
-});
