21742ca9d05c7814f403627ec0c33de04b067bf2
Jason Dent <Jason3S@users.noreply.github.com>
2026-01-09 06:17:49 +0100

    chore: refactor: perf-chart generator (#8324)

--- tools/perf-chart/src/app.ts
+++ tools/perf-chart/src/app.ts
@@ -1,12 +1,21 @@
 import { program } from 'commander';
 
-import { perfReportMd } from './perfChart.ts';
+import { perfReport } from './perfChart.ts';
 
 program
     .argument('<file>', 'path to perf data file')
     .description('Generate a min/max chart of the perf data')
-    .action(async (file) => {
-        const chart = await perfReportMd(file);
-        console.log(chart);
+    .option('-o, --output <path>', 'Path to the output the Markdown file.')
+    .option('-g, --svg <path>', 'Path to SVG file to generate.')
+    .option('-p, --png <path>', 'Path to PNG file to generate.')
+    .option('-u, --site-url <url>', 'The url to prefix svg/png path when adding it to the markdown.')
+    .option(
+        '-d, --days <number>',
+        'The number of days in the past to plot.',
+        (value: string) => Number.parseInt(value, 10),
+        30,
+    )
+    .action(async (file, options) => {
+        await perfReport(file, options);
     })
     .parseAsync();
---
+++ tools/perf-chart/src/plotSvg.ts
@@ -0,0 +1,30 @@
+// import * as Plot from '@observablehq/plot';
+import * as Plot from '@observablehq/plot';
+import { JSDOM } from 'jsdom';
+
+import type { CsvRecordsRO } from './CsvRecord.ts';
+import { createDailyStats } from './dailyStats.ts';
+import type { Options } from './options.ts';
+
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export function plotSvg(records: CsvRecordsRO, _options: Options): string {
+    const dailyStats = createDailyStats(records);
+
+    const data = dailyStats.map((row) => ({
+        date: `${monthNames[row.date.getUTCMonth()]}-${row.date.getUTCDate()}`,
+        kps: row.kps,
+    }));
+
+    const plot = Plot.plot({
+        document: new JSDOM('').window.document,
+        marks: [Plot.dot(data, { x: 'date', y: 'kps' })],
+    });
+
+    plot.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns', 'http://www.w3.org/2000/svg');
+    plot.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink');
+
+    return plot.outerHTML;
+}
+
+// cspell:dictionaries html
