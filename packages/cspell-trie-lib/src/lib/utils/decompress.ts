3c864581914ea86e3fa806010a5bca0c901d5ffd
Jason Dent <jason@streetsidesoftware.nl>
2026-02-12 14:06:37 +0100

    Update decompress.ts

--- packages/cspell-trie-lib/src/lib/utils/decompress.ts
+++ packages/cspell-trie-lib/src/lib/utils/decompress.ts
@@ -5,29 +5,33 @@ export async function decompress(
     method: CompressionFormat = 'gzip',
 ): Promise<Uint8Array<ArrayBuffer>> {
     // eslint-disable-next-line n/no-unsupported-features/node-builtins
-    const ds = new DecompressionStream(method || 'deflate-raw');
+    const ds = new DecompressionStream(method);
 
     const writer = ds.writable.getWriter();
-    writer.write(data);
-    writer.close();
-
     const reader = ds.readable.getReader();
 
-    const chunks: Uint8Array[] = [];
-    let size = 0;
+    try {
+        const pWrite = writer.write(data).then(() => writer.close());
 
-    while (true) {
-        const chunk = await reader.read();
-        if (chunk.done) break;
-        chunks.push(chunk.value);
-        size += chunk.value.length;
+        const chunks: Uint8Array[] = [];
+        let size = 0;
+
+        while (true) {
+            const chunk = await reader.read();
+            if (chunk.done) break;
+            chunks.push(chunk.value);
+            size += chunk.value.length;
+        }
+
+        const result = new Uint8Array(size);
+        for (let offset = 0, i = 0; i < chunks.length; i++) {
+            result.set(chunks[i], offset);
+            offset += chunks[i].length;
+        }
+        await pWrite;
+        return result;
+    } finally {
+        reader.releaseLock();
+        writer.releaseLock();
     }
-
-    const result = new Uint8Array(size);
-    for (let offset = 0, i = 0; i < chunks.length; i++) {
-        result.set(chunks[i], offset);
-        offset += chunks[i].length;
-    }
-
-    return result;
 }
