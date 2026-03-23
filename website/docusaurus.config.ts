3de0694a3906e7888490691f4afb7b50094778a2
Jason Dent <jason@streetsidesoftware.nl>
2026-03-23 15:55:07 +0100

    chore: fix edit website link

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -100,7 +100,7 @@ const config: Config = {
                     sidebarPath: './sidebars.ts',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: 'https://github.com/streetsidesoftware/cspell/blob/main/website/docs',
+                    editUrl: 'https://github.com/streetsidesoftware/cspell/tree/main/website',
                     remarkPlugins: [npm2yarn],
                 },
                 // blog: {
